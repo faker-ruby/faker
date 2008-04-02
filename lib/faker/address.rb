e8da0fab40e0d0e81338085b6cce370b2ae14da8
Benjamin Curtis <benjamin.curtis@gmail.com>
2008-04-02 16:07:49 +0000

    Added city names

--- lib/faker/address.rb
+++ lib/faker/address.rb
@@ -13,6 +13,23 @@ module Faker
         %w(AL AK AS AZ AR CA CO CT DE DC FM FL GA GU HI ID IL IN IA KS KY LA ME MH MD MA MI MN MS MO MT NE NV NH NJ NM NY NC ND MP OH OK OR PW PA PR RI SC SD TN TX UT VT VI VA WA WV WI WY AE AA AP).rand
       end
       
+      def city_prefix
+        %w(North East West South New Lake Port).rand
+      end
+      
+      def city_suffix
+        %w(town ton land ville berg burgh borough bury view port mouth stad furt chester mouth fort haven side shire).rand
+      end
+      
+      def city
+        [
+          '%s %s%s' % [city_prefix, Name.first_name, city_suffix],
+          '%s %s' % [city_prefix, Name.first_name],
+          '%s%s' % [Name.first_name, city_suffix],
+          '%s%s' % [Name.last_name, city_suffix],
+        ].rand
+      end
+      
       def street_suffix
         %w(Alley Avenue Branch Bridge Brook Brooks Burg Burgs Bypass Camp Canyon Cape Causeway Center Centers Circle Circles Cliff Cliffs Club Common Corner Corners Course Court Courts Cove Coves Creek Crescent Crest Crossing Crossroad Curve Dale Dam Divide Drive Drive Drives Estate Estates Expressway Extension Extensions Fall Falls Ferry Field Fields Flat Flats Ford Fords Forest Forge Forges Fork Forks Fort Freeway Garden Gardens Gateway Glen Glens Green Greens Grove Groves Harbor Harbors Haven Heights Highway Hill Hills Hollow Inlet Inlet Island Island Islands Islands Isle Isle Junction Junctions Key Keys Knoll Knolls Lake Lakes Land Landing Lane Light Lights Loaf Lock Locks Locks Lodge Lodge Loop Mall Manor Manors Meadow Meadows Mews Mill Mills Mission Mission Motorway Mount Mountain Mountain Mountains Mountains Neck Orchard Oval Overpass Park Parks Parkway Parkways Pass Passage Path Pike Pine Pines Place Plain Plains Plains Plaza Plaza Point Points Port Port Ports Ports Prairie Prairie Radial Ramp Ranch Rapid Rapids Rest Ridge Ridges River Road Road Roads Roads Route Row Rue Run Shoal Shoals Shore Shores Skyway Spring Springs Springs Spur Spurs Square Square Squares Squares Station Station Stravenue Stravenue Stream Stream Street Street Streets Summit Summit Terrace Throughway Trace Track Trafficway Trail Trail Tunnel Tunnel Turnpike Turnpike Underpass Union Unions Valley Valleys Via Viaduct View Views Village Village  Villages Ville Vista Vista Walk Walks Wall Way Ways Well Wells).rand
       end
