8817b7e134d80cd66cfa6de20dfdeb4ed2270d49
George Ornbo <george@shapeshed.com>
2011-03-07 11:17:11 +0000

    cli work in progress

--- lib/faker.rb
+++ lib/faker.rb
@@ -47,5 +47,6 @@ require 'faker/lorem'
 require 'faker/name'
 require 'faker/phone_number'
 require 'faker/version'
+require 'faker/cli'
 
 require 'extensions/array'
---
+++ lib/faker/cli.rb
@@ -0,0 +1,84 @@
+require 'thor'
+require 'faker.rb'
+
+module Faker
+  class CLI < Thor
+
+    default_task :help
+
+    desc "Help", "Help text for the command line tool"
+    def help
+      puts 'help text to do'
+    end
+
+    desc "Address", "Returns random data for an address"
+    def address(option)
+      case option
+        when 'street_name'
+          puts Faker::Address.street_name
+        when 'street_address'
+          puts Faker::Address.street_address
+        when 'secondary_address'
+          puts Faker::Address.secondary_address
+        when 'zip_code'
+          puts Faker::Address.zip_code
+        when 'street_suffix'
+          puts Faker::Address.street_suffix
+        when 'city_suffix'
+          puts Faker::Address.city_suffix
+        when 'city_prefix'
+          puts Faker::Address.city_prefix
+        when 'state_abbr'
+          puts Faker::Address.state_abbr
+        when 'state'
+          puts Faker::Address.state
+        when 'country'
+          puts Faker::Address.country
+        else
+          puts 'no option supplied'
+      end
+    end
+
+    desc "Company", "Returns random data for a company"
+    def company(option)
+      case option
+        when 'name'
+          puts Faker::Company.name
+        when 'suffix'
+          puts Faker::Company.suffix
+        when 'catch_phrase'
+          puts Faker::Company.catch_phrase
+        when 'bs'
+          puts Faker::Company.bs
+        else
+          puts 'no option supplied'
+        end
+    end
+
+    desc "Internet", "Returns random internet data"
+    def internet(option)
+      case option
+        when 'email'
+          puts Faker::Internet.email
+        when 'free_email'
+          puts Faker::Internet.free_email
+        when 'user_name'
+          puts Faker::Internet.user_name
+        when 'domain_name'
+          puts Faker::Internet.domain_name
+        when 'domain_word'
+          puts Faker::Internet.domain_word
+        when 'domain_suffix'
+          puts Faker::Internet.domain_suffix
+        when 'ip_v4_address'
+          puts Faker::Internet.ip_v4_address
+        when 'ip_v6_address'
+          puts Faker::Internet.ip_v6_address
+        else
+          puts 'no option supplied'
+        end
+    end
+
+  end
+end
+#
