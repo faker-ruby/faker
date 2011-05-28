80d9d7814484b5f5d33c5d5732e3adf64886f24b
Chris Kottom <chris@chriskottom.com>
2011-05-28 14:45:00 +0200

    Add optional extended wordlist for Lorem functions

--- lib/faker/lorem.rb
+++ lib/faker/lorem.rb
@@ -1,30 +1,32 @@
 module Faker
   # Based on Perl's Text::Lorem
   class Lorem < Base
-    def self.words(num = 3)
-      I18n.translate('faker.lorem.words').shuffle[0, num]
+    def self.words(num = 3, supplemental = false)
+      wordlist = I18n.translate('faker.lorem.words')
+      wordlist += I18n.translate('faker.lorem.supplemental') if supplemental
+      wordlist.shuffle[0, num]
     end
 
-    def self.sentence(word_count = 4)
-      words(word_count + rand(6)).join(' ').capitalize + '.'
+    def self.sentence(word_count = 4, supplemental = false)
+      words(word_count + rand(6), supplemental).join(' ').capitalize + '.'
     end
 
-    def self.sentences(sentence_count = 3)
+    def self.sentences(sentence_count = 3, supplemental = false)
       [].tap do |sentences|
         1.upto(sentence_count) do
-          sentences << sentence
+          sentences << sentence(3, supplemental)
         end
       end
     end
 
-    def self.paragraph(sentence_count = 3)
-      sentences(sentence_count + rand(3)).join(' ')
+    def self.paragraph(sentence_count = 3, supplemental = false)
+      sentences(sentence_count + rand(3), supplemental).join(' ')
     end
 
-    def self.paragraphs(paragraph_count = 3)
+    def self.paragraphs(paragraph_count = 3, supplemental = false)
       [].tap do |paragraphs|
         1.upto(paragraph_count) do
-          paragraphs << paragraph
+          paragraphs << paragraph(3, supplemental)
         end
       end
     end
