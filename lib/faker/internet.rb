3b709811356f65039f0ec1946f32f72c780fdb73
Daniel Hackney <dan@haxney.org>
2011-06-25 16:17:04 -0700

    Add `http_url` method for generating fake http urls.

--- lib/faker/internet.rb
+++ lib/faker/internet.rb
@@ -4,35 +4,35 @@ module Faker
       def email(name = nil)
         [ user_name(name), domain_name ].join('@')
       end
-      
+
       def free_email(name = nil)
         [ user_name(name), fetch('internet.free_email') ].join('@')
       end
-      
+
       def user_name(name = nil)
         return name.scan(/\w+/).shuffle.join(%w(. _).rand).downcase if name
-        
-        [ 
+
+        [
           Proc.new { Name.first_name.gsub(/\W/, '').downcase },
-          Proc.new { 
-            [ Name.first_name, Name.last_name ].map {|n| 
+          Proc.new {
+            [ Name.first_name, Name.last_name ].map {|n|
               n.gsub(/\W/, '')
             }.join(%w(. _).rand).downcase }
         ].rand.call
       end
-      
+
       def domain_name
         [ domain_word, domain_suffix ].join('.')
       end
-      
+
       def domain_word
         Company.name.split(' ').first.gsub(/\W/, '').downcase
       end
-      
+
       def domain_suffix
         fetch('internet.domain_suffix')
       end
-      
+
       def ip_v4_address
         ary = (2..255).to_a
         [ary.rand,
@@ -45,6 +45,10 @@ module Faker
         @@ip_v6_space ||= (0..65535).to_a
         container = (1..8).map{ |_| @@ip_v6_space.rand }
         container.map{ |n| n.to_s(16) }.join(':')
+      end
+
+      def http_url
+        "http://#{domain_name}/#{user_name}"
       end
     end
   end
