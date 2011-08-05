0d128d84694d56eb1bf7c1c1215389ed8185fa98
Katrina Owen <katrina.owen@gmail.com>
2011-08-05 23:29:08 +0200

    Remove unused method

--- test/test_faker.rb
+++ test/test_faker.rb
@@ -2,9 +2,6 @@ require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestFaker < Test::Unit::TestCase
 
-  def setup
-  end
-  
   def test_numerify
     assert Faker::Base.numerify('###').match(/\d{3}/)
   end
