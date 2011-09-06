17c669894ccf10b9d16cfd62b92b02748fbc330a
Katrina Owen <katrina.owen@gmail.com>
2011-09-06 09:25:27 +0200

    Refactor tests for clarity.

--- test/test_flexible.rb
+++ test/test_flexible.rb
@@ -10,8 +10,9 @@ class TestFlexible < Test::Unit::TestCase
 
   def setup
     I18n.backend.store_translations(:xx, :faker => {:chow => {:yummie => [:fudge, :chocolate, :caramel], :taste => "delicious"}})
-    I18n.backend.store_translations(:xy, :faker => {:address => {:birthplace => [:bed, :hospital, :airplane]}})
-    I18n.backend.store_translations(:yz, :faker => {:name => {:girls_name => [:alice, :cheryl, :tatiana]}})
+    I18n.backend.store_translations(:home, :faker => {:address => {:birthplace => [:bed, :hospital, :airplane]}})
+    I18n.backend.store_translations(:kindergarden, :faker => {:name => {:girls_name => [:alice, :cheryl, :tatiana]}})
+    I18n.backend.store_translations(:work, :faker => {:company => {:do_stuff => [:work, :work, :work]}})
   end
 
   def test_flexible_multiple_values
@@ -36,15 +37,20 @@ class TestFlexible < Test::Unit::TestCase
   end
   
   def test_address_is_flexible
-    I18n.with_locale(:xy) do
+    I18n.with_locale(:home) do
       assert [:bed, :hospital, :airplane].include? Faker::Address.birthplace
     end
   end
 
   def test_name_is_flexible
-    I18n.with_locale(:yz) do
+    I18n.with_locale(:kindergarden) do
       assert [:alice, :cheryl, :tatiana].include? Faker::Name.girls_name
     end
   end
 
+  def test_company_is_flexible
+    I18n.with_locale(:work) do
+      assert Faker::Company.do_stuff == :work
+    end
+  end
 end
