23303df5742e14c4876e421525180d085101a683
Benjamin Curtis <benjamin.curtis@gmail.com>
2011-12-26 19:42:42 -0800

    Stricter UK postcode generation.  Fixes #44

--- test/test_locale.rb
+++ test/test_locale.rb
@@ -16,20 +16,26 @@ class TestLocale < Test::Unit::TestCase
     assert Faker::PhoneNumber.phone_number.match(/\(0\d+\) \d+|\+49-\d+-\d+/)
     assert Faker::Address.street_name.match(//)
   end
-  
+
   def test_configured_locale_translation
     Faker::Config.locale = 'en-bork'
     assert_equal Faker::Base.translate('faker.lorem.words').first, LoadedYaml['en-bork']['lorem']['words'].first
   end
-  
+
   def test_locale_override_when_calling_translate
     Faker::Config.locale = 'en-bork'
     assert_equal Faker::Base.translate('faker.lorem.words', :locale => :en).first, LoadedYaml['en']['lorem']['words'].first
   end
-  
+
   def test_translation_fallback
     Faker::Config.locale = 'en-bork'
     assert_nil LoadedYaml['en-bork']['name']
     assert_equal Faker::Base.translate('faker.name.first_name').first, LoadedYaml['en']['name']['first_name'].first
+  end
+
+  def test_regex
+    Faker::Config.locale = 'en-gb'
+    re = /[A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}/
+    assert re.match(result = Faker::Address.postcode), "#{result} didn't match #{re}"
   end
 end
