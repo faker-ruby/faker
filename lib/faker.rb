49026076dfae217c07a4e399ca68a3ab08f75d4b
Benjamin Curtis <benjamin.curtis@gmail.com>
2011-12-26 19:41:48 -0800

    Added ability to handle (simple) regexes as the format to generate

--- lib/faker.rb
+++ lib/faker.rb
@@ -21,33 +21,77 @@ module Faker
       end
     end
   end
-  
+
   class Base
+    Numbers = Array(0..9)
+    ULetters = Array('A'..'Z')
+    Letters = ULetters + Array('a'..'z')
+
     class << self
       ## make sure numerify results doesn’t start with a zero
       def numerify(number_string)
         number_string.sub(/#/) { (rand(9)+1).to_s }.gsub(/#/) { rand(10).to_s }
       end
-  
+
       def letterify(letter_string)
-        letter_string.gsub(/\?/) { ('A'..'Z').to_a.sample }
+        letter_string.gsub(/\?/) { ULetters.sample }
       end
-  
+
       def bothify(string)
         letterify(numerify(string))
       end
-      
+
+      # Given a regular expression, attempt to generate a string
+      # that would match it.  This is a rather simple implementation,
+      # so don't be shocked if it blows up on you in a spectacular fashion.
+      #
+      # It does not handle ., *, unbounded ranges such as {1,},
+      # extensions such as (?=), character classes, some abbreviations
+      # for character classes, and nested parentheses.
+      #
+      # I told you it was simple. :) It's also probably dog-slow,
+      # so you shouldn't use it.
+      #
+      # It will take a regex like this:
+      #
+      # /^[A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}$/
+      #
+      # and generate a string like this:
+      #
+      # "U3V  3TP"
+      #
+      def regexify(re)
+        re = re.source if re.respond_to?(:source) # Handle either a Regexp or a String that looks like a Regexp
+        re.
+          gsub(/^\/?\^?/, '').gsub(/\$?\/?$/, '').                                                                      # Ditch the anchors
+          gsub(/\{(\d+)\}/, '{\1,\1}').gsub(/\?/, '{0,1}').                                                             # All {2} become {2,2} and ? become {0,1}
+          gsub(/(\[[^\]]+\])\{(\d+),(\d+)\}/) {|match| $1 * Array(Range.new($2.to_i, $3.to_i)).sample }.                # [12]{1,2} becomes [12] or [12][12]
+          gsub(/(\([^\)]+\))\{(\d+),(\d+)\}/) {|match| $1 * Array(Range.new($2.to_i, $3.to_i)).sample }.                # (12|34){1,2} becomes (12|34) or (12|34)(12|34)
+          gsub(/(\\?.)\{(\d+),(\d+)\}/) {|match| $1 * Array(Range.new($2.to_i, $3.to_i)).sample }.                      # A{1,2} becomes A or AA or \d{3} becomes \d\d\d
+          gsub(/\((.*?)\)/) {|match| match.gsub(/[\(\)]/, '').split('|').sample }.                                      # (this|that) becomes 'this' or 'that'
+          gsub(/\[([^\]]+)\]/) {|match| match.gsub(/(\w\-\w)/) {|range| Array(Range.new(*range.split('-'))).sample } }. # All A-Z inside of [] become C (or X, or whatever)
+          gsub(/\[([^\]]+)\]/) {|match| $1.split('').sample }.                                                          # All [ABC] become B (or A or C)
+          gsub('\d') {|match| Numbers.sample }.
+          gsub('\w') {|match| Letters.sample }
+      end
+
       # Helper for the common approach of grabbing a translation
       # with an array of values and selecting one of them.
       def fetch(key)
-        translate("faker.#{key}").sample
+        fetched = translate("faker.#{key}")
+        fetched = fetched.sample if fetched.respond_to?(:sample)
+        if fetched.match(/^\//) and fetched.match(/\/$/) # A regex
+          regexify(fetched)
+        else
+          fetched
+        end
       end
-      
+
       # Load formatted strings from the locale, "parsing" them
       # into method calls that can be used to generate a
       # formatted translation: e.g., "#{first_name} #{last_name}".
       def parse(key)
-        fetch(key).scan(/#\{([A-Za-z]+\.)?([^\}]+)\}([^#]+)?/).map {|kls, meth, etc| 
+        fetch(key).scan(/#\{([A-Za-z]+\.)?([^\}]+)\}([^#]+)?/).map {|kls, meth, etc|
           # If the token had a class Prefix (e.g., Name.first_name)
           # grab the constant, otherwise use self
           cls = kls ? Faker.const_get(kls.chop) : self
@@ -55,12 +99,12 @@ module Faker
           # If the class has the method, call it, otherwise
           # fetch the transation (i.e., faker.name.first_name)
           text = cls.respond_to?(meth) ? cls.send(meth) : fetch("#{(kls || self).to_s.split('::').last.downcase}.#{meth.downcase}")
-          
+
           # And tack on spaces, commas, etc. left over in the string
           text += etc.to_s
         }.join
       end
-      
+
       # Call I18n.translate with our configured locale if no
       # locale is specified
       def translate(*args)
@@ -74,7 +118,7 @@ module Faker
         # in en either, then it will raise again.
         I18n.translate(*(args.push(opts.merge(:locale => :en))))
       end
-      
+
       def flexible(key)
         @flexible_key = key
       end
--- test/test_faker.rb
+++ test/test_faker.rb
@@ -4,7 +4,7 @@ class TestFaker < Test::Unit::TestCase
 
   def setup
   end
-  
+
   def test_numerify
     100.times do
       assert Faker::Base.numerify('###').match(/[1-9]\d{2}/)
@@ -14,4 +14,16 @@ class TestFaker < Test::Unit::TestCase
   def test_letterify
     assert Faker::Base.letterify('???').match(/[A-Z]{3}/)
   end
+
+  def test_regexify
+    {
+      'uk post code' => /^([A-PR-UWYZ0-9][A-HK-Y0-9][AEHMNPRTVXY0-9]?[ABEHMNPRVWXY0-9]? {1,2}[0-9][ABD-HJLN-UW-Z]{2}|GIR 0AA)$/,
+      'us phone' => /^(1-?)[1-8][0-1][0-9]-\d{3}-\d{4}$/
+    }.each do |label, re|
+      10.times do
+        assert re.match(result = Faker::Base.regexify(re)), "#{result} is not a match for #{label}"
+      end
+    end
+  end
+
 end
