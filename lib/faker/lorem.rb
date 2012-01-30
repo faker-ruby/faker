e99b0bacfa8298e14365494d577781f959ca57a8
Dane O'Connor <dane.oconnor@gmail.com>
2012-01-30 00:42:24 -0500

    support generating random Lorem group lengths

--- lib/faker/lorem.rb
+++ lib/faker/lorem.rb
@@ -7,15 +7,15 @@ module Faker
       end
 
       def words(num = 3, supplemental = false)
-      (
-        translate('faker.lorem.words') +
-        (supplemental ? translate('faker.lorem.supplemental') : [])
-      ).shuffle[0, num]
+        (
+          translate('faker.lorem.words') +
+          (supplemental ? translate('faker.lorem.supplemental') : [])
+        ).shuffle[0, resolve(num)]
       end
 
       def characters(char_count = 255)
-        return '' if char_count.to_i < 1
-        rand(36**char_count.to_i).to_s(36)
+        return '' if char_count.respond_to?(:to_i) && char_count.to_i < 1
+        rand(36**resolve(char_count)).to_s(36)
       end
 
       def sentence(word_count = 4, supplemental = false)
@@ -24,22 +24,34 @@ module Faker
 
       def sentences(sentence_count = 3, supplemental = false)
         [].tap do |sentences|
-          1.upto(sentence_count) do
+          1.upto(resolve(sentence_count)) do
             sentences << sentence(3, supplemental)
           end
         end
       end
 
       def paragraph(sentence_count = 3, supplemental = false)
-        sentences(sentence_count + rand(3), supplemental).join(' ')
+        sentences(resolve(sentence_count) + rand(3), supplemental).join(' ')
       end
 
       def paragraphs(paragraph_count = 3, supplemental = false)
         [].tap do |paragraphs|
-          1.upto(paragraph_count) do
+          1.upto(resolve(paragraph_count)) do
             paragraphs << paragraph(3, supplemental)
           end
         end
+      end
+    end
+
+  private
+
+    # If an array or range is passed, a random value will be selected.
+    # All other values are simply returned.
+    def self.resolve(value)
+      case value
+      when Array then value[rand(value.size)]
+      when Range then rand((value.last+1) - value.first) + value.first
+      else value
       end
     end
   end
--- test/test_faker_lorem.rb
+++ test/test_faker_lorem.rb
@@ -5,10 +5,10 @@ class TestFakerLorem < Test::Unit::TestCase
   def setup
     @tester = Faker::Lorem
     @standard_wordlist = I18n.translate('faker.lorem.words')
-    @complete_wordlist = 
+    @complete_wordlist =
       @standard_wordlist + I18n.translate('faker.lorem.supplemental')
   end
-  
+
   # Words delivered by a standard request should be on the standard wordlist.
   def test_standard_words
     @words = @tester.words(1000)
@@ -26,5 +26,24 @@ class TestFakerLorem < Test::Unit::TestCase
     @tester = Faker::Lorem
     @standard_wordlist = I18n.translate('faker.lorem.words')
     1000.times { assert @standard_wordlist.include?(@tester.word) }
+  end
+
+  def test_exact_count_param
+    assert(@tester.characters(2).length == 2)
+    assert(@tester.words(2).length == 2)
+    assert(@tester.sentences(2).length == 2)
+    assert(@tester.paragraphs(2).length == 2)
+  end
+
+  def test_range_count_param
+    cs = @tester.characters(2..5)
+    ws = @tester.words(2..5)
+    ss = @tester.sentences(2..5)
+    ps = @tester.paragraphs(2..5)
+
+    assert(2 <= cs.length && cs.length <= 5)
+    assert(2 <= ws.length && ws.length <= 5)
+    assert(2 <= ss.length && ss.length <= 5)
+    assert(2 <= ps.length && ps.length <= 5)
   end
 end
