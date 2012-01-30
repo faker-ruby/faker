290f72e0d0bd6b38656402c5214edc0391b300ca
Dane O'Connor <dane.oconnor@gmail.com>
2012-01-30 00:42:24 -0500

    support generating random Lorem group lengths

--- test/test_faker_lorem.rb
+++ test/test_faker_lorem.rb
@@ -20,4 +20,23 @@ class TestFakerLorem < Test::Unit::TestCase
     @words = @tester.words(10000, true)
     @words.each {|w| assert @complete_wordlist.include?(w) }
   end
+
+  def test_exact_count_param
+    assert(@tester.characters(2).length == 2)
+    assert(@tester.words(2).length == 2)
+    assert(@tester.sentences(2).length == 2)
+    assert(@tester.paragraphs(2).length == 2)
+  end
+
+  def test_range_count_param
+    cs = @tester.characters(2..5)
+    ws = @tester.words(2..5)
+    ss = @tester.sentences(2..5)
+    ps = @tester.paragraphs(2..5)
+
+    assert(2 <= cs.length && cs.length <= 5)
+    assert(2 <= ws.length && ws.length <= 5)
+    assert(2 <= ss.length && ss.length <= 5)
+    assert(2 <= ps.length && ps.length <= 5)
+  end
 end
