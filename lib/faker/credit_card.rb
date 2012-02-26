b8f9c04d101aeb85799772b15266ab3bb91e6d96
Jeff Casimir <jeff@casimircreative.com>
2012-02-26 10:12:34 -0500

    Added expiration dates

--- lib/faker/credit_card.rb
+++ lib/faker/credit_card.rb
@@ -1,16 +1,25 @@
+require 'date'
+
 module Faker
   class CreditCard < Base
     class << self
       VISA_TOTAL_LENGTH = 16
+      MAXIMUM_EXPIRATION_DISTANCE = 48
 
       def number(card_type)
         "4" + generate_string_digits(VISA_TOTAL_LENGTH - 1)
       end
 
+      def expiration_date
+        months_to_advance = rand(1..MAXIMUM_EXPIRATION_DISTANCE)
+        exp = Date.today >> months_to_advance
+        exp.strftime("%m/%y")
+      end
+
       private
 
-      def generate_string_digits(quantity)
-        (0...quantity).map{ rand(10).to_s }.join
+      def generate_string_digits(quantity, values = (0...10))
+        (0...quantity).map{ rand(values).to_s }.join
       end
     end   
   end
--- test/test_faker_credit_card.rb
+++ test/test_faker_credit_card.rb
@@ -13,4 +13,30 @@ class TestFakerCreditCard < Test::Unit::TestCase
       generated << new_number
     end    
   end
+
+  def test_expiration_structure
+    assert_match Faker::CreditCard.expiration_date, /\d{2}\/\d{2}/
+  end
+
+  def test_expiration_reasonable_date
+    100.times do
+      assert_nothing_raised { Date.parse(Faker::CreditCard.expiration_date) }
+    end 
+  end
+
+  def test_expiration_randomness
+    generated = []
+    3.times do
+      new_date = Faker::CreditCard.expiration_date
+      assert !generated.include?(new_date), "Expiration dates should be randomized"
+      generated << new_date
+    end    
+  end
+
+  def test_expiration_futureness
+    100.times do
+      new_date = Faker::CreditCard.expiration_date
+      assert Date.strptime(new_date, "%m/%y") >= Date.today, "Expiration dates should be in the future"
+    end    
+  end
 end
