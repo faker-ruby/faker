fef1a3819bb8f67bd8a274b8ac2cc5a4f150a5ad
Jeff Casimir <jeff@casimircreative.com>
2012-02-26 15:43:15 -0500

    Added additional CC types and refactored existing code

--- test/test_faker_credit_card.rb
+++ test/test_faker_credit_card.rb
@@ -1,17 +1,37 @@
 require File.dirname(__FILE__) + '/test_helper.rb'
 
 class TestFakerCreditCard < Test::Unit::TestCase
-  def test_visa_format
+  def test_visa_number_format
     assert_match Faker::CreditCard.number(:visa), /^4\d{15}/
   end
 
-  def test_visa_randomness
+  def test_visa_number_randomness
     generated = []
     100.times do
       new_number = Faker::CreditCard.number(:visa)
       assert !generated.include?(new_number), "Visa numbers should be randomized"
       generated << new_number
     end    
+  end
+
+  def test_amex_number_format
+    assert_match Faker::CreditCard.number(:amex), /^3\d{14}/
+  end
+
+  def test_mastercard_number_format
+    assert_match Faker::CreditCard.number(:mastercard), /^5\d{15}/
+  end
+
+  def test_default_number_format
+    type_specified = Faker::CreditCard.number(Faker::CreditCard::DEFAULT_CARD_TYPE)
+    default = Faker::CreditCard.number
+    assert default.start_with?(type_specified[0])
+    assert_equal default.length, type_specified.length
+  end
+
+  def test_number_accepts_strings_and_symbols
+    assert Faker::CreditCard.number(:visa).start_with?("4")
+    assert Faker::CreditCard.number("visa").start_with?("4")
   end
 
   def test_expiration_structure
