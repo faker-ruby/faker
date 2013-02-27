276bdb107d59608b8c2795efc4cb501d7c60f7ec
Konstantin Ilchenko <simpli4eg@gmail.com>
2013-02-27 17:53:59 +0300

    Add isbn generator

---
+++ lib/faker/code.rb
@@ -0,0 +1,31 @@
+module Faker
+  class Code < Base
+    class << self
+      # By default generates 10 sign isbn code in format 123456789-X
+      # You can pass 13 to generate new 13 sign code
+      def isbn(base = 10)
+        base == 13 ? generate_base13_isbn : generate_base10_isbn
+      end
+
+    private
+
+      def generate_base10_isbn
+        values = regexify(/\d{9}/)
+        remainder = sum(values) { |value, index| (index + 1) * value.to_i } % 11
+        values << "-#{remainder == 10 ? 'X' : remainder}"
+      end
+
+      def generate_base13_isbn
+        values = regexify(/\d{12}/)
+        remainder = sum(values) { |value, index| index.even? ? value.to_i : value.to_i * 3 } % 10
+        values << "-#{((10 - remainder) % 10)}"
+      end
+
+      def sum(values, &block)
+        values.split(//).each_with_index.inject(0) do |sum, (value, index)|
+          sum + block.call(value, index)
+        end
+      end
+    end
+  end
+end
