ebf3473645402cf2f881c21deb7660d5a7f7e082
Andrew Curley <andrew.curley@gmail.com>
2013-08-15 17:27:24 -0400

    Add methods for :en-us locale which provides for the construction of valid US phone numbers based on the NANP standard.

--- lib/faker/phone_number.rb
+++ lib/faker/phone_number.rb
@@ -2,7 +2,11 @@ module Faker
   class PhoneNumber < Base
     class << self
       def phone_number
-        numerify(fetch('phone_number.formats'))
+        if parse('phone_number.formats') == ""
+          numerify(fetch('phone_number.formats'))
+        else
+          parse('phone_number.formats')
+        end
       end
 
       def cell_phone
@@ -12,6 +16,37 @@ module Faker
           numerify(fetch('phone_number.formats'))
         end
       end
+
+      # US only
+      def area_code
+        begin
+          fetch('phone_number.area_code')
+        rescue I18n::MissingTranslationData
+          nil
+        end
+      end
+
+      # US only
+      def exchange_code
+        begin
+          fetch('phone_number.exchange_code')
+        rescue I18n::MissingTranslationData
+          nil
+        end
+      end
+
+      # US only
+      # Can be used for both extensions and last four digits of phone number.
+      # Since extensions can be of variable length, this method taks a length parameter
+      def subscriber_number(length = 4)
+        begin
+          rand.to_s[2..(1 + length)]
+        rescue I18n::MissingTranslationData
+          nil
+        end
+      end
+
+      alias_method :extension, :subscriber_number
     end
   end
 end
