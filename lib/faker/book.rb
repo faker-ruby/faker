6a39c8ffac8ffe742d0aa6122793b71e05af4c9c
Dunya Kirkali <dunyakirkali@gmail.com>
2013-12-21 13:29:22 +0100

    refactorings

--- lib/faker/book.rb
+++ lib/faker/book.rb
@@ -2,15 +2,23 @@ module Faker
   class Book < Base
     class << self
       def isbn_ten
-        prefix = 978.to_s # must be 978 or 979
         registration_group_element = rand(10).to_s
         registrant_element = (rand(90000) + 10000).to_s
         publication_element = (rand(900) + 100).to_s
-        _isbn = prefix + registration_group_element + registrant_element + publication_element
         
+        isbn_without_checksum = registration_group_element + registrant_element + publication_element
+        isbn_without_checksum + calculate_checkdigit(isbn_without_checksum)
+      end
+      
+      alias_method :isbn, :isbn_ten
+      alias_method :asin, :isbn_ten
+      
+      private
+      
+      def calculate_checkdigit(raw_isbn)
         check_digit = 0
         i = 0
-        _isbn.each_char do |letter|
+        raw_isbn.each_char do |letter|
           i+= 1
           letter = letter.to_i
           if i % 2 == 0
@@ -25,12 +33,8 @@ module Faker
         else
           check_digit = 10 - rem
         end
-
-        _isbn + check_digit.to_s
+        check_digit.to_s
       end
-      
-      alias_method :isbn, :isbn_ten
-      alias_method :asin, :isbn_ten
     end
   end
 end
