341c3c44f08ff533d8beab6adfdcc0a1966ba02c
Alex Kardos <alexander.kardos@gmail.com>
2014-02-10 15:12:16 -0700

    Added a decimal generator

--- lib/faker/number.rb
+++ lib/faker/number.rb
@@ -5,6 +5,12 @@ module Faker
         rand(10 ** digits).to_s.rjust(digits, '0')
       end
 
+      def decimal(l_digits, r_digits = 2)
+        l_d = self.number(l_digits)
+        r_d = self.number(r_digits) 
+        "#{l_d}.#{r_d}"
+      end
+
       def digit
         rand(9).to_s
       end
