d74173f9a071b941ad0c1a7fde924a6995e4b0be
Alex Kardos <alexander.kardos@gmail.com>
2014-02-10 15:11:56 -0700

    Fixed number generator to generate the correct number of digits

--- lib/faker/number.rb
+++ lib/faker/number.rb
@@ -2,7 +2,7 @@ module Faker
   class Number < Base
     class << self
       def number(digits)
-        rand(1 ** digits).to_s.rjust(digits, '0')
+        rand(10 ** digits).to_s.rjust(digits, '0')
       end
 
       def digit
