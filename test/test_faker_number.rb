7e5a958759b17750ab8b4a3e6eda84549bae7ac7
Benjamin Curtis <benjamin.curtis@gmail.com>
2014-03-08 11:00:17 -0800

    Test correct number of digits
    Added test from https://github.com/stympy/faker/pull/147 that verifies
    behavior in 722041c

--- test/test_faker_number.rb
+++ test/test_faker_number.rb
@@ -5,16 +5,20 @@ class TestFakerNumber < Test::Unit::TestCase
   def setup
     @tester = Faker::Number
   end
-  
+
   def test_number
     assert @tester.number(10).match(/[0-9]{10}/)
+    10.times do |digits|
+      digits += 1
+      assert @tester.number(digits).match(/^[0-9]{#{digits}}$/)
+    end
   end
 
   def test_decimal
     assert @tester.decimal(2).match(/[0-9]{2}\.[0-9]{2}/)
     assert @tester.decimal(4, 5).match(/[0-9]{4}\.[0-9]{5}/)
   end
-  
+
   def test_digit
     assert @tester.digit.match(/[0-9]{1}/)
   end
