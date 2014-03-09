c58f0e13eba06b1496cf7339271b525be2a1f15f
Shim Tw <marocchino@gmail.com>
2014-03-09 11:47:00 +0900

    Fix typo

--- test/test_en_locale.rb
+++ test/test_en_locale.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
 
-class TesetEnLocale < Test::Unit::TestCase
+class TestEnLocale < Test::Unit::TestCase
   def setup
     Faker::Config.locale = nil
   end
