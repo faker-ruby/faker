b156882cf020811b89eacf35ad7ffa6fd913ede9
Oliver Wong <oliver@owiber.com>
2014-05-21 15:43:16 -0500

    adjust test, comment

--- test/test_seeding.rb
+++ test/test_seeding.rb
@@ -6,15 +6,16 @@ class TestSeeding < Test::Unit::TestCase
 
   def test_deterministic
     Faker::Config.random = Random.new(5)
-    first_random = Faker::Number.number(1000)
+    first_random = Faker::Number.number(100000)
     Faker::Config.random = Random.new(5)
-    second_random = Faker::Number.number(1000)
-    Faker::Config.random = Random.new
-    third_random = Faker::Number.number(1000)
+    second_random = Faker::Number.number(100000)
+    Faker::Config.random = nil
+    third_random = Faker::Number.number(100000)
 
     assert first_random == second_random
 
-    # Small chance this will fail randomly
+    # Tiny chance this will fail randomly if the unseeded Random just so
+    # happen generates the same number as Random.new(5)
     assert first_random != third_random
   end
 
