f546e3f3ad4e47b8fadd8585837934335babf890
Hiemanshu Sharma <hiemanshu.s@multunus.com>
2014-06-24 11:13:53 +0530

    Add Faker::App to generate random App details

--- lib/faker.rb
+++ lib/faker.rb
@@ -163,6 +163,7 @@ require 'faker/version'
 require 'faker/number'
 require 'faker/bitcoin'
 require 'faker/avatar'
+require 'faker/app'
 
 require 'extensions/array'
 require 'extensions/symbol'
---
+++ lib/faker/app.rb
@@ -0,0 +1,22 @@
+module Faker
+  class App < Base
+    class << self
+
+      def name
+        fetch('app.name')
+      end
+
+      def version
+        if parse('app.version') == ""
+          numerify(fetch('app.version'))
+        else
+          parse('app.version')
+        end
+      end
+
+      def author
+        parse('app.author')
+      end
+    end
+  end
+end
