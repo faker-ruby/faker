abc67a86807104b030aefdcbc2d7c205b6b556cf
Benjamin Curtis <benjamin.curtis@gmail.com>
2014-07-03 14:32:45 -0700

    Fix Date conflict

--- lib/faker/business.rb
+++ lib/faker/business.rb
@@ -8,15 +8,15 @@ module Faker
       def credit_card_number
         fetch('business.credit_card_numbers')
       end
-      
+
       def credit_card_expiry_date
-        Date.parse(fetch('business.credit_card_expiry_dates'))
+        ::Date.parse(fetch('business.credit_card_expiry_dates'))
       end
-      
+
       def credit_card_type
         fetch('business.credit_card_types')
       end
     end
-    
+
   end
 end
