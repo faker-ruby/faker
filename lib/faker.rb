7fb22a213371ee22bd93a31eb13596750be0f2f9
Benjamin Curtis <benjamin.curtis@gmail.com>
2014-08-15 09:59:10 -0700

    i18n fix

--- lib/faker.rb
+++ lib/faker.rb
@@ -7,7 +7,11 @@ rescue LoadError
 end
 
 require 'i18n'
-defined? I18n.enforce_available_locales = true
+require 'set' # Fixes a bug in i18n 0.6.11
+
+if I18n.respond_to?(:enforce_available_locales=)
+  I18n.enforce_available_locales = true
+end
 I18n.load_path += Dir[File.join(mydir, 'locales', '*.yml')]
 
 
