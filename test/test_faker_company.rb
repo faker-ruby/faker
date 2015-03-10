fd47dcefb7304c86c813ead6391eb6b2842d3120
Erik Midander <erik.midander@gmail.com>
2015-03-10 15:50:25 +0100

    Added Swedish organisation number generator.

--- test/test_faker_company.rb
+++ test/test_faker_company.rb
@@ -16,4 +16,11 @@ class TestFakerCompany < Test::Unit::TestCase
   def test_logo
 	  assert @tester.logo.match(%r{http://pigment.github.io/fake-logos/logos/medium/color/\d+\.png})
   end
+
+  def test_swedish_organisation_number
+    org_no = @tester.swedish_organisation_number
+    assert org_no.match(/\d{10}/)
+    assert org_no[9] == @tester.send(:luhn_algorithm, org_no[0..8]).to_s
+  end
+
 end
