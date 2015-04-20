f5a9a1a09d084fb32c38bbeb0c34f93e23f4660d
Ben Polinsky <benjamin.polinsky@gmail.com>
2015-04-20 17:18:25 -0400

    extra comma

--- lib/faker/placeholdit.rb
+++ lib/faker/placeholdit.rb
@@ -3,7 +3,7 @@ module Faker
     class << self
       SUPPORTED_FORMATS = %w(png jpg bmp jpeg)
 
-      def image(slug = nil, size = '300x300', format = 'png', color = nil, )
+      def image(slug = nil, size = '300x300', format = 'png', color = nil)
         raise ArgumentError, "Size should be specified in format 300x300" unless size.match(/^[0-9]+x[0-9]+$/)
         raise ArgumentError, "Supported formats are #{SUPPORTED_FORMATS.join(', ')}" unless SUPPORTED_FORMATS.include?(format)
         image_url = "http://placehold.it/#{size}.#{format}"
@@ -11,6 +11,9 @@ module Faker
         image_url += "&text=#{slug}" if slug
         image_url
       end
+      
     end
+    
   end
+  
 end
