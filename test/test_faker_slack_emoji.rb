4713ce066568e19c6948bbd325235a71f211eabd
Sergey Morozov <sergey@slack-corp.com>
2015-05-26 11:49:04 -0700

    better slack_emoji tests
    Thanks to for @theB264 and @NikitaAvvakumov catching and fixing it
    
    https://github.com/factory-helper/factory-helper/pull/24

--- test/test_faker_slack_emoji.rb
+++ test/test_faker_slack_emoji.rb
@@ -4,41 +4,42 @@ class TestFakerSlackEmoji < Test::Unit::TestCase
 
   def setup
     @tester = Faker::SlackEmoji
+    @emoticon_regex= /^:([\w-]+):$/
   end
 
   def test_people
-    assert @tester.people.match(/:\w+:/)
+    assert @tester.people.match(@emoticon_regex)
   end
 
   def test_nature
-    assert @tester.nature.match(/:\w+:/)
+    assert @tester.nature.match(@emoticon_regex)
   end
 
   def test_food_and_drink
-    assert @tester.food_and_drink.match(/:\w+:/)
+    assert @tester.food_and_drink.match(@emoticon_regex)
   end
 
   def test_celebration
-    assert @tester.celebration.match(/:\w+:/)
+    assert @tester.celebration.match(@emoticon_regex)
   end
 
   def test_activity
-    assert @tester.activity.match(/:\w+:/)
+    assert @tester.activity.match(@emoticon_regex)
   end
 
   def test_travel_and_places
-    assert @tester.travel_and_places.match(/:\w+:/)
+    assert @tester.travel_and_places.match(@emoticon_regex)
   end
 
   def test_objects_and_symbols
-    assert @tester.objects_and_symbols.match(/:\w+:/)
+    assert @tester.objects_and_symbols.match(@emoticon_regex)
   end
 
   def test_custom
-    assert @tester.custom.match(/:\w+:/)
+    assert @tester.custom.match(@emoticon_regex)
   end
 
   def test_emoji
-    assert @tester.emoji.match(/:\w+:/)
+    assert @tester.emoji.match(@emoticon_regex)
   end
 end
