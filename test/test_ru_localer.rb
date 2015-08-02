f39fd7c9979468b6705afef003ca1a17ccac242f
Alexander Shestakov <Maksar.mail@gmail.com>
2015-08-02 11:18:39 +0300

    Added support for email addresses in ru locale.
    Previosly, Faker::Internet.email wasn't working for :ru locale, because
    domain_name part of the email is based on Faker::Company.name,
    which is in russian letters.
    
    Patch is based on :uk locale implementation, which was sligtly fixed
    to support locale as symbol. Fixed rare test failures in uk locale
    due to cyrilic domain suffixes.

--- test/test_ru_localer.rb
+++ test/test_ru_localer.rb
@@ -1,44 +1,44 @@
 require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
 
-class TestUkLocale < Test::Unit::TestCase
+class TestRuLocale < Test::Unit::TestCase
   def setup
     @previous_locale = Faker::Config.locale
-    Faker::Config.locale = :uk
+    Faker::Config.locale = :ru
   end
 
   def teardown
     Faker::Config.locale = @previous_locale
   end
 
-  def test_uk_email_at_symbol_is_not_followed_by_dot
+  def test_ru_email_at_symbol_is_not_followed_by_dot
     assert Faker::Internet.email.match(/.+@[^.].+\.\p{Alpha}+/)
   end
 
-  def test_uk_domain_word_returns_latin_chars
+  def test_ru_domain_word_returns_latin_chars
     assert Faker::Internet.domain_word.match(/^\w+$/)
   end
 
-  def test_uk_zipcode_length
-    assert Faker::Address.zip_code.match(/^\d{5}$/)
+  def test_ru_zipcode_length
+    assert Faker::Address.zip_code.match(/^\d{6}$/)
   end
 
-  def test_uk_street_prefix_returns_true_value
-    assert_send([Faker::Address, :street_prefix])
+  def test_ru_street_suffix_returns_true_value
+    assert_send([Faker::Address, :street_suffix])
   end
 
-  def test_uk_city_prefix_absent
+  def test_ru_city_prefix_absent
     assert_equal("", Faker::Address.city_prefix)
   end
 
-  def test_uk_city_suffix_absent
+  def test_ru_city_suffix_absent
     assert_equal("", Faker::Address.city_suffix)
   end
 
-  def test_uk_states_do_not_have_abbreviations
+  def test_ru_states_do_not_have_abbreviations
     assert_equal("", Faker::Address.state_abbr)
   end
 
-  def test_uk_company_prefix_returns_true_value
+  def test_ru_company_prefix_returns_true_value
     assert_send([Faker::Company, :prefix])
   end
 end
