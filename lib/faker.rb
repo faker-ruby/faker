0c19621e262890c83583afc2fbf1771620a8afdb
adam-wanninger <wanninaj@miamioh.edu>
2015-08-19 19:11:00 -0400

    added require for faker/shakespeare

--- lib/faker.rb
+++ lib/faker.rb
@@ -181,6 +181,7 @@ require 'faker/hacker'
 require 'faker/app'
 require 'faker/slack_emoji'
 require 'faker/book'
+require 'faker/shakespeare'
 
 require 'extensions/array'
 require 'extensions/symbol'
