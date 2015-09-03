0cf85ae2985601f680ce3b33110f7421fd3bfb7e
Taryn East <git@taryneast.org>
2015-09-03 15:44:23 +1000

    and a dash in the names
    for consistency

--- test/test_en_au_ocker_locale.rb
+++ test/test_en_au_ocker_locale.rb
@@ -18,7 +18,7 @@ class TestEnAuOckerLocale < Test::Unit::TestCase
     assert Faker::Address.region.is_a? String
   end
 
-  def testaussie_mobiles_start_with_04
+  def test_aussie_mobiles_start_with_04
     mobile = Faker::PhoneNumber.cell_phone.gsub(/\D/,'')
     assert_equal '0', mobile[0]
     assert_equal '4', mobile[1]
