dbd51c5d8759662a395874407bca7a69355ac5bc
Taryn East <git@taryneast.org>
2015-09-03 15:31:36 +1000

    Add AU mobile-phone formats
    and remove it from the standard phone types.
    make sure all aussie mobiles start with 04

--- test/test_an_au_locale.rb
+++ test/test_an_au_locale.rb
@@ -2,17 +2,16 @@ require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
 
 class TestEnUsLocale < Test::Unit::TestCase
   def setup
-    Faker::Config.locale = 'en-au-ocker'
+    Faker::Config.locale = 'en-AU'
   end
 
   def teardown
     Faker::Config.locale = nil
   end
 
-  def test_au_ocker_methods_with_en_au_ocker_locale
+  def test_au_methods_with_en_au_locale
     assert Faker::Name.first_name.is_a? String
     assert Faker::Name.last_name.is_a? String
-    assert Faker::Name.ocker_first_name.is_a? String
     assert Faker::Address.street_name.is_a? String
     assert Faker::Address.city.is_a? String
     assert Faker::Address.region.is_a? String
