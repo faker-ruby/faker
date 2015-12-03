cc5a85043cc311fd06784718384a98d1d6feb7da
Luke Shillabeer <luke.shillabeer@careerlounge.com.au>
2015-12-03 14:51:24 +1100

    Fixed spacing issue with .university and .secondary_school

--- lib/faker/educator.rb
+++ lib/faker/educator.rb
@@ -4,13 +4,12 @@ module Faker
 
     class << self
       def university
-        fetch('educator.name') + fetch('educator.tertiary')
+        "#{fetch('educator.name')} #{fetch('educator.tertiary')}"
       end
 
       def secondary_school
-        fetch('educator.name') + fetch('educator.secondary')
+        "#{fetch('educator.name')} #{fetch('educator.secondary')}"
       end
-
     end
   end
 end
