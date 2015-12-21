4bafdf935ed86b5adeaf47ef1cee31b8a57a531a
Swapnil <swapnil@joshsoftware.com>
2015-12-21 11:59:57 +0530

    Ref #487 added middle_name

--- lib/faker/name.rb
+++ lib/faker/name.rb
@@ -8,10 +8,11 @@ module Faker
         parse('name.name')
       end
 
-      def first_name; fetch('name.first_name'); end
-      def last_name;  fetch('name.last_name'); end
-      def prefix;     fetch('name.prefix'); end
-      def suffix;     fetch('name.suffix'); end
+      def first_name;  fetch('name.first_name'); end
+      def middle_name; fetch('name.first_name'); end
+      def last_name;   fetch('name.last_name'); end
+      def prefix;      fetch('name.prefix'); end
+      def suffix;      fetch('name.suffix'); end
 
       # Generate a buzzword-laden job title
       # Wordlist from http://www.bullshitjob.com/title/
