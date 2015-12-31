37c16ca78de417886665c49f2392c813b41e1ad8
Bryan Mytko <bryanmytko@gmail.com>
2015-12-31 01:50:51 -0500

    Add superhero.name

--- lib/faker/superhero.rb
+++ lib/faker/superhero.rb
@@ -4,6 +4,22 @@ module Faker
       def power
         fetch('superhero.power')
       end
+
+      def prefix
+        fetch('superhero.prefix')
+      end
+
+      def suffix
+        fetch('superhero.suffix')
+      end
+
+      def descriptor
+        fetch('superhero.descriptor')
+      end
+
+      def name
+        parse('superhero.name')
+      end
     end
   end
 end
