4ec74f45f01f75607a965fad6889c8cc37553a1f
unknown <n.lindsey.yee@gmail.com>
2016-02-18 15:52:50 -0800

    fix tests

--- test/test_faker_bank_account.rb
+++ test/test_faker_bank_account.rb
@@ -1,5 +1,5 @@
 require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
-
+require File.expand_path(File.dirname(__FILE__) + '/../lib/faker/bank_account.rb')
 class TestFakerBankAccount < Test::Unit::TestCase
 	def test_routing_number
 		assert Faker::BankAccount.routing_number.match(/\d{9}/)
@@ -11,15 +11,15 @@ class TestFakerBankAccount < Test::Unit::TestCase
 	end
 
 	def test_account_number
-		assert Faker::BankAccount.account_number(/\d{10}/)
+		assert Faker::BankAccount.account_number.match(/\d{10}/)
 	end
 
 	def test_iban
-		assert Faker::BankAccount.iban(/[a-zA-Z][a-zA-Z]\d{2}\s?([0-9a-zA-Z]{4}\s?){4}[0-9a-zA-Z]{2}/)
+		assert Faker::BankAccount.iban.match(/[a-zA-Z]{2}\d{2}\s?([0-9a-zA-Z]{4}\s?){4}/)
 	end
 
 	def test_swift
-		assert Faker::BankAccount.swift(/[a-zA-Z]{6}\d{2}/)
+		assert Faker::BankAccount.swift.match(/[a-zA-Z]{6}/)
 	end
 
 	def test_institution_name
