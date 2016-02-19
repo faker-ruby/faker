f4673a5ddf48f202e718addb3ac5e116f1586bc8
unknown <n.lindsey.yee@gmail.com>
2016-02-18 16:00:03 -0800

    Scale down ridiculous for loop

--- lib/faker/bank_account.rb
+++ lib/faker/bank_account.rb
@@ -41,7 +41,7 @@ module Faker
       end
 
       def valid_routing_number
-        for i in 0..500000
+        for i in 0..50
           micr = compile_routing_number
           if checksum(micr)
             break
