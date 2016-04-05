aafc3ffbb952546f7ad6d74ca7b9c6972fad01d2
Dunya Kirkali <dunyakirkali@gmail.com>
2016-04-05 23:53:10 +0200

    Clean up

--- lib/faker/image.rb
+++ lib/faker/image.rb
@@ -5,7 +5,7 @@ module Faker
     def self.image(slug = nil, size = DEFAULT_SIZE)
       raise ArgumentError, "Size should be specified in format #{ DEFAULT_SIZE }" unless size.match(/^[0-9]+x[0-9]+$/)
       sizes = size.split('x')
-      "http://loremflickr.com/#{sizes[0]}/#{sizes[1]}/#{slug ? slug : ''}"
+      "http://loremflickr.com/#{sizes[0]}/#{sizes[1]}/#{slug}"
     end
   end
 end
