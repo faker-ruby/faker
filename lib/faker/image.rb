ff1049da6415c2626668fbf6a6fb4616d7220661
Dunya Kirkali <dunyakirkali@gmail.com>
2016-04-05 23:30:43 +0200

    Use a nice default for default image size

--- lib/faker/image.rb
+++ lib/faker/image.rb
@@ -1,9 +1,10 @@
 module Faker
   class Image < Base
     class << self
-      def image(slug = nil, size = '300x300')
+      DEFAULT_SIZE = '300x300'
+
+      def image(slug = nil, size = DEFAULT_SIZE)
         raise ArgumentError, "Size must be specified" if size.nil?
-        raise ArgumentError, "Size should be specified in format 300x300" unless size.match(/^[0-9]+x[0-9]+$/)
         sizes = size.split('x')
         "http://loremflickr.com/#{sizes[0]}/#{sizes[1]}/#{slug}"
       end
