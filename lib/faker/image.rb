d6828354a9ac3b64ee2b3d1290c663ebab883422
Dunya Kirkali <dunyakirkali@gmail.com>
2016-04-05 23:51:44 +0200

    Clean up

--- lib/faker/image.rb
+++ lib/faker/image.rb
@@ -1,13 +1,11 @@
 module Faker
   class Image < Base
-    class << self
-      DEFAULT_SIZE = '300x300'
+    DEFAULT_SIZE = '300x300'
 
-      def image(slug = nil, size = DEFAULT_SIZE)
-        raise ArgumentError, "Size must be specified" if size.nil?
-        sizes = size.split('x')
-        "http://loremflickr.com/#{sizes[0]}/#{sizes[1]}/#{slug}"
-      end
+    def self.image(slug = nil, size = DEFAULT_SIZE)
+      raise ArgumentError, "Size should be specified in format #{ DEFAULT_SIZE }" unless size.match(/^[0-9]+x[0-9]+$/)
+      sizes = size.split('x')
+      "http://loremflickr.com/#{sizes[0]}/#{sizes[1]}/#{slug ? slug : ''}"
     end
   end
 end
