eeac2fd6d54934d304d2407c47d42b507fa9ace9
Juha <jremes@outlook.com>
2016-04-27 18:19:38 +0100

    Deleted TestFakerBoolean test

--- test/test_faker_boolean.rb
+++ test/test_faker_boolean.rb
@@ -9,15 +9,4 @@ class TestFakerBoolean < Test::Unit::TestCase
   def test_boolean
     assert [true, false].include?(@tester.boolean)
   end
-
-  def test_distribution
-    true_ratio = rand.round(1)
-    true_counter = 0
-
-    10000.times do
-      true_counter += 1 if @tester.boolean(true_ratio)
-    end
-
-    assert_in_delta true_ratio, (true_counter/10000.0), 0.01
-  end
 end
