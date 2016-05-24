d7c8c47b6186e5bdfc91bb9df654b9fbd864d392
Juha Remes <jremes@outlook.com>
2016-05-24 13:11:03 +0100

    Added en-NEP unit tests

---
+++ test/test_en_nep_locale.rb
@@ -0,0 +1,28 @@
+require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
+
+class TestEnNepLocale < Test::Unit::TestCase
+  def setup
+    Faker::Config.locale = 'nep'
+  end
+
+  def teardown
+    Faker::Config.locale = nil
+  end
+
+  def test_en_nep_methods
+    assert Faker::Name.male_first_name.is_a? String
+    assert Faker::Name.female_first_name.is_a? String
+    assert Faker::Name.first_name.is_a? String
+    assert Faker::Name.middle_name.is_a? String
+    assert Faker::Name.last_name.is_a? String
+    assert Faker::Name.name.is_a? String
+    assert Faker::Address.default_time_zone.is_a? String
+    assert Faker::Address.default_country_code.is_a? String
+    assert Faker::Address.state.is_a? String
+    assert Faker::Address.city.is_a? String
+    assert Faker::Address.default_country.is_a? String
+    assert Faker::Internet.free_email.is_a? String
+    assert Faker::Internet.domain_suffix.is_a? String
+    assert Faker::Company.suffix.is_a? String
+  end
+end
