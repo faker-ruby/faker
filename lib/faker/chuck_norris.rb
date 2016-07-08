1c9d4eb67bcc24e2bf1d919a5da792350fd5b5ba
Paweł Smoliński <pawel@onepagecrm.com>
2016-07-08 13:56:22 +0100

    Remove Faker::ChuckNorris.name method (#624)

--- lib/faker/chuck_norris.rb
+++ lib/faker/chuck_norris.rb
@@ -3,11 +3,6 @@ module Faker
     flexible :name
 
     class << self
-
-      def name
-        parse('chuck_norris')
-      end
-
       # from: https://github.com/jenkinsci/chucknorris-plugin/blob/master/src/main/java/hudson/plugins/chucknorris/FactGenerator.java
       def fact; fetch('chuck_norris.fact'); end
 
