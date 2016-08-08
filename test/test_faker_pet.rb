88c230fe910328a632e333a264faccf1918c8c79
amattson21 <amattson21@gmail.com>
2016-08-07 21:46:40 -0700

    Fixed typo in pet test

--- test/test_faker_pet.rb
+++ test/test_faker_pet.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
 
-class TestFakerCat < Test::Unit::TestCase
+class TestFakerPet < Test::Unit::TestCase
   def setup
     @tester = Faker::Pet
   end
