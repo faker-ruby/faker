87ec9049cca779c1d0d0c97676944e8d1da7c726
Marty McLaughlin <martymclaugh@gmail.com>
2016-08-20 16:54:54 -0700

    fix spelling, redefine test method, add measurement sizes to en.yml, all tests passing

--- lib/faker/food.rb
+++ lib/faker/food.rb
@@ -5,13 +5,13 @@ module Faker
         fetch('food.ingredients')
       end
 
-      def spices
+      def spice
         fetch('food.spices')
       end
 
       def measurement
-        ["1/4 ", "1/3 ", "1/2 ", "1 ", "2 ", "3 "].sample + fetch('food.measurements')
-      end
+        fetch('food.measurement_sizes') + fetch('food.measurements')
+      end  
     end
   end
 end
