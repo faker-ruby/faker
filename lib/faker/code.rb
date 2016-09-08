ed669f8721daef914004c4646aef54cc05e496d9
Juha <jremes@outlook.com>
2016-09-08 19:46:43 +0100

    Refactor Luhn Checksum

--- lib/faker/code.rb
+++ lib/faker/code.rb
@@ -77,7 +77,7 @@ module Faker
 
         # Calculate the Luhn checksum of the values thus far
         len_offset = (len + 1) % 2
-        for pos in (0..(len - 1))
+        (0..(len - 1)).each do |pos|
           if (pos + len_offset) % 2 != 0
             t = str[pos] * 2
 
