e9bbc6d7a54bc751cc9f9d56918ef1469a7a1bab
vveliev <vveliev@ryerson.ca>
2016-09-15 05:19:28 -0400

    Update chuck_norris.rb

--- lib/faker/chuck_norris.rb
+++ lib/faker/chuck_norris.rb
@@ -3,10 +3,8 @@ module Faker
     flexible :name
 
     class << self
-
       # from: https://github.com/jenkinsci/chucknorris-plugin/blob/master/src/main/java/hudson/plugins/chucknorris/FactGenerator.java
       def fact; fetch('chuck_norris.fact'); end
-        
     end
   end
 end
