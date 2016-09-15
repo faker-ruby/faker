fcdf7f2c168dfcd221e77a4356f7d9ed40bec885
vveliev <vveliev@ryerson.ca>
2016-09-15 05:19:02 -0400

    Update chuck_norris.rb

--- lib/faker/chuck_norris.rb
+++ lib/faker/chuck_norris.rb
@@ -6,7 +6,7 @@ module Faker
 
       # from: https://github.com/jenkinsci/chucknorris-plugin/blob/master/src/main/java/hudson/plugins/chucknorris/FactGenerator.java
       def fact; fetch('chuck_norris.fact'); end
-
+        
     end
   end
 end
