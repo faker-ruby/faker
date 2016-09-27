e2e8ba79c02cfa4bbfa239328432c36a598b8528
Ryder Timberlake <rtimberlake@salesforce.com>
2016-09-26 22:59:34 -0400

    Made it so blank? will only be defined on Object if there is no existing implementation.

--- lib/extensions/object.rb
+++ lib/extensions/object.rb
@@ -1,6 +1,11 @@
 class Object
-  def blank?
-    return true if self.nil? || self.empty?
-    false
+  def self.add_method(method_name, &block)
+    define_method(method_name, &block)
+  end
+end
+
+unless Object.respond_to? :blank?
+  Object.add_method 'blank?' do
+    self.nil? || self.empty? ? true : false
   end
 end
