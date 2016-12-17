2eaa6fd67028927d54da62cf6d102ab72c0e4990
Tiago Farias <aeroboom@gmail.com>
2016-12-17 12:50:28 -0300

    Creates tests for Address#zip_code (#746)

---
+++ test/test_faker_zip_code.rb
@@ -0,0 +1,66 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+class TestFakerZipCode < Test::Unit::TestCase
+  def setup
+    @zip_codes_without_state = ['50817', '48666', '55551', '14242', '99852']
+    @zip_codes_with_state = ['55555', '44444', '33333', '22222', '11111']
+
+    @old_locales = I18n.config.available_locales
+
+    locale_without_state = {
+      :faker => {
+        :address => {
+          :state_abbreviation => [''],
+          :postcode => @zip_codes_without_state
+        }
+      }
+    }
+
+    locale_with_state = {
+      :faker => {
+        :address => {
+          :postcode_by_state => {
+            :NY => @zip_codes_with_state
+          }
+        }
+      }
+    }
+
+    I18n.backend.store_translations(:xy, locale_without_state)
+    I18n.backend.store_translations(:xz, locale_with_state)
+    I18n.config.available_locales += [:xy, :xz]
+    @tester = Faker::Address
+  end
+
+  def teardown
+    I18n.config.available_locales = @old_locales
+  end
+
+  def test_default_zip_codes_without_states
+    I18n.with_locale(:xy) do
+      zip_codes = @zip_codes_without_state
+      100.times do
+        zip_code = @tester.zip_code
+        assert zip_codes.include?(zip_code), "Expected <#{zip_codes.join(' / ')}>, but got #{zip_code}"
+      end
+    end
+  end
+
+  def test_zip_codes_with_states
+    I18n.with_locale(:xz) do
+      zip_codes = @zip_codes_with_state
+      100.times do
+        zip_code = @tester.zip_code('NY')
+        assert zip_codes.include?(zip_code), "Expected <#{zip_codes.join(' / ')}>, but got #{zip_code}"
+      end
+    end
+  end
+
+  def test_zip_codes_with_states_with_inexisting_state
+    assert_raises I18n::MissingTranslationData do
+      I18n.with_locale(:xz) do
+        @tester.zip_code('MI')
+      end  
+    end
+  end
+end
