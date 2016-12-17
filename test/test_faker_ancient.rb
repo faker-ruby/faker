87e65d3268e77429576f2b4c5b3a4cc6c4927e4c
Paul Phönixweiß <phoenixweiss@me.com>
2016-12-17 18:24:50 +0300

    Add Faker::Ancient
    Includes Ancient Greek gods, primordial deities, titans and common mortal heroes
    
    Closes #765

---
+++ test/test_faker_ancient.rb
@@ -0,0 +1,24 @@
+require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
+
+class TestFakerAncient < Test::Unit::TestCase
+  def setup
+    @tester = Faker::Ancient
+  end
+
+  def test_god
+    assert @tester.god.match(/\w+/)
+  end
+
+  def test_primordial
+    assert @tester.primordial.match(/\w+/)
+  end
+
+  def test_titan
+    assert @tester.titan.match(/\w+/)
+  end
+
+  def test_hero
+    assert @tester.hero.match(/\w+/)
+  end
+
+end
