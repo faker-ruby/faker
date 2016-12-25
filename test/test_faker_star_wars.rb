f0dd0e01465959dbe92b77cdabb327ca8d3d67f2
Ryder Timberlake <wrtimberlake@gmail.com>
2016-12-25 18:41:56 -0500

    Refute blank (#707)

--- test/test_faker_star_wars.rb
+++ test/test_faker_star_wars.rb
@@ -14,38 +14,32 @@ class TestFakerStarWars < Test::Unit::TestCase
   def test_strings
     assert @characters.size == 33
     @characters.each do |character|
-       assert !character.nil?
-       assert character != ""
+       refute character.to_s.empty?
     end
 
   	assert @droids.size == 21
     @droids.each do |droid|
-       assert !droid.nil?
-       assert droid != ""
+       refute droid.to_s.empty?
     end
 
     assert @planets.size == 20
     @planets.each do |planet|
-      assert !planet.nil?
-      assert planet != ""
+      refute planet.to_s.empty?
     end
 
   	assert @quotes.size == 30
     @quotes.each do |quote|
-       assert !quote.nil?
-       assert quote != ""
+       refute quote.to_s.empty?
     end
 
   	assert @species.size == 9
     @species.each do |specie|
-       assert !specie.nil?
-       assert specie != ""
+       refute specie.to_s.empty?
     end
 
     assert @vehicles.size == 21
     @vehicles.each do |vehicle|
-       assert !vehicle.nil?
-       assert vehicle != ""
+       refute vehicle.to_s.empty?
     end
 
     assert @wookie_words.size == 22
