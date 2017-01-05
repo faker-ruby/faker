e23fed0149f3f552922b342e3fa3a36bfdb15a37
tiagofsilva <aeroboom@gmail.com>
2017-01-05 04:07:28 -0300

    Refactors code in Faker::Name class

--- lib/faker/name.rb
+++ lib/faker/name.rb
@@ -12,14 +12,27 @@ module Faker
         parse('name.name_with_middle')
       end
 
-      def first_name; fetch('name.first_name'); end
-      def last_name;  fetch('name.last_name'); end
-      def prefix;     fetch('name.prefix'); end
-      def suffix;     fetch('name.suffix'); end
+      def first_name
+        fetch('name.first_name')
+      end
+
+      def last_name 
+        fetch('name.last_name')
+      end
+
+      def prefix 
+        fetch('name.prefix')
+      end
+
+      def suffix 
+        fetch('name.suffix')
+      end
 
       # Generate a buzzword-laden job title
       # Wordlist from http://www.bullshitjob.com/title/
-      def title; fetch('name.title.descriptor') + ' ' + fetch('name.title.level') + ' ' + fetch('name.title.job'); end
+      def title 
+        "#{fetch('name.title.descriptor')} #{fetch('name.title.level')} #{fetch('name.title.job')}"
+      end
 
       def job_titles
         fetch_all('name.title.job')
