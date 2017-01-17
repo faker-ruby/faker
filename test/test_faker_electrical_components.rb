dcf008caf71f2c35c25beec918d16d8744b9afe8
Brian Heim <bheim67@gmail.com>
2017-01-17 14:09:54 -0700

    test setup added for electrical components, test is included in test utput when running rake task, all tests pass

---
+++ test/test_faker_electrical_components.rb
@@ -0,0 +1,7 @@
+require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
+
+class TestFakerElectricalComponents < Test::Unit::TestCase
+  def setup
+    @tester = Faker::ElectricalComponents
+  end
+end
