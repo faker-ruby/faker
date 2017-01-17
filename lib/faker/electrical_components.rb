81ddfc90431a2a8767313386fb6af97d536b6d59
Brian Heim <bheim67@gmail.com>
2017-01-17 15:06:01 -0700

    remove unnecessary class << self code

--- lib/faker/electrical_components.rb
+++ lib/faker/electrical_components.rb
@@ -2,7 +2,6 @@ module Faker
   class ElectricalComponents < Base
     flexible :electrical_components
 
-    class << self
       def active
         fetch('electrical_components.active')
       end
@@ -14,6 +13,6 @@ module Faker
       def electromechanical
         fetch('electrical_components.electromechanical')
       end
-    end
+
   end
 end
