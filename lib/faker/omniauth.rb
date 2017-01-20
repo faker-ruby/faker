b9e1bd5a8c8c4583b5c1795bdca9eac9fc0516e1
PlanetEfficacy <jspevack@gmail.com>
2017-01-20 16:37:18 -0700

    Add initializer

--- lib/faker/omniauth.rb
+++ lib/faker/omniauth.rb
@@ -3,6 +3,18 @@
 # end
 module Faker
   class Omniauth < Base
+    require 'pry'
+    attr_reader :name
 
+    def initialize
+      @name = Name.name
+      @first_name = name.split(' ').first
+      @last_name = name.split(' ').last
+    end
+
+    def self.google
+      auth = Omniauth.new()
+      
+    end
   end
 end
