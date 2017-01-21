7fe8a6470a0b7d9fdd9bcbfd14f217ea80366c54
PlanetEfficacy <jspevack@gmail.com>
2017-01-20 17:18:55 -0700

    Add google omniauth hash provider, uid, info, credentials

--- lib/faker/omniauth.rb
+++ lib/faker/omniauth.rb
@@ -4,7 +4,10 @@
 module Faker
   class Omniauth < Base
     require 'pry'
-    attr_reader :name
+    require 'time'
+    attr_reader :name,
+                :first_name,
+                :last_name
 
     def initialize
       @name = Name.name
@@ -14,7 +17,37 @@ module Faker
 
     def self.google
       auth = Omniauth.new()
-      
+      {
+        provider:     "google_oauth2",
+        uid:          Number.number(9),
+        info:         {
+          name:       auth.name,
+          first_name: auth.first_name,
+          last_name:  auth.last_name,
+          email:      "#{auth.first_name.downcase}@example.com",
+          image:      Placeholdit.image
+        },
+        credentials:  {
+          token:          Crypto.md5,
+          refresh_token:  Crypto.md5,
+          expires_at:     one_hour_from_now.to_i,
+          expires:        true
+        },
+        extra:        {
+          raw_info:     {
+            name: auth.name,
+            given_name: auth.first_name,
+            family_name: auth.last_name,
+            sub: Number.number(9)
+          }
+        }
+      }
     end
+
+    private
+
+      def self.one_hour_from_now
+        Object::Time.now + (60 * 60)
+      end
   end
 end
