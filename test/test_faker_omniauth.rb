5cc7f04df85ada3c0c2e63dc3a7715266a687293
PlanetEfficacy <jspevack@gmail.com>
2017-01-20 18:58:26 -0700

    Refactor google omniauth test

--- test/test_faker_omniauth.rb
+++ test/test_faker_omniauth.rb
@@ -7,42 +7,56 @@ class TestFakerInternetOmniauth < Test::Unit::TestCase
   end
 
   def test_omniauth_google
-    auth = @tester.google
+    auth            = @tester.google
+    provider        = auth[:provider]
+    uid             = auth[:uid]
+    info            = auth[:info]
+    credentials     = auth[:credentials]
+    extra_raw_info  = auth[:extra][:raw_info]
+    id_info         = auth[:id_info]
 
-    assert_equal "google_oauth2", auth[:provider]
-    assert_equal 9, auth[:uid].length
-    assert_equal 2, auth[:info][:name].split(' ').count
-    assert_equal "#{auth[:info][:first_name].downcase}@example.com", auth[:info][:email]
-    assert_equal auth[:info][:name].split(' ').first, auth[:info][:first_name]
-    assert_equal auth[:info][:name].split(' ').last, auth[:info][:last_name]
-    assert_instance_of String, auth[:info][:image]
-    assert_instance_of String, auth[:credentials][:token]
-    assert_instance_of String, auth[:credentials][:refresh_token]
-    assert_instance_of Fixnum, auth[:credentials][:expires_at]
-    assert_equal true, auth[:credentials][:expires]
-    assert_equal 9, auth[:extra][:raw_info][:sub].length
-    assert_equal auth[:info][:email], auth[:extra][:raw_info][:email]
-    assert_equal true, auth[:extra][:raw_info][:email_verified]
-    assert_equal auth[:info][:name], auth[:extra][:raw_info][:name]
-    assert_equal auth[:info][:first_name], auth[:extra][:raw_info][:given_name]
-    assert_equal auth[:info][:last_name], auth[:extra][:raw_info][:family_name]
-    assert_equal "https://plus.google.com/#{auth[:uid]}", auth[:extra][:raw_info][:profile]
-    assert_instance_of String, auth[:extra][:raw_info][:picture]
-    assert ["female", "male"].include?(auth[:extra][:raw_info][:gender])
-    assert_instance_of String, auth[:extra][:raw_info][:birthday]
-    assert_equal "en", auth[:extra][:raw_info][:local]
-    assert_instance_of String, auth[:extra][:raw_info][:hd]
-    assert_equal "accounts.google.com", auth[:id_info]["iss"]
-    assert_instance_of String, auth[:id_info]["at_hash"]
-    assert_instance_of String, auth[:id_info]["email_verified"]
-    assert_equal 28, auth[:id_info]["sub"].length
-    assert_equal "APP_ID", auth[:id_info]["azp"]
-    assert_equal auth[:info][:email], auth[:id_info]["email"]
-    assert_equal "APP_ID", auth[:id_info]["aud"]
-    assert_instance_of String, auth[:id_info]["iat"]
-    assert_instance_of String, auth[:id_info]["exp"]
+    assert_equal "google_oauth2", provider
+    assert_equal 9, uid.length
+    assert_equal 2, info[:name].split(' ').count
+    assert_equal "#{info[:first_name].downcase}@example.com", info[:email]
+    assert_equal info[:name].split(' ').first, info[:first_name]
+    assert_equal info[:name].split(' ').last, info[:last_name]
+    assert_instance_of String, info[:image]
+    assert_instance_of String, credentials[:token]
+    assert_instance_of String, credentials[:refresh_token]
+    assert_instance_of Fixnum, credentials[:expires_at]
+    assert_equal true, credentials[:expires]
+    assert_equal 9, extra_raw_info[:sub].length
+    assert_equal info[:email], extra_raw_info[:email]
+    assert_equal true, extra_raw_info[:email_verified]
+    assert_equal info[:name], extra_raw_info[:name]
+    assert_equal info[:first_name], extra_raw_info[:given_name]
+    assert_equal info[:last_name], extra_raw_info[:family_name]
+    assert_equal "https://plus.google.com/#{auth[:uid]}", extra_raw_info[:profile]
+    assert_instance_of String, extra_raw_info[:picture]
+    assert ["female", "male"].include?(extra_raw_info[:gender])
+    assert_instance_of String, extra_raw_info[:birthday]
+    assert_equal "en", extra_raw_info[:local]
+    assert_instance_of String, extra_raw_info[:hd]
+    assert_equal "accounts.google.com", id_info["iss"]
+    assert_instance_of String, id_info["at_hash"]
+    assert_instance_of String, id_info["email_verified"]
+    assert_equal 28, id_info["sub"].length
+    assert_equal "APP_ID", id_info["azp"]
+    assert_equal info[:email], id_info["email"]
+    assert_equal "APP_ID", id_info["aud"]
+    assert_instance_of String, id_info["iat"]
+    assert_instance_of String, id_info["exp"]
     assert_equal "https://www.google.com/accounts/o8/id?id=#{auth[:uid]}",
-      auth[:id_info]["openid_id"]
+      id_info["openid_id"]
+  end
+
+  def test_omniauth_facebook
+    auth = @tester.facebook
+
+    assert_equal "facebook", auth[:provider]
+    assert_equal 7, auth[:uid].length
+    # assert_equal auth[:]
   end
 
 end
