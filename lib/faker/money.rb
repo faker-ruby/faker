b1e1c8206b382e76d4cffe768c3dd8e4e5f72652
Gahee Heo <ghbooth12@gmail.com>
2017-01-24 17:17:24 -0600

    Edit money_with_zero_padding and Add test code for Money class

--- lib/faker/money.rb
+++ lib/faker/money.rb
@@ -3,23 +3,22 @@ module Faker
     flexible :money
 
     class << self
-      def money(min=0, max=100, decimal_place=2)
-        zero_decimals = [:ko, :ja, :vi]
-        decimal_place = 0 if zero_decimals.include?(Faker::Config.locale)
+      @@zero_decimals = ["ko", "ja", "vi"]
 
+      def money(min=0, max=100, decimal_place=2)
+        decimal_place = 0 if @@zero_decimals.include?(Faker::Config.locale.to_s)
         Random.new.rand(min..max.to_f).round(decimal_place)
       end
 
-      def zero_padding(min=0, max=100, decimal_place=2)
+      def money_with_zero_padding(min=0, max=100, decimal_place=2)
         amount = money(min, max, decimal_place)
-        return amount if decimal_place < 1
-
         money_parts = amount.to_s.split('.')
+        return amount.to_s if money_parts.length < 2
         money_parts[0] + "." + money_parts[-1].ljust(decimal_place, "0")
       end
 
       def money_with_symbol(min=0, max=100, decimal_place=2)
-        "#{fetch('currency.symbol')}#{zero_padding(min, max, decimal_place)}"
+        "#{fetch('currency.symbol')}#{money_with_zero_padding(min, max, decimal_place)}"
       end
 
       def money_without_cents(min=0, max=100)
