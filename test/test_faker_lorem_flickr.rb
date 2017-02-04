2dd18fe4cd9bbcb8e3481e60e4171d035bc2b2ae
Steven Atkinson <mrstebouk@gmail.com>
2017-02-04 13:31:17 +0000

    Added tests for the LoremFlickr class.

---
+++ test/test_faker_lorem_flickr.rb
@@ -0,0 +1,25 @@
+require File.dirname(__FILE__) + '/test_helper.rb'
+
+class TestFakerLoremFlickr < Test::Unit::TestCase
+
+  def setup
+    @tester = Faker::LoremFlickr
+  end
+
+  def test_image
+    assert @tester.image == 'http://loremflickr.com/300/300'
+  end
+
+  def test_image_with_size
+    assert @tester.image('50x60') == 'http://loremflickr.com/50/60'
+  end
+
+  def test_image_with_is_gray
+    assert @tester.image('50x60', true) == 'http://loremflickr.com/g/50/60'
+  end
+
+  def test_image_with_search_term
+    assert @tester.image('50x60', false, 'faker') == 'http://loremflickr.com/50/60/faker'
+  end
+
+end
