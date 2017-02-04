dfdff673083ef6a2048b2f75f9ed2012a2b013f4
Steven Atkinson <mrstebouk@gmail.com>
2017-02-04 14:23:26 +0000

    Extracted building out URL to separate method.

--- lib/faker/lorem_flickr.rb
+++ lib/faker/lorem_flickr.rb
@@ -2,14 +2,21 @@ module Faker
   class LoremFlickr < Base
     class << self
       def image(size = '300x300', is_gray = false, search_terms = [], match_all = false)
+        format = 'g' if is_gray
+        build_url(size, format, search_terms, match_all)
+      end
+
+      private
+
+      def build_url(size, format, search_terms, match_all)
         raise ArgumentError, 'Size should be specified in format 300x300' unless size.match(/^[0-9]+x[0-9]+$/)
 
         url_parts = ['http://loremflickr.com']
-        url_parts << 'g' if is_gray
+        url_parts << format
         url_parts += size.split('x')
         url_parts << search_terms.compact.join(',') if search_terms.any?
         url_parts << 'all' if match_all
-        url_parts.join('/')
+        url_parts.compact.join('/')
       end
     end
   end
