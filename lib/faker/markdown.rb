d1b62eb97f498f88881256bbd0b8f2d6db3e046f
Michael Scherer <michael.c.scherer@gmail.com>
2017-02-26 12:47:35 -0700

    Create emphasis method

--- lib/faker/markdown.rb
+++ lib/faker/markdown.rb
@@ -6,6 +6,15 @@ module Faker
         "#{fetch('markdown.headers')} #{Faker::Lorem.word.capitalize}"
       end
 
+      def emphasis
+        paragraph = Faker::Lorem.paragraph(3)
+        words = paragraph.split(' ')
+        position = rand(0..words.length - 1)
+        formatting = fetch('markdown.emphasis')
+        words[position] = "#{formatting}#{words[position]}#{formatting}"
+        words.join(' ')
+      end
+
     end
   end
 end
--- test/test_faker_markdown.rb
+++ test/test_faker_markdown.rb
@@ -9,4 +9,8 @@ class TestFakerMarkdown < Test::Unit::TestCase
     assert @tester.headers.match(/\W \w+/)
   end
 
+  def test_emphasis
+    assert @tester.emphasis.match(/\W \w+/)
+  end
+
 end
