b7f434354960c07f9e012fee5d1b45c5ab5eeacf
Nathan Herrera <nathanherrera@gmail.com>
2017-03-02 18:37:48 -0800

    Clean up tests and commented code

--- lib/faker/coffee.rb
+++ lib/faker/coffee.rb
@@ -9,8 +9,6 @@ module Faker
 
       def origin
         country = fetch('coffee.country')
-        # search_country = country.downcase
-        # search_country.split(' ').join('_') if search_country.split(' ').length > 1
         region = fetch("coffee.regions.#{search_format(country)}")
         "#{region}, #{country}"
       end
--- test/test_faker_coffee.rb
+++ test/test_faker_coffee.rb
@@ -14,14 +14,12 @@ class TestFakerCoffee < Test::Unit::TestCase
   end
 
   def test_origin
-    # test for valid string formatting, capture output matched values
-    my_origin = @tester.origin
-    assert origin = my_origin.match(/\A(?<region>([[:alnum:]]+'?-?.?,?\s?){1,5}), (?<country>([[:alnum:]]+\s?){1,5})\z/)
-    # test for valid region, country pair
-    region, country = origin[1], origin[2]
-    # sanitize country for snake case/ lower case
+    assert origin = @tester.origin.match(/\A(?<region>([[:alnum:]]+'?-?.?,?\s?){1,5}), (?<country>([[:alnum:]]+\s?){1,5})\z/)
+
+    region, country = origin[:region], origin[:country]
     search_format_country = country.split.length > 1 ? country.downcase.split.join('_') : country.downcase
     regions = Faker::Base.fetch_all("coffee.regions.#{search_format_country}")
+
     assert @countries.include?(country)
     assert regions.include?(region)
   end
@@ -29,8 +27,10 @@ class TestFakerCoffee < Test::Unit::TestCase
   def test_notes
     assert notes = @tester.notes
                    .match(/\A(?<intensifier>[\s\w-]+), (?<body>[\s\w-]+), (?<f1>[!\s\w-]+), (?<f2>[!\s\w-]+), (?<f3>[!\s\w-]+)\z/)
+
     assert @intensifiers.include?(notes[:intensifier])
     assert @body_descriptors.include?(notes[:body])
+
     [notes[:f1], notes[:f2], notes[:f3]].each do |flavor|
       assert @flavor_descriptors.include?(flavor)
     end
@@ -38,11 +38,13 @@ class TestFakerCoffee < Test::Unit::TestCase
 
   def test_variety
     assert @tester.variety.match(/\w+\.?/)
+
     assert @varieties.include?(@tester.variety)
   end
 
   def test_blend_name
     assert blend = @tester.blend_name.match(/(([[:alnum:]]+(-?\w*)*('?\w+)?) ?){1,}/)
+
     blend[0].split.each do |word|
       assert @name_words.include?(word)
     end
