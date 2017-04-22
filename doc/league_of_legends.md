8ef77ea0bab9d2a84745e785f6e283a2e96c2f97
DonkeyFish456 <stancalderon@gmail.com>
2017-04-22 09:23:12 -0700

    Adds League of Legends summoner spells, masteries and rank

--- doc/league_of_legends.md
+++ doc/league_of_legends.md
@@ -7,4 +7,9 @@ Faker::LeagueOfLegends.location #=> "Demacia"
 
 Faker::LeagueOfLegends.quote #=> "Purge the unjust."
 
+Faker::LeagueOfLegends.summoner_spell #=> "Flash"
+
+Faker::LeagueOfLegends.masteries #=> "Double Edged Sword"
+
+Faker::LeagueOfLegends.rank #=> "Bronze V"
 ```
--- lib/faker/league_of_legends.rb
+++ lib/faker/league_of_legends.rb
@@ -12,6 +12,18 @@ module Faker
       def quote
         fetch('league_of_legends.quote')
       end
+
+      def summoner_spell
+        fetch('league_of_legends.summoner_spell')
+      end
+
+      def masteries
+        fetch('league_of_legends.masteries')
+      end
+
+      def rank
+        fetch('league_of_legends.rank')
+      end
     end
   end
 end
