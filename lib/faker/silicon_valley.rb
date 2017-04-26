574321a203edcd61b1db4b6a91b47e3f4f8d41cf
Romain <romainmorlevat@gmail.com>
2017-04-26 22:39:53 +0200

    refactored to put data in locale

--- lib/faker/silicon_valley.rb
+++ lib/faker/silicon_valley.rb
@@ -2,40 +2,15 @@ module Faker
   class SiliconValley < Base
     class << self
       def character
-        sample(characters)
+        fetch('silicon_valley.character')
       end
 
       def company
-        sample(companies)
+        fetch('silicon_valley.company')
       end
 
       def quote
-        sample(quotes)
-      end
-
-      def characters
-        ['Richard Hendricks', 'Erlich Bachman', 'Nelson « Big Head » Bighetti',
-         'Bertram Gilfoyle', 'Dinesh Chugtai', 'Donald « Jared » Dunn',
-         'Monica Hall', 'Gavin Belson', 'Laurie Bream', 'Peter Gregory',
-         'Jian-Yang', 'Jack Barker', 'Russ Hanneman', 'Ron LaFlamme',
-         'Denpok', 'Pete Monahan']
-      end
-
-      def companies
-        ['Pied Piper', 'Hooli', 'Raviga Capital', 'End Frame', 'Bachmanity']
-      end
-
-      def quotes
-        ["I got a spot on my pants, kind of like I pissed myself like a big baby.",
-         "What\'s happening? I\'m sorry, what? Mr. Car?",
-         "You just brought piss to a sh*t fight!",
-         "Your muffins smell like s**t and so do your ideas.",
-         "One of you is one of the least attractive people I\'ve ever met and I\'m not going to say which one.",
-         "Your logo looks like a sideways vagina. I find that to be racist. Don't you?",
-         "It looks like a guy sucking a dick, with another dick tucked behind his ear for later. Like a snack dick.",
-         "We have to act like any animal that\'s been cornered, acting erratically and blindly lashing out at anything around us",
-         "I don\'t know about you people, but I don\'t want to live in a world where someone else makes the world a better place better than we do.",
-         "What? Revenue? No, no, no, no, no. No."]
+        fetch('silicon_valley.quote')
       end
     end
   end
--- test/test_faker_silicon_valley.rb
+++ test/test_faker_silicon_valley.rb
@@ -2,37 +2,18 @@ require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
 
 class TestFakerSiliconValley < Test::Unit::TestCase
   def setup
-    @characters = Faker::SiliconValley.characters
-    @companies = Faker::SiliconValley.companies
-    @quotes = Faker::SiliconValley.quotes
-  end
-
-  def test_strings
-    assert @characters.size == 16
-    @characters.each do |character|
-      refute character.to_s.empty?
-    end
-
-    assert @companies.size == 5
-    @companies.each do |company|
-      refute company.to_s.empty?
-    end
-
-    assert @quotes.size == 10
-    @quotes.each do |quote|
-      refute quote.to_s.empty?
-    end
+    @silicon_valley = Faker::SiliconValley
   end
 
   def test_character
-    assert Faker::SiliconValley.character.match(/\w+/)
+    assert @silicon_valley.character.match(/\w+/)
   end
 
   def test_company
-    assert Faker::SiliconValley.company.match(/\w+/)
+    assert @silicon_valley.company.match(/\w+/)
   end
 
   def test_quote
-    assert Faker::SiliconValley.quote.match(/\w+/)
+    assert @silicon_valley.quote.match(/\w+/)
   end
 end
