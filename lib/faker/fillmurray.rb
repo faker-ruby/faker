cc7cb48baf001fc7c67e1007d98ce4c562c731ab
Andy Zhao <andyzhao.zhao@gmail.com>
2017-05-09 19:42:26 -0400

    fixed fillmurray default values

--- lib/faker/fillmurray.rb
+++ lib/faker/fillmurray.rb
@@ -2,7 +2,7 @@ module Faker
   class Fillmurray < Base
     class << self
 
-      def image(grayscale = false, width = 200, height = 200)
+      def image(grayscale = false, width = '200', height = '200')
         raise ArgumentError, "Width should be a number" unless width.to_s.match(/^\d+$/)
         raise ArgumentError, "Height should be a number" unless height.to_s.match(/^\d+$/)
         raise ArgumentError, "Grayscale should be a boolean" unless [true, false].include?(grayscale)
