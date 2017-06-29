fa774e657f31fa1f196b17cdd73e41912ff42206
Azuan <alienxp03@gmail.com>
2017-06-29 16:26:50 +0800

    Add en-MS.yml

---
+++ test/test_en_ms_locale.rb
@@ -0,0 +1,37 @@
+require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
+
+class TestEnMsLocale < Test::Unit::TestCase
+  def setup
+    @previous_locale = Faker::Config.locale
+    Faker::Config.locale = 'en-MS'
+  end
+
+  def teardown
+    Faker::Config.locale = @previous_locale
+  end
+
+  def test_en_ms_methods
+    assert Faker::Name.prefix.is_a? String
+    assert Faker::Name.malay_male_first_name.is_a? String
+    assert Faker::Name.malay_female_first_name.is_a? String
+    assert Faker::Name.chinese_male_first_name.is_a? String
+    assert Faker::Name.chinese_male_last_name.is_a? String
+    assert Faker::Name.chinese_female_first_name.is_a? String
+    assert Faker::Name.first_name.is_a? String
+    assert Faker::Name.name.is_a? String
+    assert Faker::Address.city_name.is_a? String
+    assert Faker::Address.city.is_a? String
+    assert Faker::Address.province.is_a? String
+    assert Faker::Address.postcode.is_a? String
+    assert Faker::Address.building_number.is_a? String
+    assert Faker::Address.street_prefix.is_a? String
+    assert Faker::Address.street_title.is_a? String
+    assert Faker::Address.street_name.is_a? String
+    assert Faker::Address.street_address.is_a? String
+    assert Faker::PhoneNumber.cell_phone.is_a? String
+  end
+
+  def test_en_ms_default_country
+    assert_equal('Malaysia', Faker::Address.default_country)
+  end
+end
