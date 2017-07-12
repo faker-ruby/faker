e6c47a941e6087c37d437915e9cf64061464aad3
andrewdwooten <andrewdwooten@icloud.com>
2017-07-12 07:24:52 -0600

    Add greek_organization method to University (#932)

--- lib/faker/university.rb
+++ lib/faker/university.rb
@@ -15,6 +15,18 @@ module Faker
         fetch('university.suffix')
       end
 
+      def greek_organization
+        organization = ''
+        3.times do |e|
+          organization = organization + sample(greek_alphabet)
+        end
+        organization
+      end
+
+      def greek_alphabet
+        ['Α', 'B', 'Γ', 'Δ', 'E', 'Z', 'H', 'Θ', '|', 'K', 'Λ', 'M', 'N', 'Ξ',
+         'O', 'Π', 'P', 'Σ', 'T', 'Y', 'Φ', 'X', 'Ψ', 'Ω']
+      end
     end
   end
 end
