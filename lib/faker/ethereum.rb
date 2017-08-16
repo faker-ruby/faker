0e97def9c9fa73a4f79d687133e80b28ba083b7b
TC Wu <tc@hellogold.com>
2017-08-16 10:36:03 +0800

    added test

---
+++ lib/faker/ethereum.rb
@@ -0,0 +1,14 @@
+module Faker
+  class Ethereum < Base
+    class << self
+      
+      def address
+        hex_alphabet = '0123456789abcdef'
+        var = "0x"
+        (1..40).each { |e| var << sample(shuffle(hex_alphabet.split(''))) }
+        var
+      end
+
+    end
+  end
+end
