4644f1688a87ea897b5024bc00a4c5325fb95655
Anil Kulkarni <anil.kulkarni@outlook.com>
2017-09-28 10:34:03 -0700

    Update the image method in LoremPixel to take a hash

--- doc/lorem_pixel.md
+++ doc/lorem_pixel.md
@@ -3,15 +3,15 @@
 ```ruby
 Faker::LoremPixel.image #=> "http://lorempixel.com/300/300"
 
-Faker::LoremPixel.image("50x60") #=> "http://lorempixel.com/50/60"
+Faker::LoremPixel.image(size: "50x60") #=> "http://lorempixel.com/50/60"
 
-Faker::LoremPixel.image("50x60", true) #=> "http://lorempixel.com/g/50/60"
+Faker::LoremPixel.image(size: "50x60", grayscale: true) #=> "http://lorempixel.com/g/50/60"
 
-Faker::LoremPixel.image("50x60", false, 'sports') #=> "http://lorempixel.com/50/60/sports"
+Faker::LoremPixel.image(size: "50x60", category: 'sports') #=> "http://lorempixel.com/50/60/sports"
 
-Faker::LoremPixel.image("50x60", false, 'sports', 3) #=> "http://lorempixel.com/50/60/sports/3"
+Faker::LoremPixel.image(size: "50x60", category: 'sports', id: 3) #=> "http://lorempixel.com/50/60/sports/3"
 
-Faker::LoremPixel.image("50x60", false, 'sports', 3, 'Dummy-text') #=> "http://lorempixel.com/50/60/sports/3/Dummy-text"
+Faker::LoremPixel.image(size: "50x60", category: 'sports', id: 3, text: 'Dummy-text') #=> "http://lorempixel.com/50/60/sports/3/Dummy-text"
 
-Faker::LoremPixel.image("50x60", false, 'sports', nil, 'Dummy-text') #=> "http://lorempixel.com/50/60/sports/Dummy-text"
+Faker::LoremPixel.image("50x60", category: 'sports', text: 'Dummy-text') #=> "http://lorempixel.com/50/60/sports/Dummy-text"
 ```
--- lib/faker/lorem_pixel.rb
+++ lib/faker/lorem_pixel.rb
@@ -3,7 +3,20 @@ module Faker
     class << self
       SUPPORTED_CATEGORIES = %w(abstract animals business cats city food nightlife fashion people nature sports technics transport)
 
-      def image(size = '300x300', is_gray = false, category = nil, number = nil, text = nil)
+      def image(*args)
+        if args.length == 1 && args[0].is_a?(Hash)
+          options = args[0]
+        else
+          # LoremPixel used to have positional arguments. This is now deprecated in favor of named arguments
+          # However, in order to prevent breaking existing applications, shim the arguments into a hash
+          positional_args = [:size, :grayscale, :category, :id, :text]
+          options = Hash[positional_args.zip(args)].compact!
+        end
+        create_image(options)
+      end
+
+      private
+      def create_image(size: '300x300', grayscale: false, category: nil, id: nil, text: nil)
         raise ArgumentError, 'Size should be specified in format 300x300' unless size.match(/^[0-9]+x[0-9]+$/)
         raise ArgumentError, "Supported categories are #{SUPPORTED_CATEGORIES.join(', ')}" unless category.nil? || SUPPORTED_CATEGORIES.include?(category)
         raise ArgumentError, 'Category required when number is passed' if !number.nil? && category.nil?
@@ -11,9 +24,9 @@ module Faker
         raise ArgumentError, 'Category and number must be passed when text is passed' if !text.nil? && number.nil? && category.nil?
 
         url_parts = ['http://lorempixel.com']
-        url_parts << 'g' if is_gray
+        url_parts << 'g' if grayscale
         url_parts += size.split('x')
-        url_parts += [category, number, text].compact
+        url_parts += [category, id, text].compact
         url_parts.join('/')
       end
     end
