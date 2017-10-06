1bd2d89d4346dbbab2d2b1911d15449a781b1e80
Peter Doherty <pdoherty926@gmail.com>
2017-10-05 23:52:18 -0400

    introduce Aqua Teen Hunger Force characters

---
+++ test/test_faker_aqua_teen_hunger_force.rb
@@ -0,0 +1,13 @@
+require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
+
+ class TestFakerAquaTeenHungerForce < Test::Unit::TestCase
+
+   def setup
+     @tester = Faker::AquaTeenHungerForce
+   end
+
+   def test_character
+     assert @tester.character.match(/\w/)
+   end
+
+ end
