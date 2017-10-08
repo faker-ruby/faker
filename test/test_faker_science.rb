d22ba57b43a3af287fcf93e2e9066f28540d9a1f
Joel Guerra <joelaguerra1@gmail.com>
2017-10-08 11:21:34 -0700

    adds test for Faker:Science

---
+++ test/test_faker_science.rb
@@ -0,0 +1,12 @@
+require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
+
+class TestFakerScience < Test::Unit::TestCase
+  def setup
+    @tester = Faker::Science
+  end
+
+  def test_element
+    assert @tester.element.match(/\w+/)
+  end
+
+end
