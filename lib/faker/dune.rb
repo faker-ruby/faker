11036bc2516439afcce34d219dd01d058f9d582d
Russell Schmidt <russell.schmidt@me.com>
2017-10-21 23:42:09 -0700

    add error checking in event sources, characters not included

--- lib/faker/dune.rb
+++ lib/faker/dune.rb
@@ -1,6 +1,9 @@
 module Faker
   class Dune < Base
     class << self
+      QUOTED_CHARACTERS = %w(translate("faker.characters"))
+      SAYING_SOURCES = %w(translate("faker.sources"))
+
       def character
         fetch('dune.characters')
       end
@@ -17,7 +20,7 @@ module Faker
         return bothify(fetch('dune.quotes.' + fetch('dune.quotes'.keys))) if character === ''
 
         # error check if character not included in dune.yml
-
+        raise ArgumentError, "Characters quoted are #{QUOTED_CHARACTERS.join(', ')}" unless QUOTED_CHARACTERS.include?(character)
         bothify(fetch('dune.quotes.' + character))
       end
 
@@ -25,7 +28,7 @@ module Faker
         return bothify(fetch('dune.sayings.' + fetch('dune.sayings'.keys))) if source === ''
 
         # error check if source not included in dune.yml
-
+        raise ArgumentError, "Sources quoted are #{SAYING_SOURCES.join(', ')}" unless SAYING_SOURCES.include?(source)
         bothify(fetch('dune.sayings.' + source))
       end
     end
