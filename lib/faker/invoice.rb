5e50cc501702f63afdeaf814679b526f76b4a6ba
Onni Hakala <onni@keksi.io>
2017-10-23 00:14:10 +0300

    Add Invoice class to generate bank slip references

---
+++ lib/faker/invoice.rb
@@ -0,0 +1,92 @@
+# encoding: utf-8
+module Faker
+  class Invoice < Base
+    flexible :invoice
+
+    class << self
+      def reference
+      end
+
+      # International bank slip reference https://en.wikipedia.org/wiki/Creditor_Reference
+      def creditor_reference(country_code = 'FI')
+        ref = reference
+
+        'RF' + iban_checksum('RF',ref) + ref
+      end
+
+      # Payment references have some rules in certain countries
+      def reference(country_code = 'FI')
+
+        pattern = fetch("invoice.reference.#{country_code.downcase}.pattern")
+
+        reference = Base.regexify(/#{pattern}/)
+
+        # If reference contains reserved '#' characters we need to calculate check_digits as well
+        check_digit_match = reference.match(/#+/)
+        if check_digit_match
+          # Get the method for selected language
+          check_digit_method = fetch("invoice.reference.#{country_code.downcase}.check_digit_method")
+
+          # Calculate the check digit with matching method name
+          # Trim all '#' from the reference before calculating that
+          check_digit = self.send(check_digit_method,reference.tr('#',''))
+
+          # Make sure that our check digit is as long as all of the '###' we found
+          check_digit = check_digit.to_s.rjust(check_digit_match[0].length, '0')
+
+          # Replace all of the
+          reference.sub!(check_digit_match[0], check_digit)
+        end
+
+        reference
+      end
+
+      private
+
+      # Calculates the mandatory checksum in 3rd and 4th characters in IBAN format
+      # source: https://en.wikipedia.org/wiki/International_Bank_Account_Number#Validating_the_IBAN
+      def iban_checksum(country_code, account)
+        # Converts letters to numbers according the iban rules, A=10..Z=35
+        account_to_number = "#{account}#{country_code}00".upcase.chars.map { |d|
+           d.match(/[A-Z]/) ? (d.ord - 55).to_s : d
+        }.join.to_i
+
+        # This is answer to (iban_to_num + checksum) % 97 == 1
+        checksum = ( 1 - account_to_number ) % 97
+
+        # Use leftpad to make the size always to 2
+        checksum.to_s.rjust(2, '0')
+      end
+
+      # 731 Method
+      # Source: https://wiki.xmldation.com/support/fk/finnish_reference_number
+      def method_731(base)
+        weighted_sum = calculate_weighted_sum(base, [7,3,1])
+        mod10_remainder(weighted_sum)
+      end
+
+      # Norsk Modulus 10 - KIDMOD10
+      def kidmod10(base)
+        weighted_sum = calculate_weighted_sum(base, [1,2])
+        mod10_remainder(weighted_sum)
+      end
+
+      # Calculates weigthed sum
+      #
+      # For example with 12345678, [1,2]
+      # Ref.num. 1 2 3 4 5 6 7 8
+      # Multipl. 1 2 1 2 1 2 1 2
+      # Total 1+ 4+ 3+ 8+ 5+1+2+ 7+1+6 = 38
+      def calculate_weighted_sum(base, weight_factors)
+        base.to_s.reverse.each_char.with_index.map { |digit,index|
+          digit.to_i * weight_factors.at(index % weight_factors.length)
+        }.sum
+      end
+
+      # MOD-10 - remainder
+      def mod10_remainder(number)
+        -number % 10
+      end
+    end
+  end
+end
