7a9fd3e3dcf5b2e8dc04560dc47d262df37ded09
Donnie Propst <donnie@Donnies-MacBook-Pro.local>
2017-10-25 19:07:09 -0400

    meta to include_meta

--- lib/faker/semantic_version.rb
+++ lib/faker/semantic_version.rb
@@ -2,17 +2,18 @@ module Faker
   class SemanticVersion < Base
     class << self
 
-      def version(major=nil, minor=nil, patch=nil, meta=false)
-        major ||= rand(10)
-        minor ||= rand(10)
-        patch ||= rand(20)
+      def version(major = nil, minor = nil, patch = nil, include_meta = false)
+        major ||= rand(0..10)
+        minor ||= rand(0..20)
+        patch ||= rand(0..20)
         metadata = ''
-        if meta
-          metadata = "-#{fetch('semantic_version.meta')}-#{rand(50000)}"
+        if include_meta
+          details = rand(50000)
+          metadata = "-#{fetch('semantic_version.meta')}-#{details}"
         end
         "#{major}.#{minor}.#{patch}#{metadata}"
       end
-
+      
     end
   end
 end
