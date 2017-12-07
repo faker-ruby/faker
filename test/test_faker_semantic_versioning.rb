8641bd88111b72de5a1b365e5e393ef2d5a11ffb
PuZZleDucK <puzzleduck@gmail.com>
2017-12-07 18:18:47 +1100

    add tests for params

--- test/test_faker_semantic_versioning.rb
+++ test/test_faker_semantic_versioning.rb
@@ -9,13 +9,46 @@ class TestFakerSemanticVersioning < Test::Unit::TestCase
   def test_basic_semantic_version
     10.times do |digits|
       test_sem_vers = @tester.semantic_version().split(".")
-      print("\n::: test_sem_vers: #{test_sem_vers}")
       assert test_sem_vers[0].match(/[0-9]{1}/)
       assert test_sem_vers[1].match(/[0-9]{1}/)
       assert test_sem_vers[2].match(/[1-9]{1}/)
     end
-
   end
 
+  def test_major_semantic_version
+    10.times do |digits|
+      test_sem_vers = @tester.semantic_version(1000, 9999).split(".")
+      assert test_sem_vers[0].match(/[0-9]{4}/)
+      assert test_sem_vers[1].match(/[0-9]{1}/)
+      assert test_sem_vers[2].match(/[1-9]{1}/)
+    end
+  end
+
+  def test_minor_semantic_version
+    10.times do |digits|
+      test_sem_vers = @tester.semantic_version(0, 9, 1000, 9999).split(".")
+      assert test_sem_vers[0].match(/[0-9]{1}/)
+      assert test_sem_vers[1].match(/[0-9]{4}/)
+      assert test_sem_vers[2].match(/[1-9]{1}/)
+    end
+  end
+
+  def test_patch_semantic_version
+    10.times do |digits|
+      test_sem_vers = @tester.semantic_version(0, 9, 0, 9, 1000, 9999).split(".")
+      assert test_sem_vers[0].match(/[0-9]{1}/)
+      assert test_sem_vers[1].match(/[0-9]{1}/)
+      assert test_sem_vers[2].match(/[0-9]{4}/)
+    end
+  end
+
+  def test_all_semantic_version
+    10.times do |digits|
+      test_sem_vers = @tester.semantic_version(from_major=1000, to_major=9999, from_minor=1000, to_minor=9999, from_patch=1000, to_patch=9999).split(".")
+      assert test_sem_vers[0].match(/[0-9]{4}/)
+      assert test_sem_vers[1].match(/[0-9]{4}/)
+      assert test_sem_vers[2].match(/[0-9]{4}/)
+    end
+  end
 
 end
