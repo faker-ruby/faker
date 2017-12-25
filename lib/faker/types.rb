4d4252c4e7003c40ec4ae1098a10ffbfd8f0e224
Benjamin Curtis <ben@bencurtis.com>
2017-12-25 00:27:52 -0800

    Remove whitespace

--- lib/faker/types.rb
+++ lib/faker/types.rb
@@ -3,7 +3,7 @@ module Faker
     CHARACTERS = ('0'..'9').to_a + ('a'..'z').to_a
     SIMPLE_TYPES = [:string, :fixnum]
     COMPLEX_TYPES = [:hash, :array]
-    
+
     class << self
       def string(words=1)
         resolved_num = resolve(words)
@@ -13,15 +13,15 @@ module Faker
         word_list = word_list * ((resolved_num / word_list.length) + 1)
         shuffle(word_list)[0, resolved_num].join(" ")
       end
-      
+
       def character
         sample(CHARACTERS)
       end
-      
+
       def integer(from=0, to=100)
         rand(from..to).to_i
       end
-      
+
       def hash(key_count=1)
         Hash.new.tap do |hsh|
           key_count.times do
@@ -29,7 +29,7 @@ module Faker
           end
         end
       end
-      
+
       def complex_hash(key_count=1)
         Hash.new.tap do |hsh|
           key_count.times do
@@ -37,7 +37,7 @@ module Faker
           end
         end
       end
-      
+
       def array(len=1)
         Array.new.tap do |ar|
           len.times do
@@ -45,7 +45,7 @@ module Faker
           end
         end
       end
-      
+
       def random_type
         type_to_use = SIMPLE_TYPES[rand(0..SIMPLE_TYPES.length - 1)]
         case type_to_use
@@ -57,7 +57,7 @@ module Faker
           self.integer
         end
       end
-      
+
       def random_complex_type
         types = SIMPLE_TYPES + COMPLEX_TYPES
         type_to_use = types[rand(0..types.length - 1)]
@@ -74,13 +74,13 @@ module Faker
           self.integer
         end
       end
-      
+
       private
-      
+
       def titleize(word)
         word.split(/(\W)/).map(&:capitalize).join
       end
-      
+
       def resolve(value)
         case value
         when Array then sample(value)
