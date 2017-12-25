f7899d148ae440c4dc91dd5b62f16435fbf759f4
Benjamin Curtis <ben@bencurtis.com>
2017-12-25 00:30:55 -0800

    Ensure enough unique hash keys for key_count
    Try to avoid cases where the same hash key is returned more than once, resulting in the hash having fewer keys than were requested.

--- lib/faker/types.rb
+++ lib/faker/types.rb
@@ -32,8 +32,8 @@ module Faker
 
       def complex_hash(key_count=1)
         Hash.new.tap do |hsh|
-          key_count.times do
-            hsh.merge!({self.string.to_sym => self.random_complex_type})
+          Lorem.words(key_count * 2).uniq.first(key_count).each do |s|
+            hsh.merge!({s.to_sym => self.random_complex_type})
           end
         end
       end
