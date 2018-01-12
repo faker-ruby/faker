21b8678681fc2bb73a29fc70533dd93e323fc570
James Hartman <jameshartman@Jamess-MacBook-Pro.local>
2018-01-12 15:00:06 -0500

    Add Faker::Cosmere

---
+++ doc/cosmere.md
@@ -0,0 +1,23 @@
+# Faker::Cosmere
+
+```ruby
+Faker::Cosmere.aon                    #=> "Rao"
+
+Faker::Cosmere.shard_world            #=> "Yolen"
+
+Faker::Cosmere.shard                  #=> "Ambition"
+
+Faker::Cosmere.surge                  #=> "Progression"
+
+Faker::Cosmere.knight_radiant          #=> "Truthwatcher"
+
+Faker::Cosmere.metal                  #=> "Brass"
+
+Faker::Cosmere.allomancer             #=> "Coinshot"
+
+Faker::Cosmere.feruchemist            #=> "Archivist"
+
+Faker::Cosmere.herald                 #=> "Ishar"
+
+Faker::Cosmere.spren                  #=> "Flamespren"
+```
---
+++ test/test_faker_cosmere.rb
@@ -0,0 +1,43 @@
+require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
+
+class TestFakerCosmere < Test::Unit::TestCase
+  def setup
+    @tester = Faker::Cosmere
+  end
+
+  def test_aon
+    assert @tester.aon.match(/\w+\.?/)
+  end
+
+  def test_shard_world
+    assert @tester.shard_world.match(/\w+\.?/)
+  end
+
+  def test_shard
+    assert @tester.shard.match(/\w+\.?/)
+  end
+
+  def test_surge
+    assert @tester.surge.match(/\w+\.?/)
+  end
+
+  def test_knight_radiant
+    assert @tester.knight_radiant.match(/\w+\.?/)
+  end
+
+  def test_metal
+    assert @tester.metal.match(/\w+\.?/)
+  end
+
+  def test_allomancer
+    assert @tester.allomancer.match(/\w+\.?/)
+  end
+
+  def test_herald
+    assert @tester.herald.match(/\w+\.?/)
+  end
+
+  def test_spren
+    assert @tester.spren.match(/\w+\.?/)
+  end
+end
