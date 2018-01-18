a5e1489cd9edb9533a30b5cb43e389be5103cb06
Geoff Hull <geoffhull03@gmail.com>
2018-01-18 11:54:46 -0800

    Added Community

---
+++ doc/community.md
@@ -0,0 +1,7 @@
+# Faker::Community
+
+```ruby
+Faker::Community.characters #=> "Jeff Winger"
+
+Faker::Community.quote #=> I fear a political career could shine a negative light on my drug dealing."
+```
---
+++ test/test_faker_community.rb
@@ -0,0 +1,17 @@
+require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
+
+ class TestFakerCommunity < Test::Unit::TestCase
+
+   def setup
+     @tester = Faker::Community
+   end
+
+   def test_character
+     assert @tester.characters.match(/\w/)
+   end
+
+   def test_quote
+     assert @tester.quotes.match(/\w/)
+   end
+
+ end
