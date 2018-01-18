3e7207e40573aa04f0ad3546e8fbcd412b364896
Juliane Lima <jujulisan@gmail.com>
2018-01-18 11:18:25 -0200

    better docs for Faker::Food.description

--- lib/faker/food.rb
+++ lib/faker/food.rb
@@ -1,26 +1,33 @@
 module Faker
   class Food < Base
     class << self
+
+      # Retrieves a typical dish from each country
       def dish
         fetch('food.dish')
       end
 
+      # Retrieves a description about some dish
       def description
         fetch('food.descriptions')
       end
 
+      # Retrieves a ingredient for dishes
       def ingredient
         fetch('food.ingredients')
       end
 
+      # Retrieves some random spice
       def spice
         fetch('food.spices')
       end
 
+      # Retrieves cooking measures
       def measurement
         fetch('food.measurement_sizes') + ' ' + fetch('food.measurements')
       end
-      
+
+      # Retrieves metric measures
       def metric_measurement
         fetch('food.metric_measurements')
       end
