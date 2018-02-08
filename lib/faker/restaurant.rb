b113ff5c6781a1ea6772d4098dd2780ea777800f
Zhuo Chen <zhuo.h.chen@gmail.com>
2018-02-08 14:15:34 -0500

    Rename to restaurant.rb. Add cusine, description, reviews, hours methods.

--- lib/faker/restaurant.rb
+++ lib/faker/restaurant.rb
@@ -4,6 +4,30 @@ module Faker
       def name
         fetch('restaurant.name')
       end
+
+      def cuisine
+        fetch('restaurant.cuisine')
+      end
+
+      def description
+        fetch('restaurant.description')
+      end
+
+      def reviews
+        fetch('restaurant.review')
+      end
+
+      def hours
+        fetch_all('restaurant.days').map { |day| "#{day} #{get_am_hour} to #{get_pm_hour}" }
+      end
+
+      def get_am_hour
+        "#{fetch('restaurant.hour')}:#{fetch('restaurant.minute')} AM"
+      end
+
+      def get_pm_hour
+        "#{fetch('restaurant.hour')}:#{fetch('restaurant.minute')} PM"
+      end
     end
   end
 end
