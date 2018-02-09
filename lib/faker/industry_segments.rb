291a39395f6720da1f518fa913af5cdb69f3835e
Chris Desch <cdesch@gmail.com>
2018-02-09 14:45:11 -0500

    Adding Industry Segments Class
    Adding Industry Segments to options based off of the Industry Classification Benchmark

---
+++ lib/faker/industry_segments.rb
@@ -0,0 +1,24 @@
+# encoding: utf-8
+module Faker
+  class IndustrySegments < Base
+    flexible :industry_segments
+
+    class << self
+      def industry
+        fetch('industry_segments.industry')
+      end
+
+      def super_sector
+        fetch('industry_segments.super_sector')
+      end
+
+      def sector
+        fetch('industry_segments.sector')
+      end
+
+      def sub_sector
+        fetch('industry_segments.sub_sector')
+      end
+    end
+  end
+end
