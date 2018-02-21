a9abb2b38da70e459efaf6e7f906ad54b54a4a8f
PuZZleDucK <puzzleduck@gmail.com>
2018-02-21 20:45:33 +1100

    convert asserts into asert_equals

--- test/test_faker_id_number.rb
+++ test/test_faker_id_number.rb
@@ -9,8 +9,8 @@ class TestFakerIdNumber < Test::Unit::TestCase
   def test_valid_ssn
     sample = @tester.valid
     assert sample.length == 11
-    assert sample[3] == "-"
-    assert sample[6] == "-"
+    assert_equal "-", sample[3]
+    assert_equal "-", sample[6]
     assert sample[0..2].split().map{:to_i}.all?{:is_digit?}
     assert sample[4..5].split().map{:to_i}.all?{:is_digit?}
     assert sample[7..9].split().map{:to_i}.all?{:is_digit?}
@@ -19,8 +19,8 @@ class TestFakerIdNumber < Test::Unit::TestCase
   def test_invalid_ssn
     sample = @tester.invalid
     assert sample.length == 11
-    assert sample[3] == "-"
-    assert sample[6] == "-"
+    assert_equal "-", sample[3]
+    assert_equal "-", sample[6]
     assert sample[0..2].split().map{:to_i}.all?{:is_digit?}
     assert sample[4..5].split().map{:to_i}.all?{:is_digit?}
     assert sample[7..9].split().map{:to_i}.all?{:is_digit?}
