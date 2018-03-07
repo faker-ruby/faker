337eaa2989813ff73264db2a36ed817365c40786
Mohamed Hashi <maxamedhashi@gmail.com>
2018-03-07 14:06:48 +0100

    fix(currency-test): add whitespace in name regex
    The currency "Kuwaiti Dinar" was not matched by previous regex.

--- test/test_faker_currency.rb
+++ test/test_faker_currency.rb
@@ -6,7 +6,7 @@ class TestFakerCurrency < Test::Unit::TestCase
   end
 
   def test_name
-    assert @tester.name.match(/[\w\']+/)
+    assert @tester.name.match(/[\w\' ]+/)
   end
 
   def test_code
