4a0a88fa3c779469fb03e634dceb031a1be6e5af
Matheus Graciano <matheus.andrade@webedia-group.com>
2018-05-15 16:25:35 -0300

    better tests for Faker::Source

--- test/test_faker_source.rb
+++ test/test_faker_source.rb
@@ -13,7 +13,27 @@ class TestFakerSource < Test::Unit::TestCase
     assert_instance_of String, @tester.print
   end
 
+  def test_print_javascript
+    assert_equal "console.log('some string');", @tester.print(lang: :javascript)
+  end
+
+  def test_print_another_string
+    assert_equal "print 'another string'", @tester.print(str: 'another string')
+  end
+
+  def test_print_invalid_lang
+    assert_raise(I18n::MissingTranslationData) { @tester.print(lang: :js) }
+  end
+
   def test_print_1_to_10
     assert_instance_of String, @tester.print_1_to_10
+  end
+
+  def test_print_1_to_10_javascript
+    assert_match 'console.log(i);', @tester.print_1_to_10(:javascript)
+  end
+
+  def test_print_1_to_10_matches
+    assert_match 'print i', @tester.print_1_to_10
   end
 end
