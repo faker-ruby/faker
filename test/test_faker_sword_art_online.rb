fe282ac45ff95e1d34cc263f1826cb5783020803
Luke Chambers <lukenatchambers@gmail.com>
2018-05-20 12:03:38 -0600

    Create tests for Sword Art Online

---
+++ test/test_faker_sword_art_online.rb
@@ -0,0 +1,28 @@
+require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
+
+class TestFakerSwordArtOnline < Test::Unit::TestCase
+  def setup
+    @tester = Faker::SwordArtOnline
+  end
+
+  def test_real_name
+    assert @tester.real_name.match(/\w+\.?/)
+  end
+
+  def test_game_name
+    assert @tester.game_name.match(/\w+\.?/)
+  end
+
+  def test_location
+    assert @tester.location.match(/\w+\.?/)
+  end
+
+  def test_item
+    assert @tester.item.match(/\w+\.?/)
+  end
+
+  def test_quest
+    assert @tester.quest.match(/\w+\.?/)
+  end
+
+end
