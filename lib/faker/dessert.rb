ac7c9c71bf307997bc7765cbb3f43558199f6f4c
SusiIrwin <susi.irwin@gmail.com>
2018-05-20 07:52:52 -0600

    Updates dessert class and tests

--- lib/faker/dessert.rb
+++ lib/faker/dessert.rb
@@ -2,18 +2,17 @@ module Faker
   class Dessert < Base
     flexible :dessert
 
-    class << self
-      def variety
-        fetch('dessert.variety')
-      end
-
-      def topping
-        fetch('dessert.topping')
-      end
-
-      def flavor
-        fetch('dessert.flavor')
-      end
+    def variety
+      fetch('dessert.variety')
     end
+
+    def topping
+      fetch('dessert.topping')
+    end
+
+    def flavor
+      fetch('dessert.flavor')
+    end
+    
   end
 end
