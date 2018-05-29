1476b4b693665bad5bb6dbb8b0654332d3417732
Steve Atkinson <steve.atkinson@dootrix.com>
2018-05-29 12:42:39 +0100

    Moved Faker::Auto features into Faker::Vehicle.

--- test/test_faker_vehicle.rb
+++ test/test_faker_vehicle.rb
@@ -26,6 +26,43 @@ class TestFakerVehicle < Test::Unit::TestCase
     assert flexible_key == :vehicle
   end
 
+  def test_mileage
+    mileage = @tester.mileage
+    assert mileage >= 10_000
+    assert mileage < 90_000
+  end
+
+  def test_year
+    year = @tester.year
+    assert year >= 1981
+    assert year <= ::Time.now.year
+  end
+
+  def test_door_count
+    assert @tester.door_count.match(/\d Door/)
+  end
+
+  def test_engine_size
+    assert @tester.engine_size.match(/\d Cylinder Engine/)
+  end
+
+  def test_car_options
+    car_options = @tester.car_options
+    assert car_options.length >= 5
+    assert car_options.length < 10
+  end
+
+  def test_make_and_model
+    make_model = @tester.make_and_model
+    assert @tester.translate('faker.vehicle.makes').include?(make_model.split[0])
+  end
+
+  def test_standard_specs
+    standard_specs = @tester.standard_specs
+    assert standard_specs.length >= 5
+    assert standard_specs.length < 10
+  end
+
   private
 
   def transliterate(character)
