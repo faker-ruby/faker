75871fb15fed6a9d70a9c11b7e5b936088eabdd5
snayrouz <samuel.nayrouz@gmail.com>
2018-06-01 17:17:43 -0600

    Setup WorldCup module and tests

---
+++ lib/faker/world_cup.rb
@@ -0,0 +1,17 @@
+module Faker
+  class FifaWorldCup < Base
+    class << self
+      def team
+        fetch('world_cup.teams')
+      end
+
+      def group
+        fetch('world_cup.groups')
+      end
+
+      def roster
+        fetch('world_cup.rosters')
+      end
+    end
+  end
+end
