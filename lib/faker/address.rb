326bd0b3860f19457a370187cc5d743a53c2d348
igor-starostenko <contactigorstar@gmail.com>
2018-06-02 22:27:48 -0700

    allow zip code to have leading zero

--- lib/faker/address.rb
+++ lib/faker/address.rb
@@ -28,7 +28,9 @@ module Faker
       end
 
       def zip_code(state_abbreviation = '')
-        return bothify(fetch('address.postcode')) if state_abbreviation.empty?
+        if state_abbreviation.empty?
+          return numerify(fetch('address.postcode'), leading_zero: true)
+        end
 
         # provide a zip code that is valid for the state provided
         # see http://www.fincen.gov/forms/files/us_state_territory_zip_codes.pdf
