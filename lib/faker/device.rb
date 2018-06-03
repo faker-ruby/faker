33dae3f1d84c7a4970a5b34cc80051d479e4bcf6
Vitor Oliveira <vbrazo@gmail.com>
2018-06-03 15:37:04 -0300

    Add Faker::Device.serial

--- lib/faker/device.rb
+++ lib/faker/device.rb
@@ -1,6 +1,14 @@
 module Faker
   class Device < Base
     class << self
+      def build_number
+        Faker::Number.between(1, 500)
+      end
+
+      def manufacturer
+        fetch('device.manufacturer')
+      end
+
       def model_name
         fetch('device.model_name')
       end
@@ -9,16 +17,12 @@ module Faker
         fetch('device.platform')
       end
 
+      def serial
+        fetch('device.serial')
+      end
+
       def version
         Faker::Number.between(1, 1000)
-      end
-
-      def build_number
-        Faker::Number.between(1, 500)
-      end
-
-      def manufacturer
-        fetch('device.manufacturer')
       end
     end
   end
