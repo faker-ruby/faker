716fae838053cf6acbe790238b04ed1e5f23c1ee
Vitor <vbrazo@gmail.com>
2018-07-01 12:53:04 -0300

    Fix rubocop violation

--- lib/faker/ethereum.rb
+++ lib/faker/ethereum.rb
@@ -3,7 +3,7 @@ module Faker
     class << self
       def address
         hex_alphabet = '0123456789abcdef'
-        var = "0x"
+        var = '0x'
         (1..40).each { var << sample(shuffle(hex_alphabet.split(''))) }
         var
       end
