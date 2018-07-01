e094cd8a186e8dba24b1d518e4a0f834a5287503
Vitor <vbrazo@gmail.com>
2018-07-01 12:51:46 -0300

    Fix rubocop violations

--- test/test_faker_ethereum.rb
+++ test/test_faker_ethereum.rb
@@ -1,7 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
 
 class TestFakerEthereum < Test::Unit::TestCase
-
   def setup
     @tester = Faker::Ethereum
   end
@@ -9,6 +8,4 @@ class TestFakerEthereum < Test::Unit::TestCase
   def test_address
     assert @tester.address.match(/0x([a-fA-F0-9]{40})/)
   end
-
-  
 end
