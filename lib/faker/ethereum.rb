566053487dac58133757deb1dabd8511f55ccb5f
Vitor <vbrazo@gmail.com>
2018-07-01 12:52:39 -0300

    Update ethereum.rb

--- lib/faker/ethereum.rb
+++ lib/faker/ethereum.rb
@@ -1,14 +1,12 @@
 module Faker
   class Ethereum < Base
     class << self
-      
       def address
         hex_alphabet = '0123456789abcdef'
         var = "0x"
-        (1..40).each { |e| var << sample(shuffle(hex_alphabet.split(''))) }
+        (1..40).each { var << sample(shuffle(hex_alphabet.split(''))) }
         var
       end
-
     end
   end
 end
