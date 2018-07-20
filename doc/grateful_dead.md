3ae6f443e0ca51e34df5f2c1aeb12a2a882f5dbd
wileybaba <wileymckayconte@gmail.com>
2018-07-20 01:37:29 -0400

    grateful_dread

---
+++ doc/grateful_dead.md
@@ -0,0 +1,9 @@
+# Faker::GratefulDead
+
+```ruby
+Faker::GratefulDead.player #=> "Jerry Garcia"
+```
+
+```ruby
+Faker::GratefulDead.song #=> "China Cat Sunflower"
+```
---
+++ test/test_faker_grateful_dead.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: truer
+
+equire_relative 'test_helper'
+
+def setup
+  @tester = Faker::GratefulDead
+end
+
+def test_player
+  assert @tester.player.match(/\w/)
+end
+
+def test_song
+  assert @tester.song.match(/\w/)
+end
