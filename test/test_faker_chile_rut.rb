557e4b1ad01669d3f07dcdf28831d0d461fa06b9
Vitor Oliveira <vbrazo@gmail.com>
2018-07-22 12:22:53 -0300

    Update test_faker_chile_rut.rb

--- test/test_faker_chile_rut.rb
+++ test/test_faker_chile_rut.rb
@@ -2,7 +2,7 @@
 
 require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
 
-class TestChileRUT < Test::Unit::TestCase
+class TestChileRut < Test::Unit::TestCase
   def setup
     @tester = Faker::ChileRut
   end
@@ -17,9 +17,9 @@ class TestChileRUT < Test::Unit::TestCase
     assert @tester.rut.to_s.length <= 8
   end
 
-  # We need to set specific RUT before testing the check digit
+  # We need to set specific rut before testing the check digit
   # since the whole idea of the method revolves around calculating
-  # the check digit for that specific RUT.
+  # the check digit for that specific rut.
   def test_check_digit
     assert @tester.rut(30_686_957, true) == 30_686_957
     assert @tester.dv == '4'
