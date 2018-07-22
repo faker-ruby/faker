6278fa038e8e1919df8c35467eb58ef66601ac43
Vitor Oliveira <vbrazo@gmail.com>
2018-07-22 12:12:10 -0300

    Update test_faker_chile_rut.rb

--- test/test_faker_chile_rut.rb
+++ test/test_faker_chile_rut.rb
@@ -1,26 +1,27 @@
+# frozen_string_literal: true
+
 require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
 
 class TestChileRUT < Test::Unit::TestCase
-
   def setup
-    @tester = Faker::ChileRUT
+    @tester = Faker::ChileRut
   end
 
-  def test_full_RUT
-    assert @tester.full_RUT(6, true) == "6-k"
-    assert @tester.full_RUT(30_686_957, true) == "30686957-4"
+  def test_full_rut
+    assert @tester.full_rut(6, true) == '6-k'
+    assert @tester.full_rut(30_686_957, true) == '30686957-4'
   end
 
-  def test_RUT_length
-    assert @tester.RUT.to_s.length > 0
-    assert @tester.RUT.to_s.length <= 8
+  def test_rut_length
+    assert !@tester.rut.to_s.empty?
+    assert @tester.rut.to_s.length <= 8
   end
 
   # We need to set specific RUT before testing the check digit
   # since the whole idea of the method revolves around calculating
   # the check digit for that specific RUT.
   def test_check_digit
-    assert @tester.RUT(30_686_957, true) == 30686957
-    assert @tester.dv == "4"
+    assert @tester.rut(30_686_957, true) == 30_686_957
+    assert @tester.dv == '4'
   end
 end
