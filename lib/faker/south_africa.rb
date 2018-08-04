a89d2a370e48a41ac0231c9a42de155f077b63b0
Bradley Marques <brad@next45.co>
2018-08-04 10:24:00 +0200

    Phone and cellphone numbers

--- lib/faker/south_africa.rb
+++ lib/faker/south_africa.rb
@@ -10,6 +10,18 @@ module Faker
       def invalid_id_number
         Faker::IDNumber.invalid_south_african_id_number
       end
+
+      def phone_number
+        with_locale 'en-ZA' do
+          Faker::PhoneNumber.phone_number
+        end
+      end
+
+      def cell_phone
+        with_locale 'en-ZA' do
+          Faker::PhoneNumber.cell_phone
+        end
+      end
     end
   end
 end
