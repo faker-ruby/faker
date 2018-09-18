f01ae29db9a98f50708459550150dfc499cc1df1
Scott Wendell Livingstone <scottwlivingstone@gmail.com>
2018-09-18 05:01:31 +0200

    Add Faker::Json (#1156)
    * Add Faker::Json with tests and documentation
    
    * Fix rubo cop erros
    
    add expetions to rubocop.yml
    
    * Fix rubocop erros in test_faker_json.rb
    
    * Update json.md
    
    * Update json.rb
    
    * Update test_faker_json.rb
    
    * Update test_faker_json.rb
    
    * Update json.rb
    
    * Update .rubocop.yml

---
+++ test/test_faker_json.rb
@@ -0,0 +1,27 @@
+require File.expand_path(File.dirname(__FILE__) + '/test_helper.rb')
+
+class TestFakerJson < Test::Unit::TestCase
+  require 'json'
+
+  def setup
+    @tester = Faker::Json
+  end
+
+  def test_shallow_json
+    json = Faker::Json.shallow_json(3, key: 'Name.first_name', value: 'Name.first_name')
+    assert JSON.parse(json).flatten.length.equal?(6)
+  end
+
+  def test_add_depth_to_json
+    json = Faker::Json.shallow_json(3, key: 'Name.first_name', value: 'Name.first_name')
+    json = Faker::Json.add_depth_to_json(json, 3, key: 'Name.first_name', value: 'Name.first_name')
+    assert JSON.parse(json).flatten[1].flatten.length.equal?(6)
+    assert JSON.parse(json).flatten[3].flatten.length.equal?(6)
+    assert JSON.parse(json).flatten[5].flatten.length.equal?(6)
+
+    json = Faker::Json.add_depth_to_json(json, 3, key: 'Name.first_name', value: 'Name.first_name')
+    assert JSON.parse(json).flatten[1].flatten[1].flatten.length.equal?(6)
+    assert JSON.parse(json).flatten[3].flatten[3].flatten.length.equal?(6)
+    assert JSON.parse(json).flatten[5].flatten[5].flatten.length.equal?(6)
+  end
+end
