982849f7127748483c477a115405555ef5fd102e
David Rodríguez <deivid.rodriguez@riseup.net>
2018-09-17 22:18:14 -0300

    Don't force enforce_available_locales
    Respect what each application configures.

--- lib/faker.rb
+++ lib/faker.rb
@@ -9,9 +9,6 @@ end
 require 'i18n'
 require 'set' # Fixes a bug in i18n 0.6.11
 
-if I18n.respond_to?(:enforce_available_locales=)
-  I18n.enforce_available_locales = true
-end
 I18n.load_path += Dir[File.join(mydir, 'locales', '**/*.yml')]
 I18n.reload! if I18n.backend.initialized?
 
