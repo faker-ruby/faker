c40c428d05ae470e9a5b9f63dae6f6776fa3c8fb
J Guthrie <jamie.guthrie@gmail.com>
2018-09-27 00:08:23 +0100

    Added House::furniture, House::appliance and House::room

---
+++ lib/faker/house.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Faker
+  class House < Base
+    class << self
+      def furniture
+        fetch('house.furniture')
+      end
+
+      def appliance
+        fetch('house.appliances')
+      end
+
+      def room
+        fetch('house.rooms')
+      end
+    end
+  end
+end
---
+++ test/test_faker_house.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative 'test_helper'
+
+class TestFakerHouse < Test::Unit::TestCase
+  def setup
+    @tester = Faker::House
+  end
+
+  def test_furniture
+    assert @tester.furniture.match(/\w+/)
+  end
+
+  def test_appliance
+    assert @tester.appliance.match(/\w+/)
+  end
+
+  def test_room
+    assert @tester.room.match(/\w+/)
+  end
+end
