f5fa773dd9288f208f849764736d29e1237b647f
Susan Wright <wright1@ualberta.ca>
2018-10-19 21:13:42 -0600

    Restore 'league_of_legends' and deprecate

--- test/test_determinism.rb
+++ test/test_determinism.rb
@@ -45,7 +45,7 @@ class TestDeterminism < Test::Unit::TestCase
 
   def subclasses
     Faker.constants.delete_if do |subclass|
-      %i[Base Bank Char Base58 ChileRut Config Creature Date Dota ElderScrolls Games GamesHalfLife HeroesOfTheStorm Internet Movies Time VERSION Witcher].include?(subclass)
+      %i[Base Bank Char Base58 ChileRut Config Creature Date Dota ElderScrolls Games GamesHalfLife HeroesOfTheStorm Internet LeagueOfLegends Movies Time VERSION Witcher].include?(subclass)
     end.sort
   end
 
