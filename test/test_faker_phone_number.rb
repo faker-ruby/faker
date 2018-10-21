6a4ee16c11d702100a5e5403d61b343038038536
AmrAdelKhalil <amradelkhalil@gmail.com>
2018-10-22 00:59:14 +0200

    update test cases, refactor code

--- test/test_faker_phone_number.rb
+++ test/test_faker_phone_number.rb
@@ -4,6 +4,7 @@ require_relative 'test_helper'
 class TestFakerPhone < Test::Unit::TestCase
   def setup
     @tester = Faker::PhoneNumber
+    @phone_with_country_code_regex = /\A\+[\s|\d|\-|\(|\)|x|\.]*\z/
   end
 
   def test_country_code
@@ -11,10 +12,10 @@ class TestFakerPhone < Test::Unit::TestCase
   end
 
   def test_phone_number_with_country_code
-    assert @tester.phone_number_with_country_code.match(/\A\+[\s|\d|\-|\(|\)|x|\.]*\z/)
+    assert @tester.phone_number_with_country_code.match(@phone_with_country_code_regex)
   end
 
   def test_cell_phone_with_country_code
-    assert @tester.cell_phone_with_country_code.match(/\A\+[\s|\d|\-|\(|\)|x|\.]*\z/)
+    assert @tester.cell_phone_with_country_code.match(@phone_with_country_code_regex)
   end
 end
