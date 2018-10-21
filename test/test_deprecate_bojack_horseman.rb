4f65f099bf9eb9dcc10fae50e348ad1e92592d5d
Stephen A. Wilson <Stephen-356@hotmail.com>
2018-10-21 18:29:27 -0400

    move BojackHorseman into TvShows namespace

--- test/test_deprecate_bojack_horseman.rb
+++ test/test_deprecate_bojack_horseman.rb
@@ -2,7 +2,7 @@
 
 require_relative 'test_helper'
 
-class TestFakerBojackHorseman < Test::Unit::TestCase
+class TestDeprecateBojackHorseman < Test::Unit::TestCase
   def setup
     @tester = Faker::BojackHorseman
   end
