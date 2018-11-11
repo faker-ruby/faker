08a27abdb6d6bca17fe05dd9bdf51fc06d1a12bf
Vitor Oliveira <vbrazo@gmail.com>
2018-11-11 19:39:38 -0200

    Deprecate ::BackToTheFuture

--- test/deprecate/test_deprecate_back_to_the_future.rb
+++ test/deprecate/test_deprecate_back_to_the_future.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative '../../test_helper'
+require_relative '../test_helper'
 
-class TestFakerBackToTheFuture < Test::Unit::TestCase
+class TestDeprecateBackToTheFuture < Test::Unit::TestCase
   def setup
     @tester = Faker::BackToTheFuture
   end
