adfb39f761ac6847360436317df53ff2c5838eaf
Vitor Oliveira <vbrazo@gmail.com>
2018-11-11 01:14:17 -0200

    Reorganize test folder

---
+++ test/deprecate/test_deprecate_the_fresh_prince_of_bel_air.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+
+class TestDeprecateTheFreshPrinceOfBelAir < Test::Unit::TestCase
+  def setup
+    @tester = Faker::TheFreshPrinceOfBelAir
+  end
+
+  def test_character
+    assert @tester.character.match(/\w+/)
+  end
+
+  def test_celebrity
+    assert @tester.celebrity.match(/\w+/)
+  end
+
+  def test_quote
+    assert @tester.quote.match(/\w+/)
+  end
+end
