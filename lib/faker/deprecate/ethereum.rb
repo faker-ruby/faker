8f91dea4aad0e61b897fd46b1ae118734daf7c5a
Vitor Oliveira <vbrazo@gmail.com>
2018-11-18 02:39:47 -0200

    Deprecate ::Ethereum

---
+++ lib/faker/deprecate/ethereum.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Faker
+  class Blockchain
+    class Ethereum < Base
+      class << self
+        extend Gem::Deprecate
+
+        def address
+          Faker::Blockchain::Ethereum.address
+        end
+
+        deprecate :address, 'Faker::Blockchain::Tezos.address', 2018, 12
+      end
+    end
+  end
+end
