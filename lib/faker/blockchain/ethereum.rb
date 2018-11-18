ff588a5a557439c59ceaafd1e41c6a5a5e767d50
Vitor Oliveira <vbrazo@gmail.com>
2018-11-18 02:51:07 -0200

    Minor fixes

--- lib/faker/blockchain/ethereum.rb
+++ lib/faker/blockchain/ethereum.rb
@@ -1,13 +1,15 @@
 # frozen_string_literal: true
 
 module Faker
-  class Ethereum < Base
-    class << self
-      def address
-        hex_alphabet = '0123456789abcdef'
-        var = +'0x'
-        40.times { var << sample(shuffle(hex_alphabet.split(''))) }
-        var
+  class Blockchain
+    class Ethereum < Base
+      class << self
+        def address
+          hex_alphabet = '0123456789abcdef'
+          var = +'0x'
+          40.times { var << sample(shuffle(hex_alphabet.split(''))) }
+          var
+        end
       end
     end
   end
--- lib/faker/deprecate/ethereum.rb
+++ lib/faker/deprecate/ethereum.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
 module Faker
-  class Blockchain
-    class Ethereum < Base
-      class << self
-        extend Gem::Deprecate
+  class Ethereum < Base
+    class << self
+      extend Gem::Deprecate
 
-        def address
-          Faker::Blockchain::Ethereum.address
-        end
-
-        deprecate :address, 'Faker::Blockchain::Tezos.address', 2018, 12
+      def address
+        Faker::Blockchain::Ethereum.address
       end
+
+      deprecate :address, 'Faker::Blockchain::Tezos.address', 2018, 12
     end
   end
 end
