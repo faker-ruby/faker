e8e0e1c4d2f613e32cf287ab3e9879517b107820
Vitor Oliveira <vbrazo@gmail.com>
2018-11-19 09:59:52 -0200

    Add Blockchain namespace (#1481)
    * Deprecate ::Bitcoin
    
    * Deprecate ::Ethereum
    
    * Deprecate ::Tezos
    
    * Update unreleased_README.md
    
    * Minor fixes

--- test/deprecate/test_deprecate_ethereum.rb
+++ test/deprecate/test_deprecate_ethereum.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
-require_relative '../../test_helper'
+require_relative '../test_helper'
 
-class TestFakerEthereum < Test::Unit::TestCase
+class TestDeprecateEthereum < Test::Unit::TestCase
   def setup
     @tester = Faker::Ethereum
   end
