d8fa7294f5410bc949e75c6d52524ed289b3a7a3
Maxime Lenne <maxime.lenne@gmail.com>
2018-11-25 19:39:46 +0100

    Fr format and translation (#1474)
    * change full_address in fr locale
    
    * add french color translation
    
    * add euro currency symbol
    
    * french translation and format support for compass
    
    * french translation for demografic sex
    
    * french translation for gender
    
    * french translation for measurement
    
    * fix empty line offense
    
    * add symbol for Nepalese Rupee

--- test/test_fr_locale.rb
+++ test/test_fr_locale.rb
@@ -24,8 +24,11 @@ class TestFrLocale < Test::Unit::TestCase
     assert Faker::Address.street_name.is_a? String
     assert Faker::Address.street_address.is_a? String
     assert Faker::Address.default_country.is_a? String
+    assert Faker::Address.full_address.is_a? String
     assert_match(/^[\d]{5}$/, Faker::Address.postcode)
     assert_match(/^\d+$/, Faker::Address.building_number)
+    full_address_regex = /(([a-zA-Z-éÉèÈàÀùÙâÂêÊîÎôÔûÛïÏëËüÜçÇæœ'.]*\s)\d*(\s[a-zA-Z-éÉèÈàÀùÙâÂêÊîÎôÔûÛïÏëËüÜçÇæœ']*)*,)*\d*(\s[a-zA-Z-éÉèÈàÀùÙâÂêÊîÎôÔûÛïÏëËüÜçÇæœ']*)+,\s([\d]{5})\s[a-zA-Z-éÉèÈàÀùÙâÂêÊîÎôÔûÛïÏëËüÜçÇæœ']+/
+    assert_match(full_address_regex, Faker::Address.full_address)
     assert_equal('France', Faker::Address.default_country)
   end
 
@@ -40,6 +43,28 @@ class TestFrLocale < Test::Unit::TestCase
     assert Faker::Book.quote.is_a? String
   end
 
+  def test_fr_color_methods
+    assert Faker::Color.color_name.is_a? String
+  end
+
+  def test_fr_compass_methods
+    direction_pattern = /^\w+(\-\w+){0,2}$/
+    cardinal_pattern = /^\w+$/
+    ordinal_pattern = /^\w+(\-\w+){1}$/
+    half_wind_patern = /^\w+(\-\w+){2}$/
+    letter_pattern = /^[NEOS]{1,3}$/
+
+    assert_match(direction_pattern, Faker::Compass.direction)
+    assert_match(cardinal_pattern, Faker::Compass.cardinal)
+    assert_match(ordinal_pattern, Faker::Compass.ordinal)
+    assert_match(half_wind_patern, Faker::Compass.half_wind)
+
+    assert_match(letter_pattern, Faker::Compass.abbreviation)
+    assert_match(letter_pattern, Faker::Compass.cardinal_abbreviation)
+    assert_match(letter_pattern, Faker::Compass.ordinal_abbreviation)
+    assert_match(letter_pattern, Faker::Compass.half_wind_abbreviation)
+  end
+
   def test_fr_company_methods
     assert Faker::Company.suffix.is_a? String
     assert Faker::Company.buzzword.is_a? String
@@ -47,15 +72,35 @@ class TestFrLocale < Test::Unit::TestCase
     assert Faker::Company.name.is_a? String
   end
 
+  def test_fr_demographic_methods
+    assert %w[Homme Femme].include?(Faker::Demographic.sex)
+  end
+
   def test_fr_internet_methods
     assert Faker::Internet.free_email.is_a? String
     assert Faker::Internet.domain_suffix.is_a? String
+  end
+
+  def test_fr_gender_methods
+    assert %w[Masculin Féminin].include?(Faker::Gender.binary_type)
   end
 
   def test_fr_lorem_methods
     assert Faker::Lorem.word.is_a? String
     assert Faker::Lorem.words(1000)
     assert Faker::Lorem.words(10_000, true)
+  end
+
+  def test_fr_measurement_methods
+    metric_height_pattern = /mètre/
+    metric_length_pattern = /mètre/
+    metric_volume_pattern = /litre|cube/
+    metric_weight_patern = /gramme|tonne/
+
+    assert_match(metric_height_pattern, Faker::Measurement.metric_height)
+    assert_match(metric_length_pattern, Faker::Measurement.metric_length)
+    assert_match(metric_volume_pattern, Faker::Measurement.metric_volume)
+    assert_match(metric_weight_patern, Faker::Measurement.metric_weight)
   end
 
   def test_fr_name_methods
