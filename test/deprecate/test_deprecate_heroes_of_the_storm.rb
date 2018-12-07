266d99773823f9ce290a36056fe329d21b1e451b
Vitor Oliveira <vbrazo@gmail.com>
2018-12-07 11:56:54 -0200

    Add missing Faker::HeroesOfTheStorm tests

---
+++ test/deprecate/test_deprecate_heroes_of_the_storm.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative '../test_helper'
+
+class TestDeprecateHeroesOfTheStorm < Test::Unit::TestCase
+  def setup
+    @tester = Faker::HeroesOfTheStorm
+  end
+
+  def test_class
+    assert @tester.class.match(/\w/)
+  end
+
+  def test_hero
+    assert @tester.hero.match(/\w/)
+  end
+
+  def test_quote
+    assert @tester.quote.match(/\w/)
+  end
+end
