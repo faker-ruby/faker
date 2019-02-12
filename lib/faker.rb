a9b103711a7f8dd5ca972267d68c6d38d8479314
Vitor Oliveira <vbrazo@gmail.com>
2019-02-12 10:06:04 -0200

    Require helpers

--- lib/faker.rb
+++ lib/faker.rb
@@ -9,6 +9,8 @@ end
 require 'i18n'
 require 'set' # Fixes a bug in i18n 0.6.11
 
+Dir.glob(File.join(File.dirname(__FILE__), 'helpers', '*.rb')).sort.each { |file| require file }
+
 I18n.load_path += Dir[File.join(mydir, 'locales', '**/*.yml')]
 I18n.reload! if I18n.backend.initialized?
 
