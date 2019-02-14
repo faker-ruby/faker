37017f7a3fcd1461223738b554b061c30adc7f35
Vitor Oliveira <vbrazo@gmail.com>
2019-02-14 11:26:19 -0200

    Separate v1.9.3 deprecations from current version deprecaions

---
+++ test/deprecate/v1.9.3/test_deprecate_stranger_things.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+require_relative '../../test_helper'
+
+class TestStrangerThings < Test::Unit::TestCase
+  def setup
+    @tester = Faker::StrangerThings
+  end
+
+  def test_characters
+    assert @tester.character.match(/\w+/)
+  end
+
+  def test_quotes
+    assert @tester.quote.match(/\w+/)
+  end
+end
---
+++ test/deprecate/v1.9.3/test_deprecate_v_for_vendetta.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative '../../test_helper'
+
+class TestDeprecateVForVendetta < Test::Unit::TestCase
+  def setup
+    @tester = Faker::VForVendetta
+  end
+
+  def test_character
+    assert @tester.character.match(/\w+/)
+  end
+
+  def test_speech
+    assert @tester.speech.match(/\w+/)
+  end
+
+  def test_quote
+    assert @tester.quote.match(/\w+/)
+  end
+end
