feb3cdabe7145b7c89f1307d194fd750370551ab
Stephen A. Wilson <Stephen-356@hotmail.com>
2019-02-14 19:40:43 -0500

    add some basic docs to test with

--- lib/faker/blockchain/bitcoin.rb
+++ lib/faker/blockchain/bitcoin.rb
@@ -12,10 +12,24 @@ module Faker
           testnet: 111
         }.freeze
 
+        ##
+        # @return {String} A Bitcoin wallet address
+        #
+        # @example
+        #   "147nDP22h3pHrLt2qykTH4txUwQh1ccaXp"
+        #   "1PmjxnDM2oGusc8R776s8hNxdtAsNTnWrv"
+        #   "1CJ8rPhaTbTAP16kzdiHoayEPg6UFMjXhT"
         def address
           address_for(:main)
         end
 
+        ##
+        # @return {String} A Bitcoin testnet address
+        #
+        # @example
+        #   "n4YjRyYD6V6zREpk6opqESDqD3KYnMdVEB"
+        #   "miVEG5z3yoTe8p3Bjh9kh8MBQMb4QZ7d2w"
+        #   "n4PTm5skAEnieUve9wmDxtaLZ9UhCXVDzT"
         def testnet_address
           address_for(:testnet)
         end
