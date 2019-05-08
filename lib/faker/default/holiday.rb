9e52f19924fe726358503bde4091acce6d730dd8
Sander <sander@hiro.nl>
2019-05-08 17:18:00 +0200

    Add Holiday module with Dutch translations

---
+++ lib/faker/default/holiday.rb
@@ -0,0 +1,21 @@
+module Faker
+  class Holiday < Base
+    class << self
+      def name
+        option = rand(2)
+        case option
+        when 0
+          fetch("holiday.holy") % Name.first_name.capitalize
+        when 1
+          fetch("holiday.nature") % Creature::Animal.name.capitalize
+        else
+          fetch("holiday.disaster")
+        end
+      end
+
+      def date
+        Date.forward()
+      end
+    end
+  end
+end
