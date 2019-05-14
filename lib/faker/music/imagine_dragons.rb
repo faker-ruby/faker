6df6ab86bd28d7933a63a8b138e2d903363d2102
simgesen <simge.sen@bil.omu.edu.tr>
2019-05-14 23:58:42 +0300

    Corrected rubocop errors

--- lib/faker/music/imagine_dragons.rb
+++ lib/faker/music/imagine_dragons.rb
@@ -1,21 +1,21 @@
 # frozen_string_literal: true
 
 module Faker
-    class Music
-      class ImagineDragons < Base
-        class << self
-          def member
-            fetch('imagine_dragons.members')
-          end
+  class Music
+    class ImagineDragons < Base
+      class << self
+        def member
+          fetch('imagine_dragons.members')
+        end
 
-          def album
-            fetch('imagine_dragons.albums')
-          end
+        def album
+          fetch('imagine_dragons.albums')
+        end
 
-          def song
-            fetch('imagine_dragons.songs')
-          end
+        def song
+          fetch('imagine_dragons.songs')
         end
       end
     end
+  end
 end
