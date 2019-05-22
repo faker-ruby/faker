e89812aa2e4a0058080fcd7792d46cc017c87805
Adsidera <annamaria.costalonga@gmail.com>
2019-05-22 14:28:14 +0200

    added dashed array in yml and doc

--- lib/faker/opera.rb
+++ lib/faker/opera.rb
@@ -2,22 +2,24 @@
 
 module Faker
   # A generator of titles of operas by Verdi, Rossini, Donizetti and Bellini
-  class Opera < Base
-    class << self
-      def verdi
-        fetch('opera.italian.by_giuseppe_verdi')
-      end
+  class Music
+    class Opera < Base
+      class << self
+        def verdi
+          fetch('opera.italian.by_giuseppe_verdi')
+        end
 
-      def rossini
-        fetch('opera.italian.by_gioacchino_rossini')
-      end
+        def rossini
+          fetch('opera.italian.by_gioacchino_rossini')
+        end
 
-      def donizetti
-        fetch('opera.italian.by_gaetano_donizetti')
-      end
+        def donizetti
+          fetch('opera.italian.by_gaetano_donizetti')
+        end
 
-      def bellini
-        fetch('opera.italian.by_vincenzo_bellini')
+        def bellini
+          fetch('opera.italian.by_vincenzo_bellini')
+        end
       end
     end
   end
