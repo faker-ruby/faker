c634533eee5b554c1be19673582f7559dc36cedc
Jesse Calton <jcalton@skillz.com>
2019-07-21 17:31:59 -0700

    adds prince faker methods

---
+++ lib/faker/music/prince.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+module Faker
+  class Music
+    class Prince < Base
+      class << self
+        def song
+          fetch('prince.song')
+        end
+        def lyric
+          fetch('prince.lyric')
+        end
+        def album
+          fetch('prince.album')
+        end
+      end
+    end
+  end
+end
