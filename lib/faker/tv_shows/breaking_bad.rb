339c945d525ddb77b6ab5b1da8cbacb8bfb6e17b
Connor Shea <connor.james.shea@gmail.com>
2019-08-12 19:11:32 -0600

    document Faker::TvShows::BreakingBad

--- lib/faker/tv_shows/breaking_bad.rb
+++ lib/faker/tv_shows/breaking_bad.rb
@@ -6,10 +6,24 @@ module Faker
       flexible :breaking_bad
 
       class << self
+        ##
+        # Produces the name of a character from Breaking Bad.
+        #
+        # @return [String]
+        #
+        # @example Faker::TvShows::BreakingBad.character
+        #   "Walter White"
         def character
           fetch('breaking_bad.character')
         end
 
+        ##
+        # Produces the name of an episode from Breaking Bad.
+        #
+        # @return [String]
+        #
+        # @example Faker::TvShows::BreakingBad.episode
+        #   "Fly"
         def episode
           fetch('breaking_bad.episode')
         end
