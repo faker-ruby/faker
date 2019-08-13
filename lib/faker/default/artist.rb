4dd0a248732a9eb602778c229541c7eab7838cda
Connor Shea <connor.james.shea@gmail.com>
2019-08-12 21:57:56 -0600

    document Faker::Artist

--- lib/faker/default/artist.rb
+++ lib/faker/default/artist.rb
@@ -3,6 +3,15 @@
 module Faker
   class Artist < Base
     class << self
+      ##
+      # Produces the name of an artist.
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Artist.name #=> "Michelangelo"
+      #
+      # @faker.version 1.8.8
       def name
         fetch('artist.names')
       end
