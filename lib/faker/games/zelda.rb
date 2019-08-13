35dcc2be45d277b4b693a52c47fd068055ef1aa2
Connor Shea <connor.james.shea@gmail.com>
2019-08-12 19:04:40 -0600

    document Faker::Games::Zelda

--- lib/faker/games/zelda.rb
+++ lib/faker/games/zelda.rb
@@ -5,18 +5,46 @@ module Faker
     class Zelda < Base
       flexible :space
       class << self
+        ##
+        # Produces the name of a Legend of Zelda game.
+        #
+        # @return [String]
+        #
+        # @example Faker::Games::Zelda.game
+        #   "Breath of the Wild"
         def game
           fetch('games.zelda.games')
         end
 
+        ##
+        # Produces the name of a character from the Legend of Zelda games.
+        #
+        # @return [String]
+        #
+        # @example Faker::Games::Zelda.character
+        #   "Link"
         def character
           fetch('games.zelda.characters')
         end
 
+        ##
+        # Produces the name of a character from the Legend of Zelda games.
+        #
+        # @return [String]
+        #
+        # @example Faker::Games::Zelda.location
+        #   "Hyrule Castle"
         def location
           fetch('games.zelda.locations')
         end
 
+        ##
+        # Produces the name of an item from the Legend of Zelda games.
+        #
+        # @return [String]
+        #
+        # @example Faker::Games::Zelda.item
+        #   "Boomerang"
         def item
           fetch('games.zelda.items')
         end
