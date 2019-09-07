20d12e071ac437959c98379a0edc709ba837b8c4
Ross Perkel <rossperkel@gmail.com>
2019-09-06 22:54:10 -0400

    add tests

---
+++ test/faker/default/test_faker_marvel_comics.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative '../../test_helper'
+
+class TestFakerMarvelComics < Test::Unit::TestCase
+  def setup
+    @tester = Faker::MarvelComics
+  end
+
+  def test_hero
+    assert @tester.hero.match(/\w+/)
+  end
+
+  def test_heroine
+    assert @tester.heroine.match(/\w+/)
+  end
+
+  def test_villain
+    assert @tester.villain.match(/\w+/)
+  end
+
+  def test_name
+    assert @tester.name.match(/\w+/)
+  end
+
+ 
+end
