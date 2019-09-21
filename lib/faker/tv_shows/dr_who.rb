1d52ededa895b90821367e0f35aa47a82faa1927
Connor Shea <connor.james.shea@gmail.com>
2019-09-20 18:01:59 -0600

    Add YARD docs for Doctor Who.
    This also adds an alias of 'villain' for 'villian', since it's spelled
    wrong, and marks 'villian' as being deprecated.

--- lib/faker/tv_shows/dr_who.rb
+++ lib/faker/tv_shows/dr_who.rb
@@ -6,30 +6,106 @@ module Faker
       flexible :dr_who
 
       class << self
+        ##
+        # Produces a character from Doctor Who.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::TvShows::DrWho.character #=> "Captain Jack Harkness"
+        #
+        # @faker.version 1.8.0
         def character
           fetch('dr_who.character')
         end
 
+        ##
+        # Produces an iteration of The Doctor from Doctor Who.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::TvShows::DrWho.the_doctor #=> "Ninth Doctor"
+        #
+        # @faker.version 1.8.0
         def the_doctor
           fetch('dr_who.the_doctors')
         end
 
+        ##
+        # Produces an actor from Doctor Who.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::TvShows::DrWho.actor #=> "Matt Smith"
+        #
+        # @faker.version 1.9.0
         def actor
           fetch('dr_who.actors')
         end
 
+        ##
+        # Produces a catch phrase from Doctor Who.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::TvShows::DrWho.catch_phrase #=> "Fantastic!"
+        #
+        # @faker.version 1.8.0
         def catch_phrase
           fetch('dr_who.catch_phrases')
         end
 
+        ##
+        # Produces a quote from Doctor Who.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::TvShows::DrWho.quote #=> "Lots of planets have a north!"
+        #
+        # @faker.version 1.8.0
         def quote
           fetch('dr_who.quotes')
         end
 
+        ##
+        # Produces a villain from Doctor Who.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::TvShows::DrWho.villian #=> "The Master"
+        #
+        # @deprecated Use the correctly-spelled `villain` method instead.
+        #
+        # @faker.version 1.8.0
         def villian
           fetch('dr_who.villians')
         end
 
+        ##
+        # Produces a villain from Doctor Who.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::TvShows::DrWho.villain #=> "The Master"
+        #
+        # @faker.version next
+        alias_method :villain, :villian
+
+        ##
+        # Produces a species from Doctor Who.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::TvShows::DrWho.specie #=> "Dalek"
+        #
+        # @faker.version 1.8.0
         def specie
           fetch('dr_who.species')
         end
