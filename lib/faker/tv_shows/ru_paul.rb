6ad3be632cc78372d2d1a765b236a19f67e50d15
Connor Shea <connor.james.shea@gmail.com>
2019-09-22 15:42:37 -0600

    Add YARD docs for all remaining TV Shows (#1759)
    * Add YARD docs for Faker::Music::UmphreysMcgee
    
    * Add YARD docs for Buffy the Vampire Slayer.
    
    * Add YARD docs for Aqua Teen Hunger Force.
    
    * Add YARD docs for BoJack Horseman.
    
    * Add YARD docs for Community.
    
    * Add YARD docs for Hey Arnold!
    
    * Add YARD docs for Family Guy.
    
    * Add YARD docs for Friends.
    
    * Add YARD docs for Game of Thrones.
    
    * Add YARD docs for How I Met Your Mother.
    
    * Add YARD docs for Michael Scott.
    
    * Fix some Rubocop warnings.
    
    * Add YARD docs for New Girl.
    
    * Add YARD docs for Parks and Recreation.
    
    * Add YARD docs for Rick and Morty.
    
    * Add YARD docs for RuPaul's Drag Race.
    
    * Add YARD docs for Seinfeld.
    
    * Add YARD docs for Silicon Valley.
    
    * Add YARD docs for The Simpsons
    
    * Add YARD docs for South Park.
    
    * Add YARD docs for Star Trek.
    
    * Add YARD docs for Stargate.
    
    * Add YARD docs for Stranger Things.
    
    * Add YARD docs for The Expanse.
    
    * Add YARD docs for The Fresh Prince of Bel-Air.
    
    * Add YARD docs for The IT Crowd.
    
    * Add YARD docs for The Thick of It.
    
    * Add YARD docs for Twin Peaks.
    
    * Add YARD docs for The Venture Bros.
    
    * Add YARD docs for Dumb and Dumber.
    
    * Fix grammar in Star Trek.

--- lib/faker/tv_shows/ru_paul.rb
+++ lib/faker/tv_shows/ru_paul.rb
@@ -6,10 +6,28 @@ module Faker
       flexible :rupaul
 
       class << self
+        ##
+        # Produces a quote from RuPaul's Drag Race.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::TvShows::RuPaul.quote #=> "That's Funny, Tell Another One."
+        #
+        # @faker.version 1.8.0
         def quote
           fetch('rupaul.quotes')
         end
 
+        ##
+        # Produces a queen from RuPaul's Drag Race.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::TvShows::RuPaul.queen #=> "Latrice Royale"
+        #
+        # @faker.version 1.8.0
         def queen
           fetch('rupaul.queens')
         end
