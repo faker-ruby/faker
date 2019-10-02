de1f7744b760371e33829f1361ac5af23f5e21ec
Drew Bragg <DRBragg@gmail.com>
2019-10-02 18:02:48 -0400

    Add hockey data and class

---
+++ lib/faker/sports/hockey.rb
@@ -0,0 +1,53 @@
+# frozen_string_literal: true
+
+module Faker
+  class Sports
+    class Hockey < Base
+      class << self
+        ##
+        # Produces the name of a hockey team.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Sports::Hockey.team #=> "Philadelphia Flyers"
+        def team
+          fetch('hockey.teams')
+        end
+
+        ##
+        # Produces the name of a hockey player.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Sports::Hockey.player #=> "Claude Giroux"
+        def player
+          fetch('hockey.players')
+        end
+
+        ##
+        # Produces the name of a hockey division.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Sports::Hockey.division #=> "Metropolitan"
+        def coach
+          fetch('hockey.divisions')
+        end
+
+        ##
+        # Produces a hockey penalty.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Sports::Hockey.penalty #=> "Roughing"
+        def penalty
+          fetch('hockey.penalties')
+        end
+      end
+    end
+  end
+end
