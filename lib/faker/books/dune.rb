0eea2b5b818686496a5996127d9b4877e890ffd8
Matt Kelly <mkelly@weedmaps.com>
2019-10-03 15:37:43 -0400

    Add cities functionality to Dune class

--- lib/faker/books/dune.rb
+++ lib/faker/books/dune.rb
@@ -31,6 +31,19 @@ module Faker
         end
 
         ##
+        # Produces the name of a city from Dune
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Books::Dune.city #=> "Arrakeen"
+        #
+        # @faker.version 1.9.3
+        def city
+          fetch('dune.cities')
+        end
+
+        ##
         # Produces the name of a planet from Dune
         #
         # @return [String]
