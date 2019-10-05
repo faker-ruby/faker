4bc81aa0b38e5e3c11b0cb12d0a1958d537fa7c4
Ashish Rao <ashishrao2598@gmail.com>
2019-10-05 08:53:58 +0530

    add Faker::Relationship YARD docs

--- lib/faker/default/relationship.rb
+++ lib/faker/default/relationship.rb
@@ -5,6 +5,15 @@ module Faker
     flexible :relationship
 
     class << self
+      ##
+      # Produces a random family relationship.
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Relationship.familial #=> "Grandfather"
+      #
+      # @faker.version next
       def familial(legacy_connection = NOT_GIVEN, connection: nil)
         warn_for_deprecated_arguments do |keywords|
           keywords << :connection if legacy_connection != NOT_GIVEN
@@ -26,18 +35,54 @@ module Faker
         fetch('relationship.familial.' + connection)
       end
 
+      ##
+      # Produces a random in-law relationship .
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Relationship.in_law #=> "Brother-in-law"
+      #
+      # @faker.version next
       def in_law
         fetch('relationship.in_law')
       end
 
+      ##
+      # Produces a random spouse relationship.
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Relationship.spouse #=> "Husband"
+      #
+      # @faker.version next
       def spouse
         fetch('relationship.spouse')
       end
 
+      ##
+      # Produces a random parent relationship.
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Relationship.parent #=> "Father"
+      #
+      # @faker.version next
       def parent
         fetch('relationship.parent')
       end
 
+      ##
+      # Produces a random sibling relationship.
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Relationship.sibling #=> "Sister"
+      #
+      # @faker.version next
       def sibling
         fetch('relationship.sibling')
       end
