30ff89ae0d1556077e9874fd098b4dbd8155e73f
Lucas Sapienza <sapienzalucas@gmail.com>
2019-10-11 16:40:19 -0300

    add coin docs

--- lib/faker/default/coin.rb
+++ lib/faker/default/coin.rb
@@ -3,10 +3,28 @@
 module Faker
   class Coin < Base
     class << self
+      ##
+      # Retrieves a random coin from any country.
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Coin.name #=> "Brazilian Real"
+      #
+      # @faker.version 2.6.0
       def name
         fetch('currency.name')
       end
 
+      ##
+      # Retrieves a face to a flipped coin
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Coin.flip #=> "Heads"
+      #
+      # @faker.version 2.6.0
       def flip
         fetch('coin.flip')
       end
