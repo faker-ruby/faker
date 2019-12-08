5a9142b4cd85a374f487a2e88b6b9ebe46a5e84d
Joe Nyland <7666402+JoeNyland@users.noreply.github.com>
2019-12-08 17:37:40 +0000

    Add Faker::TvShows::Futurama

---
+++ test/faker/tv_shows/test_futurama.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative '../../test_helper'
+
+class TestFakerTvShowsFuturama < Test::Unit::TestCase
+  def setup
+    @tester = Faker::TvShows::Futurama
+  end
+
+  def test_characters
+    10.times { assert @tester.character.match(/[\w]+/) }
+  end
+
+  def test_locations
+    10.times { assert @tester.location.match(/[\w]+/) }
+  end
+
+  def test_quote
+    10.times { assert @tester.quote.match(/[\w]+/) }
+  end
+end
