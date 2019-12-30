4809fb9e5fabb1cd8ef8b241c2d2f0d671aa2c03
Leandro Matheus <kesuhige@gmail.com>
2019-12-30 16:24:14 -0300

    Add YARD docs

--- lib/faker/music/the_beatles.rb
+++ lib/faker/music/the_beatles.rb
@@ -4,14 +4,41 @@ module Faker
   class Music
     class TheBeatles < Base
       class << self
+        ##
+        # Produces a member from The Beatles.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Music::TheBeatles.member #=> "John Lennon"
+        #
+        # @faker.version 1.9.4
         def member
           fetch('the_beatles.members')
         end
 
+        ##
+        # Produces an album from The Beatles.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Music::TheBeatles.album #=> "Rubber Soul"
+        #
+        # @faker.version 1.9.4
         def album
           fetch('the_beatles.albums')
         end
 
+        ##
+        # Produces a song from The Beatles.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Music::TheBeatles.song #=> "Here Comes The Sun"
+        #
+        # @faker.version 1.9.4
         def song
           fetch('the_beatles.songs')
         end
