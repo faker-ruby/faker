0ad058a764d019d8d68dae40c2582aa82d7b4543
Michael Hebblethwaite <32097294+michebble@users.noreply.github.com>
2020-01-22 20:36:59 +1100

    Add YARD docs for Faker:Verb

--- lib/faker/default/verb.rb
+++ lib/faker/default/verb.rb
@@ -3,22 +3,67 @@
 module Faker
   class Verb < Base
     class << self
+      ##
+      # Produces the base form of a verb.
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Verbs.base #=> "shape"
+      #
+      # @faker.version 1.9.0
       def base
         fetch('verbs.base')
       end
 
+      ##
+      # Produces the past form of a verb.
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Verbs.past #=> "motivated"
+      #
+      # @faker.version 1.9.0
       def past
         fetch('verbs.past')
       end
 
+      ##
+      # Produces the past participle form of a verb.
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Verbs.past_participle #=> "buzzed"
+      #
+      # @faker.version 1.9.0
       def past_participle
         fetch('verbs.past_participle')
       end
 
+      ##
+      # Produces the simple_present form of a verb.
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Verbs.simple_present #=> "gets"
+      #
+      # @faker.version 1.9.0
       def simple_present
         fetch('verbs.simple_present')
       end
 
+      ##
+      # Produces the -ing form of a verb.
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Verbs.ing_form #=> "climbing"
+      #
+      # @faker.version 1.9.0
       def ing_form
         fetch('verbs.ing_form')
       end
