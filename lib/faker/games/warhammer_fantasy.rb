6ee94b94d8e50de3818116249eedaa0327080526
Matthew Masiello <sotek222@yahoo.com>
2020-01-24 09:59:53 -0500

    ADD initial files

---
+++ lib/faker/games/warhammer_fantasy.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+module Faker
+  class Games
+    class WarhammerFantasy < Base
+      class << self
+        # hero
+        # quote
+        # race
+        # creature
+        # location
+      end
+    end
+  end
+end
