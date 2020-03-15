6936ce494373a9810a6f6fad51a539f0b5c90147
Cathy Cramer <cathy.cramer@redcanary.com>
2020-03-15 17:09:21 -0600

    add missing YARD documents

--- lib/faker/default/computer.rb
+++ lib/faker/default/computer.rb
@@ -32,26 +32,44 @@ module Faker
       ##
       # Produces the name of a computer os.
       #
+      # @param platform [String] optionally specify the platform
       # @return [String]
       #
       # @example
       #   Faker::Computer.os #=> "RHEL 6.10"
       #
       # @faker.version next
-      def os(plat = platform)
-        plat = platform unless fetch_all('computer.platform').include?(plat)
-        plat = search_format(plat)
-        fetch("computer.#{plat}.os")
+      def os(platform:)
+        platform = self.platform unless fetch_all('computer.platform').include?(platform)
+        platform = search_format(platform)
+        fetch("computer.#{platform}.os")
       end
 
+      ##
+      # Produces a string with computer platform and os
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Computer.stack #=> "Linux, RHEL 6.10"
+      #
+      # @faker.version next
       def stack
-        plat = platform
-        os = fetch("computer.os.#{search_format(plat)}")
-        "#{plat}, #{os}"
+        platform = self.platform
+        os = fetch("computer.os.#{search_format(platform)}")
+        "#{platform}, #{os}"
       end
 
       private
-
+      # Produces a string compatible with key format from a platform
+      #
+      # @!visibility private
+      # @return [String]
+      #
+      # @example
+      #   search_format(OS X) #=> "os_x"
+      #
+      # @faker.version next
       def search_format(key)
         key.split.length > 1 ? key.split.join('_').downcase : key.downcase
       end
