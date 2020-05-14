796f45c5e9bae6e07524f014d6bd25628a716bb8
danielTiringer <tiringerdaniel@gmail.com>
2020-05-14 16:25:35 +0200

    Add YARD docs to Faker::Construction

--- lib/faker/default/construction.rb
+++ lib/faker/default/construction.rb
@@ -2,26 +2,80 @@
 
 module Faker
   class Construction < Base
+    ##
+    # Produces a random material.
+    #
+    # @return [String]
+    #
+    # @example
+    #   Faker::Construction.material #=> "Wood"
+    #
+    # @faker.version 1.8.0
     def self.material
       fetch('construction.materials')
     end
 
+    ##
+    # Produces a random subcontract category.
+    #
+    # @return [String]
+    #
+    # @example
+    #   Faker::Construction.subcontract_category #=> "Curb & Gutter"
+    #
+    # @faker.version 1.8.0
     def self.heavy_equipment
       fetch('construction.heavy_equipment')
     end
 
+    ##
+    # Produces a random heavy_equipment.
+    #
+    # @return [String]
+    #
+    # @example
+    #   Faker::Construction.heavy_equipment #=> "Excavator"
+    #
+    # @faker.version 1.8.0
     def self.trade
       fetch('construction.trades')
     end
 
+    ##
+    # Produces a random trade.
+    #
+    # @return [String]
+    #
+    # @example
+    #   Faker::Construction.trade #=> "Carpenter"
+    #
+    # @faker.version 1.8.0
     def self.subcontract_category
       fetch('construction.subcontract_categories')
     end
 
+    ##
+    # Produces a random standard_cost_code.
+    #
+    # @return [String]
+    #
+    # @example
+    #   Faker::Construction.standard_cost_code #=> "1-000 - Purpose"
+    #
+    # @faker.version 1.8.0
     def self.standard_cost_code
       fetch('construction.standard_cost_codes')
     end
 
+    ##
+    # Produces a random role.
+    #
+    # @return [String]
+    #
+    # @example
+    #   Faker::Construction.role #=> "Engineer"
+    #
+    # @faker.version 1.8.0
     def self.role
       fetch('construction.roles')
     end
