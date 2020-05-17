8750f0cf84474b4b9a775c45c9ea1c1ec472364c
Armando Fox <fox@cs.berkeley.edu>
2020-05-16 17:30:42 -0700

    Update lib/faker/music/show.rb
    Co-authored-by: Stephen A. Wilson <stephen-356@hotmail.com>

--- lib/faker/music/show.rb
+++ lib/faker/music/show.rb
@@ -3,14 +3,44 @@
 module Faker
   class Show < Base
     class << self
+      ##
+      # Produces the name of a musical for an older audience
+      #
+      # @return [String]
+      # 
+      # @example
+      #   Faker::Alphanumeric.alpha
+      #     #=> "West Side Story" 
+      #
+      # @faker.version next
       def adult_musical
         fetch('show.adult_musical')
       end
 
+      ##
+      # Produces the name of a musical for a younger audience 
+      #
+      # @return [String]
+      # 
+      # @example
+      #   Faker::Alphanumeric.alpha
+      #     #=> "Into the Woods JR." 
+      #
+      # @faker.version next
       def kids_musical
         fetch('show.kids_musical')
       end
-
+      
+      ##
+      # Produces the name of a play
+      #
+      # @return [String]
+      # 
+      # @example
+      #   Faker::Alphanumeric.alpha
+      #     #=> "Death of a Salesman" 
+      #
+      # @faker.version next
       def play
         fetch('show.play')
       end
