2bbc47059e541b79d434154a2c8f00519bae3255
Zachary Fine <zachary.fine@freshly.com>
2020-05-29 21:11:35 -0400

    added new methods with documentation

--- lib/faker/music/phish.rb
+++ lib/faker/music/phish.rb
@@ -5,6 +5,32 @@ module Faker
     class Phish < Base
       class << self
         ##
+        # Produces the name of a album by Phish.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Music::Phish.album #=> "Fuego"
+        #
+        # @faker.version 1.9.2
+        def album
+          fetch('phish.song')
+        end
+
+        ##
+        # Produces the name of a band member of Phish.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Music::Phish.player #=> "Trey Anastasio"
+        #
+        # @faker.version 1.9.2
+        def album
+          fetch('phish.player')
+        end
+
+        ##
         # Produces the name of a song by Phish.
         #
         # @return [String]
