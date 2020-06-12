f416a598e8c596c2b545837704302f70f1a1c40d
Daniel Tiringer <53534182+danielTiringer@users.noreply.github.com>
2020-06-13 00:32:24 +0200

    Add YARD docs to Faker::Lorem_Pixel (#2052)

--- lib/faker/default/lorem_pixel.rb
+++ lib/faker/default/lorem_pixel.rb
@@ -17,6 +17,28 @@ module Faker
                                 technics
                                 transport].freeze
 
+      ##
+      # Produces a random image URL from lorempixel.com.
+      #
+      # @param size [String] Specifies the size of image to generate.
+      # @param is_gray [Boolean] Determines if the image is gray.
+      # @param category [Symbol] Adds the category of the generated image to the URL.
+      # @param number [Integer] Adds a number as part of the URL.
+      # @param text [Integer] Adds dummy text as part of the URL.
+      # @param secure [Boolean] Changes the image URL between http and https.
+      # @return [String]
+      #
+      # @example
+      #   Faker::LoremPixel.image #=> "https://lorempixel.com/300/300"
+      #   Faker::LoremPixel.image(size: "50x60") #=> "https://lorempixel.com/50/60"
+      #   Faker::LoremPixel.image(size: "50x60", is_gray: true) #=> "https://lorempixel.com/g/50/60"
+      #   Faker::LoremPixel.image(size: "50x60", is_gray: false, category: 'sports') #=> "https://lorempixel.com/50/60/sports"
+      #   Faker::LoremPixel.image(size: "50x60", is_gray: false, category: 'sports', number: 3) #=> "https://lorempixel.com/50/60/sports/3"
+      #   Faker::LoremPixel.image(size: "50x60", is_gray: false, category: 'sports', number: 3, text: 'Dummy-text') #=> "https://lorempixel.com/50/60/sports/3/Dummy-text"
+      #   Faker::LoremPixel.image(size: "50x60", is_gray: false, category: 'sports', number: nil, text: 'Dummy-text') #=> "https://lorempixel.com/50/60/sports/Dummy-text"
+      #   Faker::LoremPixel.image(secure: false) #=> "http://lorempixel.com/300/300"
+      #
+      # @faker.version 1.7.0
       # rubocop:disable Metrics/ParameterLists
       def image(legacy_size = NOT_GIVEN, legacy_is_gray = NOT_GIVEN, legacy_category = NOT_GIVEN, legacy_number = NOT_GIVEN, legacy_text = NOT_GIVEN, legacy_secure = NOT_GIVEN, size: '300x300', is_gray: false, category: nil, number: nil, text: nil, secure: true)
         warn_for_deprecated_arguments do |keywords|
