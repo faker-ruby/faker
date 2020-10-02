7578d3002ba25a5864f508b31338f8257d648cce
Stephen A. Wilson <stephen-356@hotmail.com>
2020-10-02 13:43:04 -0400

    fix typo (#2141)

--- lib/faker/games/control.rb
+++ lib/faker/games/control.rb
@@ -23,7 +23,7 @@ module Faker
         # @return [String]
         #
         # @example
-        #   Faker::Games::Control.character #=> "Dimensional Research"
+        #   Faker::Games::Control.location #=> "Dimensional Research"
         #
         # @faker.version 2.13.0
         def location
