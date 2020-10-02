8cea85ff85e43a4a083af1102a2d4c5e293da70d
Adrian Rivera <adrian.rivera.dev@gmail.com>
2020-10-01 20:29:12 -0700

    Fixes a bug when generating passwords with min_length eq 1

--- lib/faker/default/internet.rb
+++ lib/faker/default/internet.rb
@@ -115,7 +115,11 @@ module Faker
           keywords << :special_characters if legacy_special_characters != NOT_GIVEN
         end
 
-        min_alpha = mix_case ? 2 : 0
+        min_alpha = if mix_case && min_length > 1
+                      2
+                    else
+                      0
+                    end
         temp = Lorem.characters(number: min_length, min_alpha: min_alpha)
         diff_length = max_length - min_length
 
