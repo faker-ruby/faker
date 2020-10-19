4bd035709065154fda6296dd7785848cdfbb3474
RubyHuntsman <PublicProgramer@gmx.com>
2020-10-20 00:58:46 +0200

    Add generator to Faker::Games::ElderScrolls

--- test/faker/games/test_faker_elder_scrolls.rb
+++ test/faker/games/test_faker_elder_scrolls.rb
@@ -38,4 +38,8 @@ class TestFakerElderScrolls < Test::Unit::TestCase
   def test_last_name
     assert @tester.last_name.match(/\w+/)
   end
+
+  def test_weapon
+    assert @tester.weapon.match(/\w+/)
+  end
 end
