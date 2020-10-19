c4cc7648a24f62673dce71388acd8e0a966bd0d0
Hubert <PublicProgramer@gmx.com>
2020-10-19 14:43:25 +0200

    Add more generators to Faker::Minecraft (#2162)
    * Add more namespaces to Faker::Minecraft
    
    * Fix a typos in Minecraft methods description

--- doc/games/minecraft.md
+++ doc/games/minecraft.md
@@ -2,11 +2,26 @@
 
 ```ruby
 
+# Generate random achievement from Minecraft
+Faker::Games::Minecraft.achievement #=> "Time to Mine!"
+
+# Generate random biome from Minecraft
+Faker::Games::Minecraft.biome # => "Jungle"
+
 # Generate random block from Minecraft
 Faker::Games::Minecraft.block # => "Stone"
+
+# Generate random enchantment from Minecraft
+Faker::Games::Minecraft.enchantment # => "Fire Protection"
+
+# Generate random game mode from Minecraft
+Faker::Games::Minecraft.game_mode #=> "Survival"
 
 # Generate random item from Minecraft
 Faker::Games::Minecraft.item # => "Iron Shovel"
 
 # Generate random mob from Minecraft
 Faker::Games::Minecraft.mob # => "Sheep"
+
+# Generate random status effect from Minecraft
+Faker::Games::Minecraft.status_effect #=> "Weakness"
--- lib/faker/games/minecraft.rb
+++ lib/faker/games/minecraft.rb
@@ -5,6 +5,32 @@ module Faker
     class Minecraft < Base
       class << self
         ##
+        # Produces the name of an achievement from Minecraft.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Games::Minecraft.achievement #=> "Time to Mine!"
+        #
+        # @faker.version next
+        def achievement
+          fetch('games.minecraft.achievement')
+        end
+
+        ##
+        # Produces the name of a biome from Minecraft.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Games::Minecraft.biome #=> "Jungle"
+        #
+        # @faker.version next
+        def biome
+          fetch('games.minecraft.biome')
+        end
+
+        ##
         # Produces the name of a block from Minecraft.
         #
         # @return [String]
@@ -15,6 +41,32 @@ module Faker
         # @faker.version 2.13.0
         def block
           fetch('games.minecraft.blocks')
+        end
+
+        ##
+        # Produces the name of a enchantment from Minecraft.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Games::Minecraft.enchantment #=> "Fire Protection"
+        #
+        # @faker.version next
+        def enchantment
+          fetch('games.minecraft.enchantment')
+        end
+
+        ##
+        # Produces the name of a game mode from Minecraft.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Games::Minecraft.game_mode #=> "Survival"
+        #
+        # @faker.version next
+        def game_mode
+          fetch('games.minecraft.game_mode')
         end
 
         ##
@@ -41,6 +93,19 @@ module Faker
         # @faker.version 2.13.0
         def mob
           fetch('games.minecraft.mobs')
+        end
+
+        ##
+        # Produces the name of a status effect from Minecraft.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Games::Minecraft.status_effect #=> "Weakness"
+        #
+        # @faker.version next
+        def status_effect
+          fetch('games.minecraft.status_effect')
         end
       end
     end
