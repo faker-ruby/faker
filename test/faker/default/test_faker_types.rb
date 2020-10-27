05b2505db6c58c2e03133e6e77a3c362b48238b0
Akira Matsuda <ronnie@dio.jp>
2020-10-27 09:17:48 +0900

    rubocop -a --only Style/ClassEqualityComparison

--- test/faker/default/test_faker_types.rb
+++ test/faker/default/test_faker_types.rb
@@ -11,7 +11,7 @@ class TestFakerTypes < Test::Unit::TestCase
   end
 
   def test_rb_string_is_or_correct_type
-    assert @tester.rb_string.class == String
+    assert @tester.rb_string.instance_of?(String)
   end
 
   def test_string_returns_correct_number_of_words
@@ -25,7 +25,7 @@ class TestFakerTypes < Test::Unit::TestCase
   end
 
   def test_integer
-    assert @tester.rb_integer.class == Integer
+    assert @tester.rb_integer.instance_of?(Integer)
   end
 
   def test_rb_integer_between
@@ -36,7 +36,7 @@ class TestFakerTypes < Test::Unit::TestCase
   end
 
   def test_rb_hash_returns_a_hash
-    assert @tester.rb_hash.class == Hash
+    assert @tester.rb_hash.instance_of?(Hash)
   end
 
   def test_hash_returns_the_correct_number_of_keys
@@ -46,7 +46,7 @@ class TestFakerTypes < Test::Unit::TestCase
   end
 
   def test_complex_rb_hash_returns_a_hash
-    assert @tester.complex_rb_hash.class == Hash
+    assert @tester.complex_rb_hash.instance_of?(Hash)
   end
 
   def test_complex_hash_returns_the_correct_number_of_keys
@@ -56,7 +56,7 @@ class TestFakerTypes < Test::Unit::TestCase
   end
 
   def test_rb_array_returns_array
-    assert @tester.rb_array.class == Array
+    assert @tester.rb_array.instance_of?(Array)
   end
 
   def test_array_has_the_right_array
