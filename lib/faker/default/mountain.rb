87a9a6de9668e74a3bf25320a2423384ea2f0de8
Mac <bs_chapagain@hotmail.com>
2020-11-21 18:43:27 +1100

    Faker::Mountain class added

---
+++ lib/faker/default/mountain.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Faker
+  class Mountain < Base
+    class << self
+      ##
+      # Produces a name of a mountain
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Mountain.name #=> "Everest"
+      #
+      def name
+        fetch('mountain.name')
+      end
+
+      ##
+      # Produces a name of a range
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::Mountain.range #=> "Dhaulagiri Himalaya"
+      #
+      # @faker.version next
+      def range
+        fetch('mountain.range')
+      end
+    end
+  end
+end
