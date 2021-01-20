826c0fc6857e0d1c38e7890646d482f8feed94e8
Blake Astley <astley92@hotmail.com>
2021-01-20 22:41:44 +1100

    Add initial test

---
+++ test/faker/location/test_faker_australia.rb
@@ -0,0 +1,11 @@
+require_relative '../../test_helper'
+
+class TestFakerAustralia < Test::Unit::TestCase
+  def setup
+    @tester = Faker::Australia
+  end
+
+  def test_location
+    assert @tester.location.match(/\w+/)
+  end
+end
