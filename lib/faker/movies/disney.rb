43dd5a47f81f1405d7b9d2839db72152a1c3f874
Jessica Kwok <jessicatckwok@gmail.com>
2021-01-30 16:10:34 -0800

    Refactor disney.yml

--- lib/faker/movies/disney.rb
+++ lib/faker/movies/disney.rb
@@ -1,23 +1,22 @@
 # frozen_string_literal: true
 
 module Faker
-    class Movies
-      class Disney < Base
-        class << self
-          ##
-          # Produces a princess from Disney.
-          #
-          # @return [String]
-          #
-          # @example
-          #   Faker::Movies::Disney.princess #=> "Ariel"
-          #
-          # @faker.version next
-          def princess
-            fetch('disney.princess')
-          end
+  class Movies
+    class Disney < Base
+      class << self
+        ##
+        # Produces a princess from Disney.
+        #
+        # @return [String]
+        #
+        # @example
+        #   Faker::Movies::Disney.princess #=> "Ariel"
+        #
+        # @faker.version next
+        def princess
+          fetch('disney.princess')
         end
       end
     end
   end
-  
+end
