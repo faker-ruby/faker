6b3c41d52fa55780d46bc191bccff2905664352f
Franco Colacilli <colacillifranco@gmail.com>
2021-02-06 12:33:46 -0300

    Tests refactoring and data correction

--- test/test_es_ar_locale.rb
+++ test/test_es_ar_locale.rb
@@ -5,103 +5,260 @@ require_relative 'test_helper'
 class TestEsArLocale < Test::Unit::TestCase
   def setup
     Faker::Config.locale = 'es-AR'
+
+    @no_words_pattern = /\w{0}$/u
+    @one_word_pattern = /\w+/u
+    @one_or_more_words_pattern = /(?:\w+\ )*\w+/u
+    @many_words_pattern = /(?:\w+\ )+\w+/u
+
+    @name_with_nickname_pattern = /(?:\'*\w+\'*\ )+(?:\'*\w+\'*)/u
+
+    @compass_letter_pattern = /^[NEOS]{1,2}(?:p?[NEOS]{1,2})?$/u
+    @compass_number_pattern = /^\d+(?:.\d\d?)?$/
   end
 
   def teardown
     Faker::Config.locale = nil
   end
 
-  def test_es_ar_name_methods
-    assert Faker::Name.male_first_name.is_a? String
-    assert Faker::Name.female_first_name.is_a? String
-    assert Faker::Name.first_name.is_a? String
-    assert Faker::Name.last_name.is_a? String
-    assert Faker::Name.prefix.is_a? String
-    assert Faker::Name.male_suffix.is_a? String
-    assert Faker::Name.female_suffix.is_a? String
-    assert Faker::Name.suffix.is_a? String
-    assert Faker::Name.name.is_a? String
-    assert Faker::Name.name_with_middle.is_a? String
+  # -- NAMES
+  
+  def test_es_ar_male_first_name
+    assert Faker::Name.male_first_name.match(@one_or_more_words_pattern)
+  end
+  
+  def test_es_ar_female_first_name
+    assert Faker::Name.female_first_name.match(@one_or_more_words_pattern)
+  end
+  
+  def test_es_ar_neutral_first_name
+    assert Faker::Name.neutral_first_name.match(@no_words_pattern)
+  end
+  
+  def test_es_ar_last_name
+    assert Faker::Name.last_name.match(@one_or_more_words_pattern)
+  end
+  
+  def test_es_ar_prefix
+    assert Faker::Name.prefix.match(/(\w+\.)/u)
+  end
+  
+  def test_es_ar_suffix
+    assert Faker::Name.suffix.match(/(\w+\.?)/u)
+  end
+  
+  def test_es_ar_name
+    assert Faker::Name.name.match(@many_words_pattern)
   end
 
-  def test_es_ar_address
-    # city
-    assert Faker::Address.city_prefix.is_a? String
-    assert Faker::Address.army_city_prefix.is_a? String
-    assert Faker::Address.male_city_saint_prefix.is_a? String
-    assert Faker::Address.female_city_saint_prefix.is_a? String
-    assert Faker::Address.city_suffix.is_a? String
-    assert Faker::Address.city_names.is_a? String
-    assert Faker::Address.city.is_a? String
-    # state
-    assert Faker::Address.state.is_a? String
-    assert Faker::Address.state_abbr.is_a? String
-    assert Faker::Address.state_abbr_iata.is_a? String
-    assert_match(/[A-Z]{2,4}/, Faker::Address.state_abbr_iata)
-    assert Faker::Address.city_with_state.is_a? String
-    # address
-    assert Faker::Address.building_number.is_a? String
-    assert_match(/(S\/N|\d{2,5})/, Faker::Address.building_number)
-    assert Faker::Address.secondary_address.is_a? String
-    assert_match(/(PB|PA|(Piso|UF N°)\s\d{1,2}|(Departamento|Depto.|Lote)\s\d{1,2}[A-Z]|Manzana\s[A-Z]\d)/, Faker::Address.secondary_address)
-    assert Faker::Address.street.is_a? String
-    assert Faker::Address.street_prefix.is_a? String
-    assert Faker::Address.street_suffix.is_a? String
-    assert Faker::Address.street_name.is_a? String
-    assert Faker::Address.road_prefix.is_a? String
-    assert Faker::Address.road_suffix.is_a? String
-    assert Faker::Address.road_name.is_a? String
+  def test_es_ar_name_with_middle
+    assert Faker::Name.name_with_middle.match(@many_words_pattern)
+  end
+
+  
+  # -- ADDRESS
+
+  # ---- CITY
+
+  def test_es_ar_city_prefix
+    assert Faker::Address.city_prefix.match(@one_or_more_words_pattern)
+  end
+  
+  def test_es_ar_city_suffix
+    assert Faker::Address.city_suffix.match(@one_or_more_words_pattern)
+  end
+
+  def test_es_ar_city
+    assert Faker::Address.city.match(@one_or_more_words_pattern)
+  end
+
+  # ----- STATE
+  
+  def test_es_ar_state
+    assert Faker::Address.state.match(@one_or_more_words_pattern)
+  end
+
+  def test_es_ar_state_abbr
+    assert Faker::Address.state_abbr.match(/(\w+\.*)/u)
+  end
+
+  # ----- ADDRESS
+
+  def test_es_ar_building_number
+    assert Faker::Address.building_number.match(/(S\/N|\d{2,5})/u)
+  end
+  
+  def test_es_ar_secondary_address
+    assert Faker::Address.secondary_address.match(/(PB|PA|(Piso|UF N°)\s\d{1,2}|(Departamento|Depto.|Lote)\s\d{1,2}[A-Z]|Manzana\s[A-Z]\d)/u)
+  end
+
+  def test_es_ar_street_suffix
+    assert Faker::Address.street_suffix.match(@no_words_pattern)
+  end
+
+  def test_es_ar_street_name
+    assert Faker::Address.street_name.match(@one_or_more_words_pattern)
+  end
+
+  def test_es_ar_street_address
     assert Faker::Address.street_address.is_a? String
-    assert Faker::Address.postcode.is_a? String
-    assert_match(/(\d{4}|[A-Z]\d{4}[A-Z]{3})/, Faker::Address.postcode)
-    assert Faker::Address.postcode(state_abbreviation: 'BA').match(/(B\d{4}[A-Z]{3})/)
+  end
+
+  def test_es_ar_postcode
+    assert Faker::Address.postcode.match(/(\d{4}|[A-Z]\d{4}[A-Z]{3})/u)
+  end
+
+  def test_es_ar_postcode_by_state_abbreviation
+    assert Faker::Address.postcode(state_abbreviation: 'BA').match(/(B\d{4}[A-Z]{3})/u)
+  end
+
+  def test_es_ar_full_address
     assert Faker::Address.full_address.is_a? String
-    # country
-    assert Faker::Address.country.is_a? String
+  end
+
+  # ---- COUNTRY
+
+  def test_es_ar_country
+    assert Faker::Address.country.match(@one_or_more_words_pattern)
+  end
+
+  def test_es_ar_country_by_code
     assert Faker::Address.country_by_code(code: 'NL').match('Holanda')
+  end
+
+  def test_es_ar_country_name_to_code
     assert Faker::Address.country_name_to_code(name: 'estados_unidos').match('US')
+  end
+
+  def test_es_ar_default_country
     assert Faker::Address.default_country, 'Argentina'
   end
 
-  def test_es_ar_football
-    assert Faker::Sports::Football.team.is_a? String
-    assert Faker::Sports::Football.player.is_a? String
-    assert Faker::Sports::Football.coach.is_a? String
-    assert Faker::Sports::Football.competition.is_a? String
-    assert Faker::Sports::Football.position.is_a? String
-  end
-
-  def test_es_ar_basketball
-    assert Faker::Sports::Basketball.team.is_a? String
-    assert Faker::Sports::Basketball.player.is_a? String
-    assert Faker::Sports::Basketball.coach.is_a? String
-    assert Faker::Sports::Basketball.position.is_a? String
-  end
+  # -- FOOTBALL
   
-  def test_es_ar_military
-    assert Faker::Military.army_rank.is_a? String
-    assert Faker::Military.marines_rank.is_a? String
-    assert Faker::Military.navy_rank.is_a? String
-    assert Faker::Military.coast_guard_rank.is_a? String
-    assert Faker::Military.air_force_rank.is_a? String
-    assert Faker::Military.space_force_rank.is_a? String
+  def test_es_ar_football_team
+    assert Faker::Sports::Football.team.match(@one_or_more_words_pattern)
   end
 
-  def test_es_ar_compass
-    assert Faker::Compass.cardinal.is_a? String
-    assert Faker::Compass.ordinal.is_a? String
-    assert Faker::Compass.half_wind.is_a? String
-    assert Faker::Compass.quarter_wind.is_a? String
-    assert Faker::Compass.direction.is_a? String
-    assert Faker::Compass.abbreviation.is_a? String
-    assert Faker::Compass.azimuth.is_a? String
-    assert Faker::Compass.cardinal_abbreviation.is_a? String
-    assert Faker::Compass.ordinal_abbreviation.is_a? String
-    assert Faker::Compass.half_wind_abbreviation.is_a? String
-    assert Faker::Compass.quarter_wind_abbreviation.is_a? String
-    assert Faker::Compass.cardinal_azimuth.is_a? String
-    assert Faker::Compass.ordinal_azimuth.is_a? String
-    assert Faker::Compass.half_wind_azimuth.is_a? String
-    assert Faker::Compass.quarter_wind_azimuth.is_a? String
+  def test_es_ar_football_player
+    assert Faker::Sports::Football.player.match(@name_with_nickname_pattern)
+  end
+
+  def test_es_ar_football_coach
+    assert Faker::Sports::Football.coach.match(@name_with_nickname_pattern)
+  end
+
+  def test_es_ar_football_competition
+    assert Faker::Sports::Football.competition.match(@one_or_more_words_pattern)
+  end
+
+  def test_es_ar_football_position
+    assert Faker::Sports::Football.position.match(@one_or_more_words_pattern)
+  end
+
+  # -- BASKETBALL  
+
+  def test_es_ar_basketball_team
+    assert Faker::Sports::Basketball.team.match(@one_or_more_words_pattern)
+  end
+
+  def test_es_ar_basketball_player
+    assert Faker::Sports::Basketball.player.match(@name_with_nickname_pattern)
+  end
+
+  def test_es_ar_basketball_coach
+    assert Faker::Sports::Basketball.coach.match(@name_with_nickname_pattern)
+  end
+
+  def test_es_ar_basketball_position
+    assert Faker::Sports::Basketball.position.match(@one_or_more_words_pattern)
+  end
+
+  # -- MILITARY
+  
+  def test_es_ar_army_rank
+    assert Faker::Military.army_rank.match(@one_or_more_words_pattern)
+  end
+
+  def test_es_ar_marines_rank
+    assert Faker::Military.marines_rank.match(@no_words_pattern)
+  end
+
+  def test_es_ar_navy_rank
+    assert Faker::Military.navy_rank.match(@one_or_more_words_pattern)
+  end
+
+  def test_es_ar_coast_guard_rank
+    assert Faker::Military.coast_guard_rank.match(@one_or_more_words_pattern)
+  end
+
+  def test_es_ar_air_force_rank
+    assert Faker::Military.air_force_rank.match(@one_or_more_words_pattern)
+  end
+
+  def test_es_ar_space_force_rank
+    assert Faker::Military.space_force_rank.match(@no_words_pattern)
+  end
+
+  # -- COMPASS
+
+  def test_es_ar_cardinal 
+    assert Faker::Compass.cardinal.match(@one_word_pattern)
+  end
+
+  def test_es_ar_ordinal 
+    assert Faker::Compass.ordinal.match(@one_word_pattern)
+  end
+
+  def test_es_ar_half_wind 
+    assert Faker::Compass.half_wind.match(@one_word_pattern)
+  end
+
+  def test_es_ar_quarter_wind 
+    assert Faker::Compass.quarter_wind.match(/^\w+ por el \w+$/u)
+  end
+
+  def test_es_ar_direction 
+    assert Faker::Compass.direction.match(/^(?:\w+|\w+ por el \w+|[\w\-]+)$/u)
+  end
+
+  def test_es_ar_abbreviation 
+    assert Faker::Compass.abbreviation.match(@compass_letter_pattern)
+  end
+
+  def test_es_ar_azimuth 
+    assert Faker::Compass.azimuth.match(@compass_number_pattern)
+  end
+
+  def test_es_ar_cardinal_abbreviation 
+    assert Faker::Compass.cardinal_abbreviation.match(@compass_letter_pattern)
+  end
+
+  def test_es_ar_ordinal_abbreviation 
+    assert Faker::Compass.ordinal_abbreviation.match(@compass_letter_pattern)
+  end
+
+  def test_es_ar_half_wind_abbreviation 
+    assert Faker::Compass.half_wind_abbreviation.match(@compass_letter_pattern)
+  end
+
+  def test_es_ar_quarter_wind_abbreviation 
+    assert Faker::Compass.quarter_wind_abbreviation.match(@compass_letter_pattern)
+  end
+
+  def test_es_ar_cardinal_azimuth 
+    assert Faker::Compass.cardinal_azimuth.match(@compass_number_pattern)
+  end
+
+  def test_es_ar_ordinal_azimuth 
+    assert Faker::Compass.ordinal_azimuth.match(@compass_number_pattern)
+  end
+
+  def test_es_ar_half_wind_azimuth 
+    assert Faker::Compass.half_wind_azimuth.match(@compass_number_pattern)
+  end
+
+  def test_es_ar_quarter_wind_azimuth 
+    assert Faker::Compass.quarter_wind_azimuth.match(@compass_number_pattern)
   end
 end
