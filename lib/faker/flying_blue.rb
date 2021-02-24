ab81e5f07e8d5cd16ce5d4cf91575283183879f0
Matthias <matthias.alric@mooncard.co>
2021-02-24 15:18:27 +0100

    flying blue account name generator

---
+++ lib/faker/flying_blue.rb
@@ -0,0 +1,12 @@
+module Faker
+  class FlyingBlue < Base
+    class << self
+      def account_number
+        number = rand.to_s[2..10]
+        check_digit = (number.to_i % 7) - 1
+        check_digit = 6 if check_digit.negative?
+        "#{number}#{check_digit}"
+      end
+    end
+  end
+end
