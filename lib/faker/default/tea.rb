f2d171d58949051a142f0da296085b8511fd1f7e
Eli Sadoff <9064062+snood1205@users.noreply.github.com>
2021-04-18 16:36:17 -0400

    Apply code review suggestions per @koic

--- lib/faker/default/tea.rb
+++ lib/faker/default/tea.rb
@@ -6,7 +6,7 @@ module Faker
 
     class << self
       ##
-      # Produces a random variety or blend of tea
+      # Produces a random variety or blend of tea.
       #
       # @param type [String, nil] the type of tea to query for (valid types: 'Black', 'Green', 'Oolong', 'White', and 'Herbal')
       # @return [String] a variety of tea
@@ -18,19 +18,21 @@ module Faker
       # @example
       #   Faker::Tea.variety(type: 'Green')
       #     #=> "Jasmine"
+      # @faker.version next
       def variety(type: nil)
         type ||= fetch('tea.type')
         fetch "tea.variety.#{type.downcase}"
       end
 
       ##
-      # Produces a random type of tea
+      # Produces a random type of tea.
       #
       # @return [String] a type of tea
       #
       # @example
       #   Faker::Tea.blend
       #     #=> "Green"
+      # @faker.version next
       def type
         fetch 'tea.type'
       end
