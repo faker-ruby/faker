9db340d33ebfb23485f93561d6d5c7dc1b68a5c5
Enow Mbi <benowmbi@yahoo.com>
2021-10-25 14:22:26 +0100

    Update yard documents for new methods - character, location, qoute

--- lib/faker/religion/bible.rb
+++ lib/faker/religion/bible.rb
@@ -6,14 +6,48 @@ module Faker
       flexible :bible
 
       class << self
+
+        ##
+        ## Returns a random bible character
+        ##
+        ## @param
+        ##
+        ## @return [String]
+        ##
+        ## @example
+        ## Faker::Religion::Bible.character #=> "Jesus"
+        ##
+        ## @faker.version next
         def character
           fetch('bible.character')
         end
 
+        ##
+        ## Returns a random location(city or town) from the bible
+        ##
+        ## @param
+        ##
+        ## @return [String]
+        ##
+        ## @example
+        ## Faker::Religion::Bible.location #=> "Nasareth"
+        ##
+        ## @faker.version next
         def location
           fetch('bible.location')
         end
 
+        ##
+        ## Returns a random quote from the location
+        ##
+        ## @param 
+        ##
+        ## @return [String]
+        ##
+        ## @example
+        ## Faker::Religion::Bible.quote #=> "Seek first the kingdom of God "
+        ##
+        ## @faker.version next
         def quote
           fetch('bible.quote')
         end
