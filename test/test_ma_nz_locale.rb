9986199d3a304679ef44d3429b2a967dfb7056ca
Engie <e.matene15@gmail.com>
2021-12-14 10:48:18 +1300

    fixed formatting

--- test/test_ma_nz_locale.rb
+++ test/test_ma_nz_locale.rb
@@ -2,7 +2,7 @@
 
 require_relative 'test_helper'
 
-class TestEnNzLocale < Test::Unit::TestCase
+class TestMaNzLocale < Test::Unit::TestCase
   def setup
     Faker::Config.locale = 'ma-NZ'
   end
@@ -11,19 +11,18 @@ class TestEnNzLocale < Test::Unit::TestCase
     Faker::Config.locale = nil
   end
 
-  # def test_en_nz_address_methods
-  #   assert Faker::Address.street_name.is_a? String
-  #   assert Faker::Address.city.is_a? String
-  #   assert Faker::Address.region.is_a? String
-  #   assert Faker::Address.region_abbr.is_a? String
-  #   assert Faker::Address.postcode.is_a? String
-  #   assert_equal 'Aotearoa', Faker::Address.default_country
-  # end
+  def test_ma_nz_address_methods
+    assert Faker::Address.street_name.is_a? String
+    assert Faker::Address.city.is_a? String
+    assert Faker::Address.region.is_a? String
+    assert Faker::Address.region_abbr.is_a? String
+    assert Faker::Address.postcode.is_a? String
+    assert_equal 'Aotearoa', Faker::Address.default_country
+  end
 
   def test_ma_nz_name_methods
     assert Faker::Name.first_name.is_a? String
     assert Faker::Name.last_name.is_a? String
     assert Faker::Name.name_with_middle.is_a? String
-    puts "ma-NZ test ran"
   end
 end
