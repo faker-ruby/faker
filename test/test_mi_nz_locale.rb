fe0ff5d55fead91a43d7505df59e8cbb9d93aa01
Engie <e.matene15@gmail.com>
2021-12-14 11:44:28 +1300

    renamed mi-NZ ISO-639 code and added double quotes

--- test/test_mi_nz_locale.rb
+++ test/test_mi_nz_locale.rb
@@ -4,14 +4,14 @@ require_relative 'test_helper'
 
 class TestMaNzLocale < Test::Unit::TestCase
   def setup
-    Faker::Config.locale = 'ma-NZ'
+    Faker::Config.locale = 'mi-NZ'
   end
 
   def teardown
     Faker::Config.locale = nil
   end
 
-  def test_ma_nz_address_methods
+  def test_mi_nz_address_methods
     assert Faker::Address.street_name.is_a? String
     assert Faker::Address.city.is_a? String
     assert Faker::Address.region.is_a? String
@@ -20,7 +20,7 @@ class TestMaNzLocale < Test::Unit::TestCase
     assert_equal 'Aotearoa', Faker::Address.default_country
   end
 
-  def test_ma_nz_name_methods
+  def test_mi_nz_name_methods
     assert Faker::Name.first_name.is_a? String
     assert Faker::Name.last_name.is_a? String
     assert Faker::Name.name_with_middle.is_a? String
