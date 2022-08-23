559051bd7c223316c41f9d82e310177d19d1fd73
Nicholas Mendez <mendez.nicholasr@gmail.com>
2022-08-23 16:36:33 -0400

    Removed ruby version checks in test_faker_library_in_leap_year

--- test/faker/default/test_faker_birthday_in_leap_year.rb
+++ test/faker/default/test_faker_birthday_in_leap_year.rb
@@ -19,24 +19,5 @@ class TestFakerBirthdayInLeapYear < Test::Unit::TestCase
     assert_nothing_raised ArgumentError do
       @tester.birthday
     end
-
-    # The error raised here is changed in Ruby 2.7.
-    if RUBY_VERSION < '2.7'
-      assert_raise ArgumentError do
-        ::Date.new(@today.year - @min, @today.month, @today.day)
-      end
-
-      assert_raise ArgumentError do
-        ::Date.new(@today.year - @max, @today.month, @today.day)
-      end
-    elsif RUBY_VERSION >= '2.7'
-      assert_raise Date::Error do
-        ::Date.new(@today.year - @min, @today.month, @today.day)
-      end
-
-      assert_raise Date::Error do
-        ::Date.new(@today.year - @max, @today.month, @today.day)
-      end
-    end
   end
 end
