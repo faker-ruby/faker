2d5365f7dda1fc63262b34c10ae907585b96376f
Stefanni Brasil <stefannibrasil@gmail.com>
2022-08-27 20:53:14 -0700

    No need to explicitely set SimpleCovFormatter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require 'simplecov'
-SimpleCov.formatter = SimpleCov::Formatter::HTMLFormatter
+
 SimpleCov.start do
   add_filter ['.bundle', 'lib/extensions', 'test']
 end
