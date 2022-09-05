d27cdc49686acbc8fdfe72c9dcff8eb1d6bd43c7
Yago Santos <yagosansz@gmail.com>
2022-09-05 16:37:03 -0400

    Add Tests for LoremFlickr grayscale_image Method

--- test/faker/default/test_lorem_flickr.rb
+++ test/faker/default/test_lorem_flickr.rb
@@ -21,11 +21,47 @@ class TestLoremFlickr < Test::Unit::TestCase
     end
   end
 
-  def test_image_with_search_terms
-    assert @tester.image(size: '300x300', search_terms: ['sports', 'fitness']).match(%r{https://loremflickr\.com/\d+/\d+/(.+\,.+)})[1] == 'sports,fitness'
+  def test_image_with_single_search_term
+    assert @tester.image(size: '300x300', search_terms: ['sports']).match(%r{https://loremflickr\.com/\d+/\d+/(.+)})[1] == 'sports'
+  end
+
+  def test_image_with_multiple_search_terms
+    assert @tester.image(size: '300x300', search_terms: %w[sports fitness]).match(%r{https://loremflickr\.com/\d+/\d+/(.+,.+)})[1] == 'sports,fitness'
   end
 
   def test_image_with_search_terms_and_match_all
-    assert @tester.image(size: '300x300', search_terms: ['sports', 'fitness'], match_all: true).match(%r{https://loremflickr\.com/\d+/\d+/.+\,.+/(all)})[1] == 'all'
+    assert @tester.image(size: '300x300', search_terms: %w[sports fitness], match_all: true).match(%r{https://loremflickr\.com/\d+/\d+/.+,.+/(.+)})[1] == 'all'
+  end
+
+  def test_grayscale_image
+    assert @tester.grayscale_image.match(%r{https://loremflickr\.com/g/\d+/\d+/.+})
+  end
+
+  def test_grayscale_image_with_custom_size
+    assert @tester.grayscale_image(size: '3x3').match(%r{https://loremflickr\.com/.+/(\d+/\d+)/.+})[1] == '3/3'
+  end
+
+  def test_grayscale_image_with_incorrect_size
+    assert_raise ArgumentError do
+      @tester.grayscale_image(size: '300x300s')
+    end
+  end
+
+  def test_grayscale_image_with_single_search_term
+    assert @tester.grayscale_image(size: '300x300', search_terms: ['sports']).match(%r{https://loremflickr\.com/.+/\d+/\d+/(.+)})[1] == 'sports'
+  end
+
+  def test_grayscale_image_with_multiple_search_terms
+    assert @tester.grayscale_image(size: '300x300', search_terms: %w[sports fitness]).match(%r{https://loremflickr\.com/.+/\d+/\d+/(.+,.+)})[1] == 'sports,fitness'
+  end
+
+  def test_grayscale_image_without_search_terms
+    assert_raise ArgumentError do
+      assert @tester.grayscale_image(search_terms: [])
+    end
+  end
+
+  def test_grayscale_image_with_search_terms_and_match_all
+    assert @tester.grayscale_image(size: '300x300', search_terms: %w[sports fitness], match_all: true).match(%r{https://loremflickr\.com/.+/\d+/\d+/.+,.+/(.+)})[1] == 'all'
   end
 end
