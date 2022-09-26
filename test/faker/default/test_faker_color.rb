a7746b9576f9f035202defbf74ed437f86269121
Francois <buys.fran@gmail.com>
2022-09-26 19:47:05 +0200

    Issue-2464: Add color luminosity options (#2566)
    * Add ability to override hsl_color lightness
    
    So we can generate lighter and darker colors as requested in the issue:
    https://github.com/faker-ruby/faker/issues/2464.
    
    Next step is to convert a light/dark hsl to hex.
    
    * Use hsl_color and hsl_to_hex to generate hex_color
    
    So we can easily specify when we want light or dark colors, since hsl
    values have a very intuitive way to specify light and dark colors.
    
    See: https://github.com/faker-ruby/faker/issues/2464
    
    * Add ability to specify light or dark hex color
    
    New param allows user to pass in :light for light hex color and :dark
    for dark hex colors.
    
    Additional specs confirm that this works.
    
    See: https://github.com/faker-ruby/faker/issues/2464
    
    * Update version to next
    
    As per: https://github.com/faker-ruby/faker/blob/master/CONTRIBUTING.md#documentation
    
    * Fix rubocop linting issue
    
    * Allow specific hue and saturation values for hsl_color
    
    Adding specs to confirm this works, and also prevents specifying invalid
    hsl colors.
    
    @see https://github.com/faker-ruby/faker/pull/2566#discussion_r978576288
    
    * Add LIGHTNESS_LOOKUP constant
    
    @see https://github.com/faker-ruby/faker/pull/2566#discussion_r978575583
    
    * Introduct hsl_hash to hex_color
    
    So it cash be used to pass values (such as hue and saturation) to
    hsl_color.
    
    See: https://github.com/faker-ruby/faker/pull/2566#issuecomment-1256163322
    
    * Rename lightness to args
    
    To make it more general.
    
    @see https://github.com/faker-ruby/faker/pull/2566#issuecomment-1256163322
    
    * Allow hue, saturation or lightness for hex colors
    
    So clients are able to generate very specific colors when needed.
    
    See: https://github.com/faker-ruby/faker/pull/2566#issuecomment-1256163322
    
    * Fix documentation typo

--- test/faker/default/test_faker_color.rb
+++ test/faker/default/test_faker_color.rb
@@ -15,6 +15,33 @@ class TestFakerColor < Test::Unit::TestCase
     assert_match(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/, @tester.hex_color)
   end
 
+  # @see https://www.rapidtables.com/convert/color/rgb-to-hsl.html
+  def helper_hex_lightness(hex_color)
+    result = hex_color.scan(/([A-Fa-f0-9]{2})/).flatten.map { |x| x.hex / 255.0 }
+    (result.max + result.min) / 2
+  end
+
+  def test_hex_color_light
+    assert_in_delta(0.8, helper_hex_lightness(@tester.hex_color(:light)))
+  end
+
+  def test_hex_color_dark
+    assert_in_delta(0.2, helper_hex_lightness(@tester.hex_color(:dark)))
+  end
+
+  def test_hex_color_with_hash_is_passed_to_hsl_color
+    mock_hsl_color = lambda do |args|
+      assert_equal(100, args[:hue])
+      assert_in_delta(0.2, args[:saturation])
+      assert_in_delta(0.8, args[:lightness])
+      [args[:hue], args[:saturation], args[:lightness]]
+    end
+    @tester.stub :hsl_color, mock_hsl_color do
+      result = @tester.hex_color(hue: 100, saturation: 0.2, lightness: 0.8)
+      assert_match(/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/, result)
+    end
+  end
+
   def test_single_rgb_color
     assert @tester.single_rgb_color.between?(0, 255)
   end
@@ -36,6 +63,31 @@ class TestFakerColor < Test::Unit::TestCase
     assert @result[0].is_a?(Integer)
     assert @result[1].between?(0.0, 1.0)
     assert @result[2].between?(0.0, 1.0)
+  end
+
+  def test_hsl_color_with_a_speficied_hue
+    @result = @tester.hsl_color(hue: 5)
+    assert_in_delta(5, @result[0])
+  end
+
+  def test_hsl_color_with_a_speficied_saturation
+    @result = @tester.hsl_color(saturation: 0.35)
+    assert_in_delta(0.35, @result[1])
+  end
+
+  def test_hsl_color_with_a_speficied_but_invalid_saturation
+    @result = @tester.hsl_color(saturation: 3.05)
+    assert_in_delta(1, @result[1])
+  end
+
+  def test_hsl_color_with_a_speficied_lightness
+    @result = @tester.hsl_color(lightness: 0.5)
+    assert_in_delta(0.5, @result[2])
+  end
+
+  def test_hsl_color_with_a_speficied_but_invalid_lightness
+    @result = @tester.hsl_color(lightness: -2.5)
+    assert_in_delta(0, @result[2])
   end
 
   def test_hsla_color
