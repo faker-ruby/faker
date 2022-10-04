1efe94f7f616ecdd38748854f4049534f410695e
rishadp <rishadp2747@gmail.com>
2022-10-04 18:41:22 +0530

    Removed depricated warning generater method

--- lib/faker.rb
+++ lib/faker.rb
@@ -259,58 +259,6 @@ module Faker
       ensure
         I18n.enforce_available_locales = old_enforce_available_locales
       end
-
-      private
-
-      def warn_for_deprecated_arguments
-        keywords = []
-        yield(keywords)
-
-        return if keywords.empty?
-
-        method_name = caller.first.match(/`(?<method_name>.*)'/)[:method_name]
-
-        keywords.each.with_index(1) do |keyword, index|
-          i = case index
-              when 1 then '1st'
-              when 2 then '2nd'
-              when 3 then '3rd'
-              else "#{index}th"
-              end
-
-          warn_with_uplevel(<<~MSG, uplevel: 5)
-            Passing `#{keyword}` with the #{i} argument of `#{method_name}` is deprecated. Use keyword argument like `#{method_name}(#{keyword}: ...)` instead.
-          MSG
-        end
-
-        warn(<<~MSG)
-
-          To automatically update from positional arguments to keyword arguments,
-          install rubocop-faker and run:
-
-          rubocop \\
-            --require rubocop-faker \\
-            --only Faker/DeprecatedArguments \\
-            --auto-correct
-
-        MSG
-      end
-
-      # Workaround for emulating `warn '...', uplevel: 1` in Ruby 2.4 or lower.
-      def warn_with_uplevel(message, uplevel: 1)
-        at = parse_caller(caller[uplevel]).join(':')
-        warn "#{at}: #{message}"
-      end
-
-      def parse_caller(at)
-        # rubocop:disable Style/GuardClause
-        if /^(.+?):(\d+)(?::in `.*')?/ =~ at
-          file = Regexp.last_match(1)
-          line = Regexp.last_match(2).to_i
-          [file, line]
-        end
-        # rubocop:enable Style/GuardClause
-      end
     end
   end
 end
