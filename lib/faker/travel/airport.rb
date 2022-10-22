9a2609cb6db21aa5eb1bef7e60d8ff3cc28979c9
Zion Miller <djzionmiller@gmail.com>
2022-10-22 14:21:26 -0700

    added travel folder and airports. I often find myself creating these seeds and would love the addition to Faker to help save time on travel apps. This commit contains us and eu airports by size and iata code

---
+++ lib/faker/travel/airport.rb
@@ -0,0 +1,139 @@
+# frozen_string_literal: true
+
+module Faker
+  class Travel
+    class Airport < Base
+      class << self
+        ##
+        # Produces random large US Airport by name.
+        #
+        # @return [String]
+        #
+        # @example
+        # Faker::Travel::Airport.large_airport_us #=> "Los Angeles International Airport"
+        #
+        # @faker.version next
+        def large_airport_us
+          fetch('airport.large_airport_us')
+        end
+        ##
+        # Produces random large EU Airport by name.
+        #
+        # @return [String]
+        #
+        # @example
+        # Faker::Travel::Airport.airport_name #=> "Munich Airport"
+        #
+        # @faker.version next
+
+        def large_airport_eu
+          fetch('airport.large_airport_eu')
+        end
+        ##
+        # Produces random medium US Airport by name.
+        #
+        # @return [String]
+        #
+        # @example
+        # Faker::Travel::Airport.medium_airport_us #=> "Albuquerque International Sunport"
+        #
+        # @faker.version next
+
+        def medium_airport_us
+          fetch('airport.medium_airport_us')
+        end
+        ##
+        # Produces random medium EU Airport by name.
+        #
+        # @return [String]
+        #
+        # @example
+        # Faker::Travel::Airport.medium_airport_eu #=> "Dublin Airport"
+        #
+        # @faker.version next
+
+        def medium_airport_eu
+          fetch('airport.medium_airport_eu')
+        end
+        ##
+        # Produces random small US Airport by name.
+        #
+        # @return [String]
+        #
+        # @example
+        # Faker::Travel::Airport.small_airport_us #=> "Des Moines International Airport"
+        #
+        # @faker.version next
+
+        def small_airport_us
+          fetch('airport.small_airport_us')
+        end
+        ##
+        # Produces random large US Airport by IATA code.
+        #
+        # @return [String]
+        #
+        # @example
+        # Faker::Travel::Airport.airports_iata_code #=> "LAX"
+        #
+        # @faker.version next
+
+        def iata_large_us
+          fetch('airport.iata_large_us')
+        end
+        ##
+        # Produces random large EU Airport by IATA code.
+        #
+        # @return [String]
+        #
+        # @example
+        # Faker::Travel::Airport.iata_large_eu #=> "FRA"
+        #
+        # @faker.version next
+
+        def iata_large_eu
+          fetch('airport.iata_large_eu')
+        end
+        ##
+        # Produces random medium US Airport by IATA code.
+        #
+        # @return [String]
+        #
+        # @example
+        # Faker::Travel::Airport.iata_large_eu #=> "ABQ"
+        #
+        # @faker.version next
+
+        def iata_medium_us
+          fetch('airport.iata_medium_us')
+        end
+        ##
+        # Produces random medium EU Airport by IATA code.
+        #
+        # @return [String]
+        #
+        # @example
+        # Faker::Travel::Airport.iata_large_eu #=> "GLA"
+        #
+        # @faker.version next
+
+        def iata_medium_eu
+          fetch('airport.iata_medium_eu')
+        end
+        ##
+        # Produces random small US Airport by IATA code.
+        #
+        # @return [String]
+        #
+        # @example
+        # Faker::Travel::Airport.iata_large_eu #=> "ALB"
+        #
+        # @faker.version next
+
+        def iata_small_us
+          fetch('airport.iata_small_us')
+        end
+      end
+    end
+  end
+end
---
+++ test/faker/travel/test_faker_airports.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require_relative '../../test_helper'
+
+class TestFakerAirport < Test::Unit::TestCase
+  def setup
+    @tester = Faker::Travel::Airport
+  end
+
+  # test using assert_match not assert
+
+  def test_large_airport_us
+    assert_match(/\w+/, @tester.large_airport_us)
+  end
+
+  def test_large_airport_eu
+    assert_match(/\w+/, @tester.large_airport_eu)
+  end
+
+  def test_medium_airport_us
+    assert_match(/\w+/, @tester.medium_airport_us)
+  end
+
+  def test_medium_airport_eu
+    assert_match(/\w+/, @tester.medium_airport_eu)
+  end
+
+  def test_small_airport_us
+    assert_match(/\w+/, @tester.small_airport_us)
+  end
+
+  def test_large_iata_us
+    assert_match(/\w+/, @tester.iata_large_us)
+  end
+
+  def test_large_iata_eu
+    assert_match(/\w+/, @tester.iata_large_eu)
+  end
+
+  def test_medium_iata_us
+    assert_match(/\w+/, @tester.iata_medium_us)
+  end
+
+  def test_medium_iata_eu
+    assert_match(/\w+/, @tester.iata_medium_eu)
+  end
+
+  def test_small_iata_us
+    assert_match(/\w+/, @tester.iata_small_us)
+  end
+end
