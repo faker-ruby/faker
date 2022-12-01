78e54217b37164ae6a132aaa345e8f8a3db5e17b
Willian Gustavo Veiga <beberveiga@gmail.com>
2022-12-01 18:52:46 -0300

    Add ArrayType generator

---
+++ lib/faker/default/array_type.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+module Faker
+  class ArrayType < Base
+    class << self
+      def integer(size: 3)
+        return [] if size.zero?
+
+        size.times.map { Faker::Types.rb_integer }
+      end
+
+      def random_size_integer
+        integer(size: Faker::Types.rb_integer)
+      end
+    end
+  end
+end
---
+++ test/faker/default/test_array_type.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+require_relative '../../test_helper'
+
+class TestArrayType < Test::Unit::TestCase
+  def setup
+    @tester = Faker::ArrayType
+  end
+
+  def test_integer_default_integer_size
+    assert_equal 3, @tester.integer.count
+  end
+
+  def test_integer_zero_integers
+    assert_equal 0, @tester.integer(size: 0).count
+  end
+
+  def test_integer_five_integers
+    assert_equal 5, @tester.integer(size: 5).count
+  end
+
+  def test_random_size_integer
+    Faker::Types.stub :rb_integer, 3 do
+      assert_equal 3, @tester.random_size_integer.count
+    end
+  end
+end
