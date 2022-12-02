2e59237a340c8f7854563f4594e38bceece5ea8e
Willian Gustavo Veiga <beberveiga@gmail.com>
2022-12-02 13:49:58 -0300

    Add documentation

--- lib/faker/default/array_type.rb
+++ lib/faker/default/array_type.rb
@@ -3,6 +3,14 @@
 module Faker
   class ArrayType < Base
     class << self
+      ##
+      # Produces an array of integers of given size.
+      #
+      # @param size [Integer]
+      # @return [Array]
+      #
+      # @example
+      #   > Faker::ArrayType.integer #=> [99, 3, 19]
       def integer(size: 3)
         raise ArgumentError, 'Size must be positive' if size.negative?
         return [] if size.zero?
@@ -10,6 +18,13 @@ module Faker
         size.times.map { Faker::Types.rb_integer }
       end
 
+      ##
+      # Produces an array of integers of random size.
+      #
+      # @return [Array]
+      #
+      # @example
+      #   > Faker::ArrayType.random_size_integer #=> [98, 3, 54, 18, 22, 33, 7, 99, 1, 7]
       def random_size_integer
         integer(size: Faker::Types.rb_integer)
       end
