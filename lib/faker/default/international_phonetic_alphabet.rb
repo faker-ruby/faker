f48dfed32b5193368d28a1e3e2c511b64518ea64
Griffin Riley <rileyg@oregonstate.edu>
2023-06-07 12:44:34 -0700

    added yard style docs for all methods

--- lib/faker/default/international_phonetic_alphabet.rb
+++ lib/faker/default/international_phonetic_alphabet.rb
@@ -5,14 +5,50 @@ module Faker
 
     class << self
 
+      ##
+      # Produces random symbols for consonants in the international phonetic alphabet
+      #
+      # @param amount [Integer] The number of consonants to generate
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::InternationalPhoneticAlphabet.consonant             #=> "ʂ"
+      #   Faker::InternationalPhoneticAlphabet.consonant(amount: 3)  #=> "fʕŋ"
+      #
+      # @faker.version next
       def consonant(amount: 1)
         (1..amount).map{fetch('international_phonetic_alphabet.consonants')}.join
       end
 
+      ##
+      # Produces random symbols for vowels in the international phonetic alphabet
+      #
+      # @param amount [Integer] The number of vowels to generate
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::InternationalPhoneticAlphabet.vowel                 #=> "ɞ"
+      #   Faker::InternationalPhoneticAlphabet.vowel(amount: 3)      #=> "əæɜ"
+      #
+      # @faker.version next
       def vowel(amount: 1)
         (1..amount).map{fetch('international_phonetic_alphabet.vowels')}.join
       end
 
+      ##
+      # Produces random symbols for characters in the international phonetic alphabet
+      #
+      # @param amount [Integer] The number of characters to generate
+      #
+      # @return [String]
+      #
+      # @example
+      #   Faker::InternationalPhoneticAlphabet.character                 #=> "ʋ"
+      #   Faker::InternationalPhoneticAlphabet.character(amount: 3)      #=> "ɜʐd"
+      #
+      # @faker.version next
       def character(amount: 1)
         (1..amount).map{fetch('international_phonetic_alphabet.characters')}.join
       end
