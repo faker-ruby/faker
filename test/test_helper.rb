68bf990cdcd4b4beed34e4168cf3085e1486e5fd
Erich Soares Machado <erichmachado@users.noreply.github.com>
2023-09-14 22:02:39 -0400

    Move the default value of :random to the #deterministically_verify method signature

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -31,9 +31,9 @@ require File.expand_path("#{File.dirname(__FILE__)}/../lib/faker")
 #     assert subject.match(/(bo(_|\.)peep|peep(_|\.)bo)/)
 #   end
 #
-def deterministically_verify(subject_proc, depth: 2, random: nil)
+def deterministically_verify(subject_proc, depth: 2, random: Random.new(42))
   results = depth.times.map do
-    Faker::Config.stub :random, random.clone || Random.new(42) do
+    Faker::Config.stub :random, random.clone do
       yield subject_proc.call.freeze
     end
   end
