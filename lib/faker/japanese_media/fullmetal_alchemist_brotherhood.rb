50b540fec49f4c693247562782066883c5ddb88c
Kirk Wang <kirk.wang@scientist.com>
2024-02-25 15:24:34 -0800

    Refactor deprecation for `FmaBrotherhood`
    This commit will refactor the changes from `FmaBrotherhood` to
    `FullmetalAlchemistBrotherhood` and use `Faker::Deprecator`.

--- lib/faker/japanese_media/fullmetal_alchemist_brotherhood.rb
+++ lib/faker/japanese_media/fullmetal_alchemist_brotherhood.rb
@@ -2,54 +2,6 @@
 
 module Faker
   class JapaneseMedia
-    class FmaBrotherhood < Base
-      class << self
-        extend Gem::Deprecate
-
-        ##
-        # Produces a character from FmaBrotherhood.
-        #
-        # @return [String]
-        #
-        # @example
-        #   Faker::JapaneseMedia::FmaBrotherhood.character #=> "Edward Elric"
-        #
-        # @faker.version next
-        def character
-          fetch('fma_brotherhood.characters')
-        end
-        deprecate :character, 'Faker::JapaneseMedia::FullmetalAlchemistBrotherhood.character', 2024, 02
-
-        ##
-        # Produces a cities from FmaBrotherhood.
-        #
-        # @return [String]
-        #
-        # @example
-        #   Faker::JapaneseMedia::FmaBrotherhood.city #=> "Central City"
-        #
-        # @faker.version next
-        def city
-          fetch('fma_brotherhood.cities')
-        end
-        deprecate :city, 'Faker::JapaneseMedia::FullmetalAlchemistBrotherhood.city', 2024, 02
-
-        ##
-        # Produces a country from FmaBrotherhood.
-        #
-        # @return [String]
-        #
-        # @example
-        #   Faker::JapaneseMedia::FmaBrotherhood.country #=> "Xing"
-        #
-        # @faker.version next
-        def country
-          fetch('fma_brotherhood.countries')
-        end
-        deprecate :country, 'Faker::JapaneseMedia::FullmetalAlchemistBrotherhood.country', 2024, 02
-      end
-    end
-
     class FullmetalAlchemistBrotherhood < Base
       class << self
         ##
@@ -92,5 +44,8 @@ module Faker
         end
       end
     end
+
+    include Faker::Deprecator
+    deprecate_generator('FmaBrotherhood', FullmetalAlchemistBrotherhood)
   end
 end
--- test/faker/japanese_media/test_faker_fullmetal_alchemist_brotherhood.rb
+++ test/faker/japanese_media/test_faker_fullmetal_alchemist_brotherhood.rb
@@ -2,39 +2,21 @@
 
 require_relative '../../test_helper'
 
-class DeprecatedTestFakerFmaBrotherhood < Test::Unit::TestCase
+class TestFakerFmaBrotherhood < Test::Unit::TestCase
   def setup
     @tester = Faker::JapaneseMedia::FmaBrotherhood
   end
 
   def test_character
-    actual_stdout, actual_stderr = capture_output do
-      @tester.character
-    end
-    expected_warning = /NOTE: Faker::JapaneseMedia::FmaBrotherhood.character is deprecated; use Faker::JapaneseMedia::FullmetalAlchemistBrotherhood.character instead. It will be removed on or after 2024-02.*\n/
-
-    assert_empty actual_stdout
-    assert_match(expected_warning, actual_stderr)
+    assert_match(/\w+/, @tester.character)
   end
 
   def test_city
-    actual_stdout, actual_stderr = capture_output do
-      @tester.city
-    end
-    expected_warning = /NOTE: Faker::JapaneseMedia::FmaBrotherhood.city is deprecated; use Faker::JapaneseMedia::FullmetalAlchemistBrotherhood.city instead. It will be removed on or after 2024-02.*\n/
-
-    assert_empty actual_stdout
-    assert_match(expected_warning, actual_stderr)
+    assert_match(/\w+/, @tester.city)
   end
 
   def test_country
-    actual_stdout, actual_stderr = capture_output do
-      @tester.country
-    end
-    expected_warning = /NOTE: Faker::JapaneseMedia::FmaBrotherhood.country is deprecated; use Faker::JapaneseMedia::FullmetalAlchemistBrotherhood.country instead. It will be removed on or after 2024-02.*\n/
-
-    assert_empty actual_stdout
-    assert_match(expected_warning, actual_stderr)
+    assert_match(/\w+/, @tester.country)
   end
 end
 
