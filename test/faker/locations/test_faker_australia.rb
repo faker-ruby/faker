865bddf52891fd6e793ad0e51d30ee2e2c505d54
Keshav Biswa <keshavbiswa21@gmail.com>
2024-03-19 10:56:26 +0530

    updated test to differentiate deprecated methods

--- test/faker/locations/test_faker_australia.rb
+++ test/faker/locations/test_faker_australia.rb
@@ -3,37 +3,29 @@
 require_relative '../../test_helper'
 
 class TestFakerLocationsAustralia < Test::Unit::TestCase
-  def setup
-    @tester = Faker::Locations::Australia
-  end
-
   def test_location
-    assert_match(/\w+/, @tester.location)
+    assert_match(/\w+/, Faker::Locations::Australia.location)
   end
 
   def test_animal
-    assert_match(/\w+/, @tester.animal)
+    assert_match(/\w+/, Faker::Locations::Australia.animal)
   end
 
   def test_state
-    assert_match(/\w+/, @tester.state)
+    assert_match(/\w+/, Faker::Locations::Australia.state)
   end
 end
 
 class TestFakerAustralia < Test::Unit::TestCase
-  def setup
-    @tester = Faker::Australia
+  def test_deprecated_location
+    assert_match(/\w+/, Faker::Australia.location)
   end
 
-  def test_location
-    assert_match(/\w+/, @tester.location)
-  end
-
-  def test_animal
-    assert_match(/\w+/, @tester.animal)
+  def test_deprecated_animal
+    assert_match(/\w+/, Faker::Australia.animal)
   end
 
   def test_state
-    assert_match(/\w+/, @tester.state)
+    assert_match(/\w+/, Faker::Australia.state)
   end
 end
