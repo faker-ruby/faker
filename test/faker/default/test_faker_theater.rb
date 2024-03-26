687cc87e3980b258837aedc3af25e9e6c3f0ceb4
Keshav Biswa <keshavbiswa21@gmail.com>
2024-03-26 11:26:16 +0530

    Renamed Faker::Show to Faker::Theater

--- test/faker/default/test_faker_theater.rb
+++ test/faker/default/test_faker_theater.rb
@@ -2,6 +2,25 @@
 
 require_relative '../../test_helper'
 
+class TestFakerTheater < Test::Unit::TestCase
+  def setup
+    @tester = Faker::Theater
+  end
+
+  def test_adult_musical
+    assert_match(/\w+/, @tester.adult_musical)
+  end
+
+  def test_kids_musical
+    assert_match(/\w+/, @tester.kids_musical)
+  end
+
+  def test_play
+    assert_match(/\w+/, @tester.play)
+  end
+end
+
+# with test_faker_show.rb
 class TestFakerShow < Test::Unit::TestCase
   def test_adult_musical
     assert_match(/\w+/, Faker::Show.adult_musical)
