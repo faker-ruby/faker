fff65dfd18ec326cbd47716f6bd8d2ed0d0e2d83
Keshav Biswa <keshavbiswa21@gmail.com>
2024-03-26 22:13:30 +0530

    renamed nhs to national_health_service

--- test/faker/default/test_faker_national_health_service.rb
+++ test/faker/default/test_faker_national_health_service.rb
@@ -11,17 +11,17 @@ class TestFakerNationalHealthService < Test::Unit::TestCase
     assert_match(/\A\d{3}\s\d{3}\s\d{4}\z/, @tester.british_number)
   end
 
-  def test_nhs_check_digit_equals_10
+  def test_check_digit_equals_10
     Faker::NationalHealthService.stub(:rand, 458_617_434) do
       assert_match('458 617 4331', @tester.british_number)
     end
   end
 
-  def test_nhs_check_digit
+  def test_check_digit
     assert_equal 6, @tester.check_digit(number: 400_012_114)
   end
 
-  def test_nhs_check_digit_11
+  def test_check_digit_11
     assert_equal 0, @tester.check_digit(number: 418_513_625)
   end
 end
