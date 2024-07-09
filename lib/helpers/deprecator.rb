2b5df1704a574193644fe2f79bed59e47cccb762
Keshav Biswa <keshavbiswa21@gmail.com>
2024-07-09 20:48:00 +0530

    Add `Deprecator.skip_warning?` ability to silence deprecators on tests (#2956)
    * Added skip_warning ability to deprecators for tests
    
    ---------
    
    Co-authored-by: Stefanni Brasil <stefannibrasil@gmail.com>
    Co-authored-by: Thiago Araujo <thd.araujo@gmail.com>

--- lib/helpers/deprecator.rb
+++ lib/helpers/deprecator.rb
@@ -10,7 +10,10 @@ module Faker
       extension = Module.new do
         def const_missing(missing_const_name)
           if class_variable_defined?(:@@_deprecated_constants) && (replacement = class_variable_get(:@@_deprecated_constants)[missing_const_name.to_s])
-            $stdout.puts("DEPRECATION WARNING: #{name}::#{replacement[:old_generator]} is deprecated. Use #{replacement[:new_constant]} instead.")
+            unless Faker::Deprecator.skip_warning?
+              $stdout.puts("DEPRECATION WARNING: #{name}::#{replacement[:old_generator]} is deprecated. Use #{replacement[:new_constant]} instead.")
+            end
+
             return replacement[:new_constant]
           end
 
@@ -24,6 +27,26 @@ module Faker
       end
 
       base.singleton_class.prepend extension
+    end
+
+    def self.skip_warning
+      original = Faker::Deprecator.skip
+      Faker::Deprecator.skip = true
+      yield
+    ensure
+      Faker::Deprecator.skip = original
+    end
+
+    def self.skip_warning?
+      skip == true
+    end
+
+    def self.skip
+      @skip ||= false
+    end
+
+    def self.skip=(value)
+      @skip = value
     end
   end
 end
