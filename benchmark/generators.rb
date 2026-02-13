01c916a6ebcbd69cc2f2675577c8c10a51a0f965
Stefanni Brasil <stefannibrasil@proton.me>
2026-02-13 16:31:45 -0700

    wip benchmark + eager load as config

--- benchmark/generators.rb
+++ benchmark/generators.rb
@@ -4,43 +4,54 @@ require 'benchmark/ips'
 require 'faker'
 
 def generators
-  constants.flat_map do |klass|
-    subclass_methods(klass)
+  faker_descendants(Faker).flat_map { |klass| subclass_methods(klass) }
+end
+
+def faker_descendants(mod)
+  mod.constants.flat_map do |const|
+    next if skipped_constants.include?(const)
+
+    value = mod.const_get(const)
+
+    case value
+    when Class
+      value < Faker::Base ? [value] : []
+    when Module
+      faker_descendants(value)
+    else
+      []
+    end
   end
 end
 
-def constants
-  Faker.constants.delete_if do |subclass|
-    %i[
-      Base
-      Cat
-      Char
-      Base58
-      Config
-      Date
-      Deprecator
-      Dog
-      Religion
-      Time
-      VERSION
-    ].include?(subclass)
-  end.sort
+def subclass_methods(klass)
+  (klass.singleton_methods(false) - Faker::Base.singleton_methods(false))
+    .filter_map do |method|
+      parameters = klass.method(method).parameters
+
+      if parameters.none? { |type, _| [:req, :keyreq].include?(type) }
+        [klass, method]
+      end
+    end
 end
 
-def subclass_methods(subclass)
-  klass = Faker.const_get(subclass)
-
-  public_methods = klass.public_methods(false) - Faker::Base.public_methods(false)
-
-  public_methods.sort.map do |method|
-    [klass, method]
-  end
+def skipped_constants
+  %i[
+    Base
+    Base58
+    Cat
+    Char
+    Config
+    Deprecator
+    Dog
+    VERSION
+  ]
 end
 
 all_generators = generators
 
 Benchmark.ips do |x|
   x.report("Number of generators: #{all_generators.count}") do
-    all_generators.each { |klass, generator| klass.send(generator) }
+    all_generators.each { |klass, generator| klass.public_send(generator) }
   end
 end
